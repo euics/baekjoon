7bd59cc4d7dd0ada22518c751ad6b5dbabffbc37
euics <pjh575157@gmail.com>
2023-07-16 18:17:28 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,38 +3,39 @@ import java.io.*;
 
 public class Main {
     static int n;
-    static int m;
     static int[][] arr;
     static boolean[] bool;
-    static int answer = 0;
-
-    private static int DFS(int start){
-        bool[start] = true;
-        for(int i = 1; i <= n; i++){
-            if(arr[start][i] == 1 && !bool[i]) {
-                answer++;
-                DFS(i);
-            }
-        }
-
-        return answer;
-    }
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
         arr = new int[n + 1][n + 1];
         bool = new boolean[n + 1];
+
+        int m = Integer.parseInt(br.readLine());
         for(int i = 0; i < m; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = arr[b][a] = 1;
+            for(int j = 0; j < 2; j++)
+                arr[a][b] = arr[b][a] = 1;
         }
 
         DFS(1);
-        System.out.println(answer);
+
+        System.out.println(count);
+    }
+
+    private static void DFS(int node){
+        bool[node] = true;
+
+        for(int i = 1; i <= n; i++){
+            if(arr[node][i] == 1 && !bool[i]){
+                count++;
+                DFS(i);
+            }
+        }
     }
 }
