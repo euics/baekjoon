b0ea94ea23fb61ab841aedd821216e6704a4b45a
euics <pjh575157@gmail.com>
2023-07-16 19:28:14 +0900

    [Silver I] Title: 안전 영역, Time: 256 ms, Memory: 19184 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,12 +2,13 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n;
+    static int[] dix = {0, 0, -1, 1};
+    static int[] diy = {-1, 1, 0, 0};
+
     static int[][] arr;
     static boolean[][] bool;
-    static int[] dix = {-1, 1, 0, 0};
-    static int[] diy = {0, 0, -1, 1};
-    static int count = 0;
+
+    static int n, count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,35 +23,33 @@ public class Main {
         }
 
         int max = Integer.MIN_VALUE;
-        for(int k = 1; k <= 100; k++){
+        for(int i = 1; i <= 100; i++){
             bool = new boolean[n][n];
-            count = 0;
-
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(!bool[i][j] && arr[i][j] >= k){
-                        DFS(i, j, k);
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k < n; k++){
+                    if(arr[j][k] >= i && !bool[j][k]){
                         count++;
+                        DFS(j, k, i);
                     }
                 }
             }
-
             max = Math.max(max, count);
+            count = 0;
         }
 
         System.out.println(max);
     }
 
-    private static void DFS(int x, int y, int k){
-        bool[x][y] = true;
+    private static void DFS(int y, int x, int height){
+        bool[y][x] = true;
 
         for(int i = 0; i < 4; i++){
             int nx = x + dix[i];
             int ny = y + diy[i];
 
             if(nx >= 0 && ny >= 0 && nx < n && ny < n){
-                if(arr[nx][ny] >= k && !bool[nx][ny])
-                    DFS(nx, ny, k);
+                if(arr[ny][nx] >= height && !bool[ny][nx])
+                    DFS(ny, nx, height);
             }
         }
     }
