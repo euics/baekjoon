4b44815e1260cc2ec45ef238c137b9300337a28b
euics <pjh575157@gmail.com>
2023-08-06 21:54:17 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 15528 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,74 +3,89 @@ import java.io.*;
 
 public class Main {
     static int n;
-    static char[][] arr;
+    static char[][] normal;
+    static char[][] blind;
     static boolean[][] bool;
-    static int count = 0;
-    static int[] dix = {-1, 1, 0, 0};
-    static int[] diy = {0, 0, -1, 1};
+    static int[] dix = {0, 0, -1, 1};
+    static int[] diy = {-1, 1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(br.readLine());
-        arr = new char[n][n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        normal = new char[n][n];
+        blind = new char[n][n];
         bool = new boolean[n][n];
-        for(int i = 0; i < n; i++){
-            String str = br.readLine();
-            for(int j = 0; j < n; j++)
-                arr[i][j] = str.charAt(j);
-        }
 
         for(int i = 0; i < n; i++){
+            String tmp = br.readLine();
+            char[] ch = tmp.toCharArray();
             for(int j = 0; j < n; j++){
-                if(!bool[i][j]){
-                    DFS(i, j);
-                    count++;
-                }
+                normal[i][j] = ch[j];
+
+                if(ch[j] == 'G')
+                    blind[i][j] = 'R';
+                else
+                    blind[i][j] = ch[j];
             }
         }
 
-        int normal = count;
-        count = 0;
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(arr[i][j] == 'G')
-                    arr[i][j] = 'R';
-            }
-        }
-
-        bool = new boolean[n][n];
+        int answer = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(!bool[i][j]) {
-                    DFS(i, j);
-                    count++;
+                    answer++;
+                    normal_DFS(j, i);
                 }
             }
         }
 
-        int addNormal = count;
-        System.out.printf("%d %d", normal, addNormal);
+        System.out.printf("%d ", answer);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                bool[i][j] = false;
+        }
+
+        answer = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(!bool[i][j]){
+                    answer++;
+                    blind_DFS(j, i);
+                }
+            }
+        }
+
+        System.out.println(answer);
     }
 
-    private static void DFS(int x, int y){
-        bool[x][y] = true;
-        char color = arr[x][y];
+    private static void normal_DFS(int x, int y){
+        bool[y][x] = true;
 
         for(int i = 0; i < 4; i++){
             int nx = x + dix[i];
             int ny = y + diy[i];
 
             if(nx >= 0 && ny >= 0 && nx < n && ny < n){
-                if(arr[nx][ny] == color && !bool[nx][ny])
-                    DFS(nx, ny);
+                if(!bool[ny][nx] && normal[ny][nx] == normal[y][x])
+                    normal_DFS(nx, ny);
             }
         }
     }
-}
 
-/*
- 0 0 0 0 1
- 0 0 0 0 0
- 1 1 1 1 1
-*/
+    private static void blind_DFS(int x, int y){
+        bool[y][x] = true;
+
+        for(int i = 0; i < 4; i++){
+            int nx = x + dix[i];
+            int ny = y + diy[i];
+
+            if(nx >= 0 && ny >= 0 && nx < n && ny < n){
+                if(!bool[ny][nx] && blind[ny][nx] == blind[y][x])
+                    blind_DFS(nx, ny);
+            }
+        }
+    }
+
+}
