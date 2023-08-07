71a92860c05d74b5e35baeff5f906e7c0afcbd0d
euics <pjh575157@gmail.com>
2023-08-07 13:54:33 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,56 +4,59 @@ import java.io.*;
 public class Main {
     static int n, m;
     static int[][] arr;
+    static int[][] dis;
     static boolean[][] bool;
-
     static int[] dix = {0, 0, -1, 1};
-    static int[] diy = {-1, 1, 0, 0,};
-    static class Position{
-        int x, y;
-
-        public Position(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static int[] diy = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         arr = new int[n][m];
         bool = new boolean[n][m];
+        dis = new int[n][m];
+
         for(int i = 0; i < n; i++){
-            String input = br.readLine();
+            char[] ch = br.readLine().toCharArray();
             for(int j = 0; j < m; j++)
-                arr[i][j] = Character.getNumericValue(input.charAt(j));
+                arr[i][j] = Character.getNumericValue(ch[j]);
         }
 
         BFS(0, 0);
-        System.out.println(arr[n - 1][m - 1]);
+        System.out.println(dis[n - 1][m - 1]);
     }
 
-    private static void BFS(int y, int x){
-        Deque<Position> deque = new LinkedList<Position>();
-        deque.addLast(new Position(x, y));
+    private static void BFS(int x, int y){
+        bool[y][x] = true;
+        dis[y][x] = 1;  // 시작 위치의 거리를 1로 설정
 
-        while(!deque.isEmpty()){
-            Position cur = deque.pollFirst();
+        Queue<Coordinate> q = new LinkedList<>();
+        q.add(new Coordinate(x, y));
+        while(!q.isEmpty()){
+            Coordinate tmp = q.poll();
 
             for(int i = 0; i < 4; i++){
-                int nx = cur.x + dix[i];
-                int ny = cur.y + diy[i];
+                int nx = tmp.x + dix[i];
+                int ny = tmp.y + diy[i];
 
                 if(nx >= 0 && ny >= 0 && nx < m && ny < n){
-                    if(arr[ny][nx] == 1 && !bool[ny][nx]){
-                        arr[ny][nx] = arr[cur.y][cur.x] + 1;
+                    if(!bool[ny][nx] && arr[ny][nx] == 1){
                         bool[ny][nx] = true;
-                        deque.addLast(new Position(nx, ny));
+                        q.add(new Coordinate(nx, ny));
+                        dis[ny][nx] = dis[tmp.y][tmp.x] + 1;
                     }
                 }
             }
         }
+    }
+}
+
+class Coordinate {
+    int x, y;
+    public Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
