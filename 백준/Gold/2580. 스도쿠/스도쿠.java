575face307328691fc1420b37c2dcb2ba158c271
euics <pjh575157@gmail.com>
2023-08-09 18:01:51 +0900

    [Gold IV] Title: 스도쿠, Time: 544 ms, Memory: 21524 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,54 +1,56 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[][] board = new int[9][9];
+    static int[][] arr = new int[9][9];
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        for (int y = 0; y < 9; y++) {
-            for (int x = 0; x < 9; x++) {
-                board[y][x] = scanner.nextInt();
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 9; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 9; j++)
+                arr[i][j] = Integer.parseInt(st.nextToken());
         }
-        
+
         sudoku(0, 0);
     }
 
-    public static void sudoku(int y, int x) {
-        if (x == 9) {
+    private static void sudoku(int y, int x){
+        if(x == 9){
             sudoku(y + 1, 0);
             return;
         }
 
-        if (y == 9) {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    System.out.print(board[i][j] + " ");
-                }
+        if(y == 9){
+            for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++)
+                    System.out.print(arr[i][j] + " ");
                 System.out.println();
             }
             System.exit(0);
         }
 
-        if (board[y][x] == 0) {
-            for (int i = 1; i <= 9; i++) {
-                if (isValid(y, x, i)) {
-                    board[y][x] = i;
+        if(arr[y][x] == 0){
+            for(int value = 1; value <= 9; value++){
+                if(isValid(y, x, value)){
+                    arr[y][x] = value;
                     sudoku(y, x + 1);
-                    board[y][x] = 0;
+                    arr[y][x] = 0;
                 }
             }
-        } else {
+        } else
             sudoku(y, x + 1);
-        }
     }
 
-    public static boolean isValid(int y, int x, int value) {
-        for (int i = 0; i < 9; i++) {
-            if (board[y][i] == value) return false;
-            if (board[i][x] == value) return false;
-            if (board[(y / 3) * 3 + i / 3][(x / 3) * 3 + i % 3] == value) return false;
+    private static boolean isValid(int y, int x, int value){
+        for(int i = 0; i < 9; i++){
+            if(arr[y][i] == value)
+                return false;
+            if(arr[i][x] == value)
+                return false;
+            if(arr[(y / 3) * 3 + i / 3][(x / 3) * 3 + i % 3] == value)
+                return false;
         }
         return true;
     }
