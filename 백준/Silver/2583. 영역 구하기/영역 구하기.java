1f8b3fae695d928e78db4c72856694ff449a4e78
euics <pjh575157@gmail.com>
2023-09-05 21:19:48 +0900

    [Silver I] Title: 영역 구하기, Time: 140 ms, Memory: 16212 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -2,63 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m, k, cnt1 = 0, cnt2 = 0;
+    static int n, m, k, area = 0, cnt = 1;
     static int[][] arr;
     static boolean[][] bool;
-    static int[] dix = {-1, 1, 0, 0};
-    static int[] diy = {0, 0, -1, 1};
-    public static void main(String[] args) throws IOException {
+    static int[] dix = {0, 0, -1, 1};
+    static int[] diy = {-1, 1, 0, 0};
+    static ArrayList<Integer> total = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
         arr = new int[m][n];
         bool = new boolean[m][n];
-        k = Integer.parseInt(st.nextToken());
 
         for(int i = 0; i < k; i++){
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int y1 = m - Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = m - Integer.parseInt(st.nextToken());
 
-            for(int a = y1; a < y2; a++){
-                for(int b = x1; b < x2; b++)
-                    arr[a][b] = 1;
-            }
-        }
-
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(!bool[i][j] && arr[i][j] == 0){
-                    cnt2 = 0;
-                    DFS(i, j);
-                    list.add(cnt2);
-                    cnt1++;
+            for(int diy = y2; diy <= y1; diy++){
+                for(int dix = x1; dix <= x2; dix++){
+                    arr[diy][dix] = 1;
                 }
             }
         }
 
-        Collections.sort(list);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(!bool[i][j] && arr[i][j] == 0){
+                    cnt = 1;
+                    area++;
+                    DFS(j, i);
+                    total.add(cnt);
+                }
+            }
+        }
 
-        System.out.println(cnt1);
-        for(int x : list)
+        Collections.sort(total);
+
+        System.out.println(area);
+        for(int x : total)
             System.out.printf("%d ", x);
     }
 
     private static void DFS(int x, int y){
-        bool[x][y] = true;
-        cnt2++;
+        bool[y][x] = true;
 
         for(int i = 0; i < 4; i++){
             int nx = x + dix[i];
             int ny = y + diy[i];
 
-            if(nx >= 0 && ny >=0 && nx < m && ny < n){
-                if(arr[nx][ny] == 0 && !bool[nx][ny])
+            if(nx >= 0 && ny >= 0 && nx < n && ny < m && arr[ny][nx] == 0){
+                if(!bool[ny][nx]){
+                    cnt++;
                     DFS(nx, ny);
+                }
             }
         }
     }
