5a8061582bfbf586c993ef9bcadb37aa47952794
euics <pjh575157@gmail.com>
2023-09-07 11:02:57 +0900

    [Gold V] Title: 톱니바퀴, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -2,88 +2,73 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int gear[][];
-	static int d[]; //기어의 회전 정보를 담고있음
-	static int n,m;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		StringTokenizer st;
-		
-		gear = new int[4][8];
-		
-		for(int i=0; i<4 ;i++) {
-			String s = br.readLine();
-			for(int j=0; j<8; j++) {
-				gear[i][j] = s.charAt(j) - '0';
-			}
-		}
-		
-		int k = Integer.parseInt(br.readLine());
-		
-		while(k-->0) {
-			st = new StringTokenizer(br.readLine());
-			//배열에 담긴 gear는 1부터 시작이아닌 0부터라 -1을 해줌
-			int gearN = Integer.parseInt(st.nextToken()) - 1;
-			int turn = Integer.parseInt(st.nextToken());
-			
-			//톱니의 회전방향 정보를 담음
-			d = new int[4]; 
-			
-			d[gearN] = turn; 
-			checkDir(gearN);
-			gearTurn();	
-		}
-		
-		int ans =0;
-		if(gear[0][0] == 1) ans+=1;
-		if(gear[1][0] == 1) ans+=2;
-		if(gear[2][0] == 1) ans+=4;
-		if(gear[3][0] == 1) ans+=8;
-		System.out.println(ans);
-	}
-
-	static void checkDir(int gearN){
-		//좌측 톱니 회전 방향 검사
-		for(int i=gearN-1; i>=0; i--) {
-			if(gear[i][2] != gear[i+1][6]) {
-				d[i] = -d[i+1];
-			}else {
-				//회전을 하지않으면 다음 톱니도 회전을 하지 않게 된다.  
-				break;
-			}
-		}
-		//우측 톱니 회전 방향 검사
-		for(int i=gearN+1; i<4; i++) {
-			if(gear[i][6] != gear[i-1][2]) {
-				d[i] = -d[i-1];
-			}else {
-				//회전을 하지않으면 다음 톱니도 회전을 하지 않게 된다.  
-				break;
-			}
-		}	
-	}
-	
-	static void gearTurn() {
-		int temp = 0;
-		
-		for(int i=0; i<4; i++) { //모든 톱니에 대해서
-			//시계방향 회전
-			if(d[i] == 1) {
-				temp = gear[i][7];
-				for(int j=7; j>0; j--) {
-					gear[i][j] = gear[i][j-1];
-				}
-				gear[i][0] = temp;
-			}
-			//반시계방향회전
-			if(d[i] == -1) {
-				temp = gear[i][0];
-				for(int j=0; j<7; j++) {
-					gear[i][j] = gear[i][j+1];
-				}
-				gear[i][7] = temp;
-			}
-		}
-	}
+    static int[][] gears = new int[4][8];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for (int i = 0; i < 4; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                gears[i][j] = input.charAt(j) - '0';
+            }
+        }
+        
+        int K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int gearNum = Integer.parseInt(st.nextToken()) - 1; // 0-based index
+            int direction = Integer.parseInt(st.nextToken());
+            
+            rotate(gearNum, direction);
+        }
+        
+        int score = 0;
+        for (int i = 0; i < 4; i++) {
+            if (gears[i][0] == 1) {
+                score += (int)Math.pow(2, i);
+            }
+        }
+        
+        System.out.println(score);
+    }
+    
+    public static void rotate(int gearNum, int direction) {
+        int[] directions = new int[4]; // store rotation direction for each gear
+        directions[gearNum] = direction;
+        
+        // check left gears
+        for (int i = gearNum; i > 0; i--) {
+            if (gears[i][6] != gears[i - 1][2]) {
+                directions[i - 1] = -directions[i];
+            } else {
+                break;
+            }
+        }
+        
+        // check right gears
+        for (int i = gearNum; i < 3; i++) {
+            if (gears[i][2] != gears[i + 1][6]) {
+                directions[i + 1] = -directions[i];
+            } else {
+                break;
+            }
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            if (directions[i] == 1) {
+                int temp = gears[i][7];
+                for (int j = 7; j > 0; j--) {
+                    gears[i][j] = gears[i][j - 1];
+                }
+                gears[i][0] = temp;
+            } else if (directions[i] == -1) {
+                int temp = gears[i][0];
+                for (int j = 0; j < 7; j++) {
+                    gears[i][j] = gears[i][j + 1];
+                }
+                gears[i][7] = temp;
+            }
+        }
+    }
 }
