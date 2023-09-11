160b09b84e9a83d43758c343009da8e54386f14e
euics <pjh575157@gmail.com>
2023-09-11 15:13:08 +0900

    [Silver II] Title: 스타트와 링크, Time: 340 ms, Memory: 15688 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,62 +1,63 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, min = Integer.MAX_VALUE;
+    static int n;
     static int[][] arr;
-    static int[] combination;
     static boolean[] bool;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        arr = new int[n + 1][n + 1];
-        bool = new boolean[n + 1];
-        combination = new int[n / 2];
+        arr = new int[n][n];
+        bool = new boolean[n];
 
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= n; j++)
+            for(int j = 0; j < n; j++)
                 arr[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        Combination(0, 1);
+        combination(0, 0);
 
-        System.out.println(min);
+        System.out.println(minDiff);
     }
 
-    private static void Combination(int L, int s){
+    private static void combination(int L, int s){
         if(L == n / 2){
-            int sum1 = 0;
-            int sum2 = 0;
-
-            for(int i = 0; i < n / 2; i++)
-                bool[combination[i]] = true;
-
-            int[] not_Combination = new int[n / 2];
-            int index = 0;
-            for(int i = 1; i <= n; i++){
-                if(!bool[i])
-                    not_Combination[index++] = i;
-            }
-
-            Arrays.fill(bool, false);
-
-            for(int i = 0; i < n / 2 - 1; i++) { // 1
-                for(int j = i + 1; j < n / 2; j++) { // 2
-                    sum1 += arr[combination[i]][combination[j]] + arr[combination[j]][combination[i]];
-                    sum2 += arr[not_Combination[i]][not_Combination[j]] + arr[not_Combination[j]][not_Combination[i]];
-                }
-            }
-
-            min = Math.min(min, Math.abs(sum1 - sum2));
-
+            calculateDiff();
+            return;
         } else{
-            for(int i = s; i <= n; i++) {
-                combination[L] = i;
-                Combination(L + 1, i + 1);
+            for(int i = s; i < n; i++){
+                bool[i] = true;
+                combination(L + 1, i + 1);
+                bool[i] = false;
             }
         }
+    }
+
+    static int minDiff = Integer.MAX_VALUE;
+
+    private static void calculateDiff(){
+        int start = 0;
+        int link = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(i == j)
+                    continue;
+
+                if(bool[i] && bool[j]){
+                    start += arr[i][j];
+                    start += arr[j][i];
+                } else if(!bool[i] && !bool[j]){
+                    link += arr[i][j];
+                    link += arr[j][i];
+                }
+            }
+        }
+
+        minDiff = Math.min(minDiff, Math.abs(start - link));
     }
 }
