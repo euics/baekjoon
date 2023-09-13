4b10e63e605f67c337cfa484094944056be5d5e2
euics <pjh575157@gmail.com>
2023-09-13 15:01:00 +0900

    [Gold IV] Title: 인구 이동, Time: 588 ms, Memory: 297852 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -2,8 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, l, r;
+    static int n, l, r, answer = 0;
     static int[][] arr;
+    static boolean[][] bool;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,16 +20,14 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        int answer = 0;
-
         while(true){
-            boolean[][] bool = new boolean[n][n];
+            bool = new boolean[n][n];
             boolean moved = false;
 
             for(int i = 0; i < n; i++){
                 for(int j = 0; j < n; j++){
                     if(!bool[i][j])
-                        moved = moved | bfs(j, i, bool);
+                        moved = moved | findUnion(j, i);
                 }
             }
 
@@ -44,13 +43,13 @@ public class Main {
     static int[] dix = {0, 0, -1, 1};
     static int[] diy = {-1, 1, 0, 0};
 
-    private static boolean bfs(int x, int y, boolean[][] bool){
+    private static boolean findUnion(int x, int y){
         Queue<int[]> q = new LinkedList<>();
-        ArrayList<int[]> union = new ArrayList<>();
         q.add(new int[]{x, y});
+        ArrayList<int[]> union = new ArrayList<>();
         union.add(new int[]{x, y});
-        int sum = arr[y][x];
         bool[y][x] = true;
+        int sum = arr[y][x];
 
         while(!q.isEmpty()){
             int[] cur = q.poll();
@@ -59,13 +58,15 @@ public class Main {
                 int nx = cur[0] + dix[i];
                 int ny = cur[1] + diy[i];
 
-                if(nx >= 0 && ny >= 0 && nx < n && ny < n && !bool[ny][nx]){
+                if(nx >= 0 && ny >= 0 && nx < n && ny < n){
                     int diff = Math.abs(arr[cur[1]][cur[0]] - arr[ny][nx]);
                     if(diff >= l && diff <= r){
-                        q.add(new int[]{nx, ny});
-                        union.add(new int[]{nx, ny});
-                        sum += arr[ny][nx];
-                        bool[ny][nx] = true;
+                        if(!bool[ny][nx]){
+                            bool[ny][nx] = true;
+                            q.add(new int[]{nx, ny});
+                            union.add(new int[]{nx, ny});
+                            sum += arr[ny][nx];
+                        }
                     }
                 }
             }
