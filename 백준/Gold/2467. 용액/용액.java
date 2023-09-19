c38d61b7d6d9a5ce0cb578f192ae9d922a2990f6
euics <pjh575157@gmail.com>
2023-09-19 22:29:57 +0900

    [Gold V] Title: 용액, Time: 356 ms, Memory: 28788 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -3,33 +3,34 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(br.readLine());
-        long[] arr = new long[n];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        long[] arr = new long[n];
+        long[] answer = new long[2];
+
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++)
             arr[i] = Long.parseLong(st.nextToken());
 
-        int p1 = 0, p2 = n - 1;
-        long res1 = 0, res2 = 0;
+        int left = 0, right = n - 1;
         long min = Long.MAX_VALUE;
-        while(p1 < p2){
-            long curSum = arr[p1] + arr[p2];
-            if(min > Math.abs(curSum)){
-                min = Math.abs(curSum);
-                res1 = arr[p1];
-                res2 = arr[p2];
+        while(left < right){
+            long sum = arr[left] + arr[right];
+
+            if(Math.abs(sum) < min){
+                min = Math.abs(sum);
+                answer[0] = arr[left];
+                answer[1] = arr[right];
             }
 
-            if(curSum > 0)
-                p2--;
+            if(sum > 0)
+                right--;
             else
-                p1++;
+                left++;
         }
 
-        System.out.printf("%d %d", res1, res2);
+        System.out.printf("%d %d", answer[0], answer[1]);
     }
 }
