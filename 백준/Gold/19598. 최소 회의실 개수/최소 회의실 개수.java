6b52934a4387c7fdb3f25c0b52a9537fd9ab8967
euics <pjh575157@gmail.com>
2023-09-21 22:21:47 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 712 ms, Memory: 50372 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -1,48 +1,47 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static class Meeting implements Comparable<Meeting> {
-        int start, end;
 
-        Meeting(int start, int end) {
-            this.start = start;
-            this.end = end;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        Conference[] arr = new Conference[n];
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i] = new Conference(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        @Override
-        public int compareTo(Meeting o) {
-            if (this.start == o.start) return this.end - o.end;
-            return this.start - o.start;
+        Arrays.sort(arr);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(arr[0].end);
+        for(int i = 1; i < n; i++){
+            if(!pq.isEmpty()){
+                if(arr[i].start >= pq.peek())
+                    pq.poll();
+                pq.add(arr[i].end);
+            }
         }
+
+        System.out.println(pq.size());
+    }
+}
+
+class Conference implements Comparable<Conference>{
+    int start, end;
+    public Conference(int start, int end){
+        this.start = start;
+        this.end = end;
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Meeting[] meetings = new Meeting[N];
-
-        for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            int start = Integer.parseInt(input[0]);
-            int end = Integer.parseInt(input[1]);
-            meetings[i] = new Meeting(start, end);
-        }
-
-        Arrays.sort(meetings);
-
-        PriorityQueue<Integer> rooms = new PriorityQueue<>();
-        rooms.offer(meetings[0].end);
-
-        for (int i = 1; i < N; i++) {
-            if (meetings[i].start >= rooms.peek()) {
-                rooms.poll();
-            }
-            rooms.offer(meetings[i].end);
-        }
-
-        System.out.println(rooms.size());
+    @Override
+    public int compareTo(Conference o){
+        if(this.start == o.start)
+            return this.end - o.end;
+        else
+            return this.start - o.start;
     }
 }
