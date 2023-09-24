a65d7aaf88b410b75a78be03e82ec9c825b44703
euics <pjh575157@gmail.com>
2023-09-24 17:03:13 +0900

    [Gold IV] Title: 뱀, Time: 152 ms, Memory: 14920 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Main {
     static int n, k, l;
     static int[][] arr;
-    static HashMap<Integer, Character> map = new HashMap<>();
-    static ArrayList<int[]> snake = new ArrayList<>();
-    static int[] dix = {1, 0, -1, 0};
+    static List<int[]> snake = new ArrayList<>();
+    static HashMap<Integer, String> map = new HashMap<>();
+    static int[] dix = {1, 0, -1, 0}; // 동 남 서 북
     static int[] diy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException{
@@ -18,17 +18,17 @@ public class Main {
 
         for(int i = 0; i < k; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            arr[y - 1][x - 1] = 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            arr[y][x] = 1;
         }
 
         l = Integer.parseInt(br.readLine());
         for(int i = 0; i < l; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int time = Integer.parseInt(st.nextToken());
-            char ch = st.nextToken().charAt(0);
-            map.put(time, ch);
+            String direction = st.nextToken();
+            map.put(time, direction);
         }
 
         solution();
@@ -39,24 +39,38 @@ public class Main {
         snake.add(new int[]{0, 0});
 
         while(true){
+            // 시간 재기
             time++;
 
+            // 뱀 이동
             int nx = x + dix[d];
             int ny = y + diy[d];
 
-            if(finish(nx, ny))
+            // 범위를 벗어나거나, 뱀 몸통을 만날 때 종료
+            boolean flag = false;
+            if(nx < 0 || ny < 0 || nx >= n || ny >= n)
+                flag = true;
+            for(int i = 0; i < snake.size(); i++){
+                int[] o = snake.get(i);
+                if(o[0] == nx && o[1] == ny)
+                    flag = true;
+            }
+
+            if(flag)
                 break;
 
+            // 사과가 있을 때, 없을 때 처리
             if(arr[ny][nx] == 1){
-                snake.add(new int[]{nx, ny});
                 arr[ny][nx] = 0;
+                snake.add(new int[]{nx, ny});
             } else {
                 snake.add(new int[]{nx, ny});
                 snake.remove(0);
             }
 
+            // 방향을 바꿔주는 시간을 만날 때, 방향 변경
             if(map.containsKey(time)){
-                if(map.get(time) == 'D')
+                if(map.get(time).equals("D"))
                     d = (d + 1) % 4;
                 else
                     d = (d + 3) % 4;
@@ -67,19 +81,5 @@ public class Main {
         }
 
         System.out.println(time);
-    }
-
-    private static boolean finish(int nx, int ny){
-        if(nx < 0 || ny < 0 || nx >= n || ny >= n)
-            return true;
-
-        for(int i = 0; i < snake.size(); i++){
-            int[] o = snake.get(i);
-
-            if(o[0] == nx && o[1] == ny)
-                return true;
-        }
-
-        return false;
     }
 }
