d3333a15bfee4db156934ba0934c7c78ea4e5576
euics <pjh575157@gmail.com>
2023-10-18 15:19:12 +0900

    [Gold V] Title: 로봇 청소기, Time: 132 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -2,65 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m, r, c, d, answer = 0;
+    static int n, m, r, c ,dir, cnt = 1;
     static int[][] arr;
-    static int[] dix = {-1, 0, 1, 0};
-    static int[] diy = {0, 1, 0, -1};
-    static boolean[][] visited;
 
-    public static void main(String[] args) throws IOException {
+    static int[] dix = {0, 1, 0, -1}; // 북 동 남 서
+    static int[] diy = {-1, 0, 1, 0};
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
+        arr = new int[n][m];
         st = new StringTokenizer(br.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
-        visited = new boolean[n][m];
-        
-        for (int i = 0; i < n; i++) {
+        dir = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for(int j = 0; j < m; j++)
                 arr[i][j] = Integer.parseInt(st.nextToken());
-            }
         }
-        
-        dfs(r, c, d);
-        
-        System.out.println(answer);
+
+        DFS(c, r, dir);
+        System.out.println(cnt);
     }
 
-    private static void dfs(int x, int y, int dir) {
-        if (!visited[x][y] && arr[x][y] == 0) {
-            visited[x][y] = true;
-            answer++;
-        }
-        
-        boolean cleaned = false;
-        for (int i = 0; i < 4; i++) {
-            dir = (dir + 3) % 4;
-            int nx = x + dix[dir];
-            int ny = y + diy[dir];
-            
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && arr[nx][ny] == 0) {
-                cleaned = true;
-                dfs(nx, ny, dir);
+    private static void DFS(int x, int y, int direction){
+        arr[y][x] = -1;
+
+        for(int i = 0; i < 4; i++){
+            direction = (direction + 3) % 4;
+
+            int nx = x + dix[direction];
+            int ny = y + diy[direction];
+            if(nx >= 0 && ny >= 0 && nx < m && ny < n && arr[ny][nx] == 0){
+                cnt++;
+                DFS(nx, ny, direction);
                 return;
             }
         }
 
-        if (!cleaned) {
-            int backDir = (dir + 2) % 4;
-            int nx = x + dix[backDir];
-            int ny = y + diy[backDir];
-            
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m && arr[nx][ny] != 1) {
-                dfs(nx, ny, dir);
-            }
-        }
+        int back = (direction + 2) % 4;
+        int bx = x + dix[back];
+        int by = y + diy[back];
+        if(bx >= 0 && by >= 0 && bx < m && by < n && arr[by][bx] != 1)
+            DFS(bx, by, direction);
     }
 }
