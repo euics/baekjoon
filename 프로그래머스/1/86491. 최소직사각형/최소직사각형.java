34ca2629351e40bd575c5c2530979ec8d5c36214
euics <pjh575157@gmail.com>
2023-11-02 17:17:19 +0900

    [level 1] Title: 최소직사각형, Time: 2.26 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,22 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        List<int[]> list = new ArrayList<>();
-        
+        int row = 0, col = 0;
         for(int i = 0; i < sizes.length; i++){
-            int max = Math.max(sizes[i][0], sizes[i][1]);
-            int min = Math.min(sizes[i][0], sizes[i][1]);
-            
-            list.add(new int[]{max, min});
+            row = Math.max(row, Math.max(sizes[i][0], sizes[i][1]));
+            col = Math.max(col, Math.min(sizes[i][0], sizes[i][1]));
         }
         
-        int maxRow = Integer.MIN_VALUE;
-        int maxCol = Integer.MIN_VALUE;
-        for(int i = 0; i < list.size(); i++){
-            maxRow = Math.max(maxRow, list.get(i)[0]);
-            maxCol = Math.max(maxCol, list.get(i)[1]);
-        }
-        
-        return maxRow * maxCol;
+        return row * col;
     }
 }
