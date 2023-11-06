ca4eeb15da7939a682ccdb6e0e78ac5bbf65d7fe
euics <pjh575157@gmail.com>
2023-11-06 16:33:15 +0900

    [level 2] Title: 귤 고르기, Time: 65.18 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < tangerine.length; i++){
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int key : map.keySet()){
+            pq.add(map.get(key));
+        }
+        
+        int cnt = 0, sum = 0;
+        while(!pq.isEmpty()){
+            int cur = pq.poll();
+            sum += cur;
+            cnt++;
+            
+            if(sum >= k){
+                break;
+            }
+        }
+        
+        return cnt;
+    }
+}
