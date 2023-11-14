d8364993e680018f757ad7d7dbdb4daf9c65145e
euics <pjh575157@gmail.com>
2023-11-14 19:01:31 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,20 @@
 import java.util.*;
 
 class Solution {
-
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
+        int n = clothes.length;
         
-        for(int i = 0; i < clothes.length; i++){
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
         int answer = 1;
-        for(int value : map.values()){
+        for(String key : map.keySet()){
+            int value = map.get(key);
             answer *= (value + 1);
         }
         
         return answer - 1;
-    
     }
 }
