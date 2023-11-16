2e3a07c028ae28e1c9d647e14b792652ba9fbc22
euics <pjh575157@gmail.com>
2023-11-16 15:53:22 +0900

    [level 2] Title: 피로도, Time: 0.74 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,50 @@
 import java.util.*;
 
 class Solution {
-    
-    static int max = Integer.MIN_VALUE;
+    static int[] pm;
     static boolean[] bool;
     
     public int solution(int k, int[][] dungeons) {
+        Arrays.sort(dungeons, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        pm = new int[dungeons.length];
         bool = new boolean[dungeons.length];
-        DFS(dungeons, k, 0);
+        permutation(0, dungeons, k);
         
         return max;
     }
     
-    public void DFS(int[][] dungeons, int curK, int cnt){
+    static int max = Integer.MIN_VALUE;
+    
+    public void permutation(int L, int[][] dungeons, int k){
+        if(L == dungeons.length){
+            max = Math.max(max, cntMove(dungeons, k));
+            return;
+        }
+        
         for(int i = 0; i < dungeons.length; i++){
-            if(!bool[i] && dungeons[i][0] <= curK){
+            if(!bool[i]){
                 bool[i] = true;
-                DFS(dungeons, curK - dungeons[i][1], cnt + 1);
+                pm[L] = i;
+                permutation(L + 1, dungeons, k);
                 bool[i] = false;
             }
         }
+    }
+    
+    public int cntMove(int[][] dungeons, int k){
+        int tmp = k, cnt = 0;
+        for(int i = 0; i < pm.length; i++){
+            int index = pm[i];
+            
+            int minTired = dungeons[index][0];
+            int useTired = dungeons[index][1];
+            
+            if(tmp >= minTired){
+                tmp -= useTired;
+                cnt++;
+            } else break;
+        }
         
-        max = Math.max(max, cnt);
+        return cnt;
     }
 }
