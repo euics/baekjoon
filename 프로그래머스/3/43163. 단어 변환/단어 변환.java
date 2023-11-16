8744beb45d66bead94747223827aeef2405f05af
euics <pjh575157@gmail.com>
2023-11-16 15:22:07 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        if(!Arrays.asList(words).contains(target)) return 0;
+        
+        Queue<String> q = new LinkedList<>();
+        Set<String> set = new HashSet<>();
+        q.add(begin);
+        
+        int L = 0;
+        while(!q.isEmpty()){
+            int length = q.size();
+            
+            for(int i = 0; i < length; i++){
+                String cur = q.poll();
+                
+                if(cur.equals(target)) return L;
+                
+                for(String word : words){
+                    if(!set.contains(word) && canTransform(cur, word)){
+                        set.add(word);
+                        q.add(word);
+                    }
+                }
+            }
+            
+            L++;
+        }
+        
+        return 0;
+    }
+    
+    public boolean canTransform(String a, String b){
+        int diffCnt = 0;
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)) diffCnt++;
+            
+            if(diffCnt > 1) return false;
+        }
+        
+        return diffCnt == 1;
+    }
+}
