a49cca2242c2083dd9d217234154f39284972124
euics <pjh575157@gmail.com>
2023-12-12 12:28:30 +0900

    [level 2] Title: 피로도, Time: 0.10 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,48 +3,48 @@ import java.util.*;
 class Solution {
     static int[] pm;
     static boolean[] bool;
-    
     public int solution(int k, int[][] dungeons) {
-        Arrays.sort(dungeons, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        pm = new int[dungeons.length];
-        bool = new boolean[dungeons.length];
-        permutation(0, dungeons, k);
-        
+        int n = dungeons.length;
+        pm = new int[n];
+        bool = new boolean[n];
+
+        permutation(0, n, k, dungeons);
+
         return max;
     }
-    
+
     static int max = Integer.MIN_VALUE;
-    
-    public void permutation(int L, int[][] dungeons, int k){
-        if(L == dungeons.length){
-            max = Math.max(max, cntMove(dungeons, k));
+
+    public void permutation(int L, int n, int k, int[][] dungeons){
+        if(L == n){
+            findMax(k, dungeons);
+
             return;
         }
-        
-        for(int i = 0; i < dungeons.length; i++){
+
+        for(int i = 0; i < n; i++){
             if(!bool[i]){
-                bool[i] = true;
                 pm[L] = i;
-                permutation(L + 1, dungeons, k);
+                bool[i] = true;
+                permutation(L + 1, n, k, dungeons);
                 bool[i] = false;
             }
         }
     }
-    
-    public int cntMove(int[][] dungeons, int k){
-        int tmp = k, cnt = 0;
-        for(int i = 0; i < pm.length; i++){
-            int index = pm[i];
-            
-            int minTired = dungeons[index][0];
-            int useTired = dungeons[index][1];
-            
-            if(tmp >= minTired){
-                tmp -= useTired;
+
+    public void findMax(int k, int[][] dungeons){
+        int cnt = 0;
+        int tmp = k;
+
+        for(int index : pm) {
+            if(tmp >= dungeons[index][0]){
+                tmp -= dungeons[index][1];
                 cnt++;
-            } else break;
+            } else {
+                break;
+            }
         }
-        
-        return cnt;
+
+        max = Math.max(max, cnt);
     }
 }
