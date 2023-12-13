f8e41887d34dbe7b824c55abc2420c00d77a6927
euics <pjh575157@gmail.com>
2023-12-13 17:37:40 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,29 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] arr = new int[n + 1];
+        int[] arr = new int[n];
         Arrays.fill(arr, 1);
-        
-        for(int i = 0; i < lost.length; i++) arr[lost[i]] -= 1;
-        for(int i = 0; i < reserve.length; i++) arr[reserve[i]] += 1;
-        
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < lost.length; i++) arr[lost[i] - 1]--;
+        for(int i = 0; i < reserve.length; i++) arr[reserve[i] - 1]++;
+
+        for(int i = 0; i < n; i++){
             if(arr[i] == 0){
-                if(i > 1 && arr[i - 1] > 1){
+                if(i - 1 >= 0 && arr[i - 1] > 1){
                     arr[i]++;
                     arr[i - 1]--;
-                } else if(i < n && arr[i + 1] > 1){
+                } else if(i + 1 < n && arr[i + 1] > 1){
                     arr[i]++;
                     arr[i + 1]--;
                 }
             }
         }
-        
+
         int answer = 0;
-        for(int i = 1; i <= n; i++){
-            if(arr[i] > 0) answer++;
+        for(int i = 0; i < n; i++){
+            if(arr[i] >= 1) answer++;
         }
-        
+
         return answer;
     }
 }
