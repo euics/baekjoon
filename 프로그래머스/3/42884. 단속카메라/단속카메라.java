74bd46d08c0f42174dc0347e80581e0d511a9a08
euics <pjh575157@gmail.com>
2023-12-15 14:48:16 +0900

    [level 3] Title: 단속카메라, Time: 6.52 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,17 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         Arrays.sort(routes, (a, b) -> a[1] - b[1]);
-        
+
         int answer = 0;
-        int cameraPosition = Integer.MIN_VALUE;
-        
-        for(int[] route : routes){
-            if(route[0] > cameraPosition){
+        for(int i = 0, finishTime = Integer.MIN_VALUE; i < routes.length; i++){
+            if(routes[i][0] > finishTime) {
+                finishTime = routes[i][1];
                 answer++;
-                cameraPosition = route[1];
             }
         }
-        
+
         return answer;
     }
 }
