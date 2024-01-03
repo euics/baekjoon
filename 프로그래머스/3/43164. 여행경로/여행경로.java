468be3cf9d148462205957129b64627d8c71f504
euics <pjh575157@gmail.com>
2024-01-03 19:50:49 +0900

    [level 3] Title: 여행경로, Time: 1.92 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -4,24 +4,22 @@ class Solution {
     public String[] solution(String[][] tickets) {
         LinkedList<String> answer = new LinkedList<>();
         Map<String, PriorityQueue<String>> flights = new HashMap<>();
-        
+
         for(String[] ticket : tickets){
-            flights.putIfAbsent(ticket[0], new PriorityQueue<String>());
+            flights.putIfAbsent(ticket[0], new PriorityQueue<>());
             flights.get(ticket[0]).add(ticket[1]);
         }
-        
+
         DFS("ICN", flights, answer);
-        
+
         return answer.stream().map(i -> i).toArray(String[]::new);
     }
-    
+
     public void DFS(String departure, Map<String, PriorityQueue<String>> flights, LinkedList<String> answer){
         PriorityQueue<String> arrivals = flights.get(departure);
-        
-        while(arrivals != null && !arrivals.isEmpty()){
-            DFS(arrivals.poll(), flights, answer);
-        }
-        
+
+        while(arrivals != null && !arrivals.isEmpty()) DFS(arrivals.poll(), flights, answer);
+
         answer.addFirst(departure);
     }
 }
