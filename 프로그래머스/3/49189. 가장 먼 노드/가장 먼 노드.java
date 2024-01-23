db31074a5c13adbed5cfa631bdf0f7939123bf1e
euics <pjh575157@gmail.com>
2024-01-23 18:22:20 +0900

    [level 3] Title: 가장 먼 노드, Time: 38.28 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,65 +1,64 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<ArrayList<Node>> graph;
     static int[] dis;
-    
+    static ArrayList<ArrayList<Edge>> graph;
+
     public int solution(int n, int[][] edge) {
         dis = new int[n + 1];
         Arrays.fill(dis, Integer.MAX_VALUE);
-        graph = new ArrayList<ArrayList<Node>>();
-        for(int i = 0; i <= n; i++) graph.add(new ArrayList<Node>());
-        
-        for(int i = 0; i < edge.length; i++) {
-            graph.get(edge[i][0]).add(new Node(edge[i][1], 1));
-            graph.get(edge[i][1]).add(new Node(edge[i][0], 1));
+        graph = new ArrayList<>();
+        for(int i = 0; i <= n; i++) graph.add(new ArrayList<Edge>());
+
+        for(int i = 0; i < edge.length; i++){
+            graph.get(edge[i][0]).add(new Edge(edge[i][1], 1));
+            graph.get(edge[i][1]).add(new Edge(edge[i][0], 1));
         }
-        
+
         dijkstra(1);
-        
+
         int max = Integer.MIN_VALUE;
-        for(int i = 1; i <= n; i++) {
-            if(max < dis[i] && dis[i] != Integer.MAX_VALUE)
-                max = dis[i];
-        }
-        
+        for(int i = 1; i <= n; i++) max = Math.max(max, dis[i]);
+
+        System.out.println(max);
+
         int answer = 0;
         for(int i = 1; i <= n; i++){
-            if(dis[i] == max) answer++;
+            if(max == dis[i]) answer++;
         }
-        
+
         return answer;
     }
-    
+
     public void dijkstra(int v){
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(v, 0));
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
         dis[v] = 0;
-        
+        pq.add(new Edge(v, 0));
+
         while(!pq.isEmpty()){
-            Node cur = pq.poll();
-            
+            Edge cur = pq.poll();
+
             if(dis[cur.v] < cur.c) continue;
-            
-            for(Node o : graph.get(cur.v)){
+
+            for(Edge o : graph.get(cur.v)){
                 if(dis[o.v] > cur.c + o.c){
                     dis[o.v] = cur.c + o.c;
-                    pq.add(new Node(o.v, cur.c + o.c));
+                    pq.add(new Edge(o.v, cur.c + o.c));
                 }
             }
         }
     }
 }
 
-class Node implements Comparable<Node>{
+class Edge implements Comparable<Edge>{
     int v, c;
-    public Node(int v, int c){
+    public Edge(int v, int c){
         this.v = v;
         this.c = c;
     }
-    
+
     @Override
-    public int compareTo(Node o){
+    public int compareTo(Edge o){
         return this.c - o.c;
-    } 
+    }
 }
