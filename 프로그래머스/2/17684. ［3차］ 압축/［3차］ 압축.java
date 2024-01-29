494d4ce4a1f8ee774dcec81bfeb47d2ed27b1cb8
euics <pjh575157@gmail.com>
2024-01-29 17:35:46 +0900

    [level 2] Title: [3차] 압축, Time: 18.34 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,24 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        Map<String, Integer> map = new HashMap<>();
         List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < 26; i++) map.put(String.valueOf((char) ('A' + i)), i + 1);
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < 26; i++)
+            map.put(String.valueOf((char) ('A' + i)), i + 1);
 
         int idx = 0;
         while(idx < msg.length()){
-            StringBuilder w = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
 
             while(idx < msg.length()){
-                if(!map.containsKey(w.toString() + msg.charAt(idx))) break;
-                else w.append(msg.charAt(idx));
+                if(!map.containsKey(sb.toString() + msg.charAt(idx))) break;
+                else sb.append(msg.charAt(idx));
 
                 idx++;
             }
 
-            answer.add(map.get(w.toString()));
+            answer.add(map.get(sb.toString()));
 
-            if(idx < msg.length()) map.put(w.append(msg.charAt(idx)).toString(), map.size() + 1);
+            if(idx < msg.length()) map.put(sb.toString() + msg.charAt(idx), map.size() + 1);
         }
 
         return answer.stream().mapToInt(i -> i).toArray();
