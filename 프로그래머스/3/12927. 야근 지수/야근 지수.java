82dd30738cc4875560cf428beb243c45e8a93809
euics <pjh575157@gmail.com>
2024-02-01 19:47:14 +0900

    [level 3] Title: 야근 지수, Time: 128.46 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int work : works) pq.add(work);
+
+        while(!pq.isEmpty()){
+            if(n == 0) break;
+
+            int cur = pq.poll() - 1;
+            if(cur != 0) pq.add(cur);
+            n--;
+        }
+
+        long answer = 0;
+        for(int x : pq) answer += Math.pow(x, 2);
+
+        return answer;
+    }
+}
