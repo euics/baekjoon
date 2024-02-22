d57402fea5bf81f188f0b79b505bccc519a63dff
euics <pjh575157@gmail.com>
2024-02-22 11:24:09 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 59.78 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String[] solution(String[] files) {
+        Arrays.sort(files, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] file1 = splitFile(o1);
+                String[] file2 = splitFile(o2);
+
+                int headCompare = file1[0].compareToIgnoreCase(file2[0]);
+                if (headCompare == 0) {
+                    int number1 = Integer.parseInt(file1[1]);
+                    int number2 = Integer.parseInt(file2[1]);
+                    return Integer.compare(number1, number2);
+                }
+                return headCompare;
+            }
+
+            private String[] splitFile(String file) {
+                String head = file.split("[0-9]")[0];
+                String number = file.substring(head.length()).replaceAll("[^0-9]", " ").trim().split(" ")[0];
+                return new String[]{head, number};
+            }
+        });
+        return files;
+    }
+}
