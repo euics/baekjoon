4a08924abf11eefb6f49e17361b88d6aec80c992
euics <pjh575157@gmail.com>
2024-02-22 14:24:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 225.22 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,21 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        Map<String, Integer> info = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) info.put(participant[i], info.getOrDefault(participant[i], 0) + 1);
+
+        for(int i = 0; i < completion.length; i++) {
+            if(info.containsKey(completion[i])) {
+                info.put(completion[i], info.get(completion[i]) - 1);
+
+                if(info.get(completion[i]) == 0) info.remove(completion[i]);
+            }
         }
-        
-        for(int i = 0; i < completion.length; i++){
-            map.put(completion[i], map.get(completion[i]) - 1);
-            if(map.get(completion[i]) == 0)
-                map.remove(completion[i]);
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for(String key : map.keySet())
-            sb.append(key);
-        
-        return sb.toString();
+
+        String answer = "";
+        for(String keySet : info.keySet()) answer = keySet;
+
+        return answer;
     }
 }
