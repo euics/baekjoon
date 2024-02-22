929c41bc6e207f79b4c59078daa31885f727ab1d
euics <pjh575157@gmail.com>
2024-02-22 14:40:56 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,14 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> clothesInfo = new HashMap<>();
-        for(String[] str : clothes){
-            String cloth = str[0];
-            String clothType = str[1];
-            
-            clothesInfo.put(clothType, clothesInfo.getOrDefault(clothType, 0) + 1);
-        }
-        
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < clothes.length; i++) map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+
         int answer = 1;
-        for(String keySet : clothesInfo.keySet()){
-            answer *= (clothesInfo.get(keySet) + 1);
-        }
-        
+        for(String key : map.keySet()) answer *= (map.get(key) + 1);
+
         return answer - 1;
     }
 }
