d47f37877ef1e4a01687453669bd9d097d6abe64
euics <pjh575157@gmail.com>
2024-02-22 14:09:28 +0900

    [level 1] Title: 폰켓몬, Time: 5.62 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,11 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int solution(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        if(nums.length / 2 > map.keySet().size())
-            return map.size();
-        else
-            return nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) set.add(num);
+
+        return Math.min(set.size(), nums.length / 2);
     }
 }
