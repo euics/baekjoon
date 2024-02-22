c768fd1ce507f0176ba10f387a77a7a1f1976664
euics <pjh575157@gmail.com>
2024-02-22 14:53:41 +0900

    [level 3] Title: 베스트앨범, Time: 6.62 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,44 +2,43 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int n = genres.length;
         Map<String, Integer> genresSum = new HashMap<>();
-        Map<String, PriorityQueue<SongInfo>> genresInfo = new HashMap<>();
-
-        for(int i = 0; i < n; i++){
+        Map<String, PriorityQueue<SongInfo>> genreInfo = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
             genresSum.put(genres[i], genresSum.getOrDefault(genres[i], 0) + plays[i]);
-            genresInfo.putIfAbsent(genres[i], new PriorityQueue<SongInfo>());
-            genresInfo.get(genres[i]).add(new SongInfo(i, plays[i]));
+
+            genreInfo.putIfAbsent(genres[i], new PriorityQueue<SongInfo>());
+            genreInfo.get(genres[i]).add(new SongInfo(i, plays[i]));
         }
 
-        List<String> genresRanking = new LinkedList<>(genresSum.keySet());
-        Collections.sort(genresRanking, (a, b) -> genresSum.get(b).compareTo(genresSum.get(a)));
-
+        List<String> genresSumList = new ArrayList<>(genresSum.keySet());
+        Collections.sort(genresSumList, (a, b) -> genresSum.get(b).compareTo(genresSum.get(a)));
         List<Integer> answer = new ArrayList<>();
-        for(String genre : genresRanking){
+        
+        for (String list : genresSumList) {
             int cnt = 0;
-            while(!genresInfo.get(genre).isEmpty() && cnt < 2){
-                answer.add(genresInfo.get(genre).poll().id);
+            PriorityQueue<SongInfo> pq = genreInfo.get(list);
+            while (!pq.isEmpty() && cnt < 2) {
+                answer.add(pq.poll().id);
                 cnt++;
             }
         }
-
+        
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
 
-class SongInfo implements Comparable<SongInfo>{
+class SongInfo implements Comparable<SongInfo> {
     int id, play;
-    public SongInfo(int id, int play){
+
+    public SongInfo(int id, int play) {
         this.id = id;
         this.play = play;
     }
 
     @Override
-    public int compareTo(SongInfo o){
-        if(this.play == o.play)
-            return this.id - o.id;
-        else
-            return o.play - this.play;
+    public int compareTo(SongInfo o) {
+        if (this.play == o.play) return this.id - o.id;
+        else return o.play - this.play;
     }
 }
