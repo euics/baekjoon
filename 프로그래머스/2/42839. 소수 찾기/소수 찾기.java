5287c6b26f07b769d7b1162a8a8921028993df54
euics <pjh575157@gmail.com>
2024-02-22 12:54:41 +0900

    [level 2] Title: 소수 찾기, Time: 15.51 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -10,23 +10,28 @@ class Solution {
     }
 
     public void permutation(String prefix, String str, Set<Integer> permutation) {
-        if (!prefix.equals("")) permutation.add(Integer.parseInt(prefix));
+        if(!prefix.equals("")) permutation.add(Integer.parseInt(prefix));
 
         int n = str.length();
 
-        for (int i = 0; i < n; i++)
+        for(int i = 0; i < n; i++)
             permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), permutation);
     }
-
+    
     public boolean isPrime(int number) {
         if(number <= 1) return false;
         if(number == 2) return true;
         if(number % 2 == 0) return false;
-
-        for(int i = 3; i * i <= number; i += 2) {
+        
+        for(int i = 3; i * i <= number; i += 2){
             if(number % i == 0) return false;
         }
-
+        
         return true;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution("011"));
     }
 }
