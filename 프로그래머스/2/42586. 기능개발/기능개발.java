d23b6484369c0905b6de25ba0143b6775eaa9f10
euics <pjh575157@gmail.com>
2024-02-22 15:36:44 +0900

    [level 2] Title: 기능개발, Time: 2.05 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,30 +1,30 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> days = new LinkedList<>();
-        for(int i = 0; i < progresses.length; i++){
-            int leftProgress = 100 - progresses[i];
-            
-            if(leftProgress % speeds[i] == 0)
-                days.add(leftProgress / speeds[i]);
-            else
-                days.add(leftProgress / speeds[i] + 1);
+        for (int i = 0; i < progresses.length; i++) {
+            int day = (100 - progresses[i]) / speeds[i];
+            if ((100 - progresses[i]) % speeds[i] != 0) day += 1;
+            days.add(day);
         }
-        
-        ArrayList<Integer> answer = new ArrayList<>();
-        while(!days.isEmpty()){
+
+        List<Integer> answer = new ArrayList<>();
+        while (!days.isEmpty()) {
             int cnt = 1;
             int cur = days.poll();
-            
-            while(!days.isEmpty() && days.peek() <= cur){
+
+            while (!days.isEmpty() && days.peek() <= cur) {
                 days.poll();
                 cnt++;
             }
-            
+
             answer.add(cnt);
         }
-        
+
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
