89a950d785a1f41362976fbb593deaa0630e6874
euics <pjh575157@gmail.com>
2024-02-22 15:52:06 +0900

    [level 2] Title: 프로세스, Time: 1.15 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,31 +1,36 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> q = new LinkedList<>();
-        for(int i = 0; i < priorities.length; i++)
-            q.add(new int[]{i, priorities[i]});
-        
-        ArrayList<Integer> answer = new ArrayList<>();
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            
-            boolean bool = false;
-            for(int i = 0; i < q.size(); i++){
-                int[] checkPriority = q.poll();
-                
-                if(checkPriority[1] > cur[1])
-                    bool = true;
-                
-                q.add(checkPriority);
-            }
-            
-            if(bool)
-                q.add(cur);
-            else
-                answer.add(cur[0]);
+        int[] ranks = new int[priorities.length];
+        Queue<Process> q = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new Process(i, priorities[i]));
+            pq.add(priorities[i]);
         }
-        
-        return answer.indexOf(location) + 1;
+
+        int rank = 1;
+        while (!q.isEmpty()) {
+            Process cur = q.poll();
+            if (pq.peek() == cur.priority) {
+                ranks[cur.id] = rank++;
+                pq.poll();
+            } else q.add(cur);
+        }
+
+        return ranks[location];
+    }
+}
+
+class Process {
+    int id, priority;
+
+    public Process(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
     }
 }
