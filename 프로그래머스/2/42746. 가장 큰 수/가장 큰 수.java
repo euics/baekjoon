3d8cb97de1e5ea561e3e2e0f5bde76f593a18594
euics <pjh575157@gmail.com>
2024-02-28 15:00:43 +0900

    [level 2] Title: 가장 큰 수, Time: 2.38 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,21 +3,20 @@ import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] str = new String[numbers.length];
-        for(int i = 0; i < numbers.length; i++) str[i] = String.valueOf(numbers[i]);
-        
-        Arrays.sort(str, new Comparator<String>() {
+        String[] tmp = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) tmp[i] = String.valueOf(numbers[i]);
+
+        Arrays.sort(tmp, new Comparator<String>(){
             @Override
-            public int compare(String o1, String o2){
+            public int compare(String o1, String o2) {
                 return (o2 + o1).compareTo(o1 + o2);
             }
         });
-        
+
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < str.length; i++) sb.append(str[i]);
+        for(String str : tmp) sb.append(str);
         
         if(sb.toString().startsWith("0")) return "0";
-        
-        return sb.toString();
+        else return sb.toString();
     }
 }
