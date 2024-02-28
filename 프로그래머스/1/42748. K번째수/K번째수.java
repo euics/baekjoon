d272e08037320f1e1a33b39b4f4db2f993c67219
euics <pjh575157@gmail.com>
2024-02-28 14:52:59 +0900

    [level 1] Title: K번째수, Time: 3.30 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         List<Integer> answer = new ArrayList<>();
-        
-        for(int i = 0; i < commands.length; i++){
-            
-            int[] tmp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            Arrays.sort(tmp);
-            
-            answer.add(tmp[commands[i][2] - 1]);
+
+        for(int d = 0; d < commands.length; d++) {
+            int i = commands[d][0] - 1;
+            int j = commands[d][1] - 1;
+            int k = commands[d][2] - 1;
+
+            List<Integer> list = new ArrayList<>();
+            for(int start = i; start <= j; start++) list.add(array[start]);
+            Collections.sort(list);
+
+            answer.add(list.get(k));
         }
         
         return answer.stream().mapToInt(i -> i).toArray();
