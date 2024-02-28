a24dbee10485245942afa75d5f953a3fb89bfd5b
euics <pjh575157@gmail.com>
2024-02-28 16:11:52 +0900

    [level 1] Title: 모의고사, Time: 2.84 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,45 +1,34 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
-        
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        for(int i = 0; i < answers.length; i++){
+        int[] first = {1, 2, 3, 4, 5};
+        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] scores = new int[3];
+
+        int i = 0;
+        for(int answer : answers) {
+            if(answer == first[i % 5]) scores[0]++;
+            if(answer == second[i % 8]) scores[1]++;
+            if(answer == third[i % 10]) scores[2]++;
             
-            if(one[i % 5] == answers[i])
-                cnt1++;
-            if(two[i % 8] == answers[i])
-                cnt2++;
-            if(three[i % 10] == answers[i])
-                cnt3++;
-            
+            i++;
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int score : scores) max = Math.max(max, score);
+
+        List<Integer> answer = new ArrayList<>();
+        for(int d = 0; d < 3; d++) {
+            if(scores[d] == max) answer.add(d + 1);
         }
         
-        int max = Math.max(cnt1, cnt2);
-        max = Math.max(max, cnt3);
+        Collections.sort(answer);
         
-        List<Integer> list = new ArrayList<>();
-        if(max == cnt1)
-            list.add(1);
-        if(max == cnt2)
-            list.add(2);
-        if(max == cnt3)
-            list.add(3);
-        
-        Collections.sort(list);
-        
-        return list.stream().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(num -> num).toArray();
     }
 }
-
-/*
- 1 2 3 4 5
- 
- 2 1 2 3 2 4 2 5
- 
- 3 3 1 1 2 2 4 4 5 5
-*/
