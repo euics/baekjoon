dfcefca7f4b9a41d0905e3ffdb517014caa836e0
euics <pjh575157@gmail.com>
2024-02-29 21:07:14 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.25 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,43 +1,47 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     static int[] unf;
     static int[] groupSize;
 
     public int solution(int n, int[][] wires) {
-        int minDiff = Integer.MAX_VALUE;
+        int diff = Integer.MAX_VALUE;
 
-        for(int i = 0; i < wires.length; i++){
+        for(int i = 0; i < wires.length; i++) {
 
             unf = new int[n + 1];
             groupSize = new int[n + 1];
-            for(int j = 1; j <= n; j++){
-                unf[j] = j;
-                groupSize[j] = 1;
-            }
+            init(n);
 
-            for(int j = 0; j < wires.length; j++){
+            for(int j = 0; j < wires.length; j++) {
+
                 if(i == j) continue;
 
                 union(wires[j][0], wires[j][1]);
             }
 
-            minDiff = Math.min(minDiff, Math.abs(n - 2 * groupSize[find(1)]));
+            diff = Math.min(diff, Math.abs(n - 2 * groupSize[find(1)]));
+
         }
 
-        return minDiff;
+        return diff;
     }
 
-    public int find(int v){
+    private void init(int n) {
+        Arrays.fill(groupSize, 1);
+        for(int i = 1; i <= n; i++) unf[i] = i;
+    }
+
+    private int find(int v) {
         if(v == unf[v]) return unf[v];
         else return unf[v] = find(unf[v]);
     }
 
-    public void union(int a, int b){
+    private void union(int a, int b) {
         int fa = find(a);
         int fb = find(b);
 
-        if(fa != fb){
+        if(fa != fb) {
             unf[fa] = fb;
             groupSize[fb] += groupSize[fa];
         }
