8dd70f151afb7790db03fa22bf86c9db16c8db60
euics <pjh575157@gmail.com>
2024-03-01 15:35:39 +0900

    [level 2] Title: 구명보트, Time: 8.54 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,18 +1,20 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public int solution(int[] people, int limit) {
+        int answer = 0;
+
         Arrays.sort(people);
         int start = 0, end = people.length - 1;
-        
-        int answer = 0;
-        while(start <= end) {
+
+        while (start <= end) {
             if(people[start] + people[end] <= limit) start++;
-            
+
             end--;
             answer++;
         }
-        
+
         return answer;
     }
 }
