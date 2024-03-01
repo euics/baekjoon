59b4992837dd3e2619cf99d6166540910a7c34f0
euics <pjh575157@gmail.com>
2024-03-01 14:46:24 +0900

    [level 3] Title: 섬 연결하기, Time: 0.54 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,60 +1,64 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
-    static int[] unf, groupSize;
+    static int[] unf;
+    static int[] groupSize;
+
     public int solution(int n, int[][] costs) {
         unf = new int[n];
         groupSize = new int[n];
-
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             unf[i] = i;
             groupSize[i] = 1;
         }
 
-        ArrayList<Edge> edge = new ArrayList<>();
-        for(int i = 0; i < costs.length; i++) edge.add(new Edge(costs[i][0], costs[i][1], costs[i][2]));
+        List<Edge> edge = new ArrayList<>();
+        for(int[] cost : costs) edge.add(new Edge(cost[0], cost[1], cost[2]));
         Collections.sort(edge);
-
+        
         int answer = 0;
-        for(int i = 0; i < costs.length; i++){
-            int fv1 = find(edge.get(i).v1);
-            int fv2 = find(edge.get(i).v2);
-
+        for(Edge o : edge) {
+            int fv1 = find(o.v1);
+            int fv2 = find(o.v2);
+            
             if(fv1 != fv2) {
-                union(edge.get(i).v1, edge.get(i).v2);
-                answer += edge.get(i).cost;
+                union(o.v1, o.v2);
+                answer += o.cost;
             }
         }
-
-       return answer; 
+        
+        return answer;
     }
 
-    public int find(int v){
+    private int find(int v) {
         if(v == unf[v]) return unf[v];
         else return unf[v] = find(unf[v]);
     }
 
-    public void union(int a, int b){
+    private void union(int a, int b) {
         int fa = find(a);
         int fb = find(b);
 
-        if(fa != fb){
+        if(fa != fb) {
             unf[fa] = fb;
             groupSize[fb] += groupSize[fa];
         }
     }
 }
 
-class Edge implements Comparable<Edge>{
+class Edge implements Comparable<Edge> {
     int v1, v2, cost;
-    public Edge(int v1, int v2, int cost){
+
+    public Edge(int v1, int v2, int cost) {
         this.v1 = v1;
         this.v2 = v2;
         this.cost = cost;
     }
 
     @Override
-    public int compareTo(Edge o){
+    public int compareTo(Edge o) {
         return this.cost - o.cost;
     }
 }
