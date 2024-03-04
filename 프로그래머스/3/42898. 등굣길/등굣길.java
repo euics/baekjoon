f0d9b06e5b4e41f80b3ce8346e569afa40875436
euics <pjh575157@gmail.com>
2024-03-04 18:13:45 +0900

    [level 3] Title: 등굣길, Time: 1.18 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,21 +1,27 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int MOD = 1_000_000_007;
-        int[][] dp = new int[n + 1][m + 1];
-        for(int[] puddle : puddles) dp[puddle[1]][puddle[0]] = -1;
+        int[][] map = init(m, n, puddles);
 
-        dp[1][1] = 1;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(dp[i][j] == -1 || (i == 1 && j == 1)) continue;
-                
-                if(dp[i][j - 1] != -1) dp[i][j] = (dp[i][j] + dp[i][j - 1]) % MOD;
-                if(dp[i - 1][j] != -1) dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+        map[1][1] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if(map[i][j] == -1 || (i == 1 && j == 1)) continue;
+
+                if(map[i][j - 1] != -1) map[i][j] = (map[i][j] + map[i][j - 1]) % MOD;
+                if(map[i - 1][j] != -1) map[i][j] = (map[i][j] + map[i - 1][j]) % MOD;
             }
         }
         
-        return dp[n][m];
+        return map[n][m];
+    }
+
+    private int[][] init(int m, int n, int[][] puddles) {
+        int[][] map = new int[n + 1][m + 1];
+        for (int[] puddle : puddles) map[puddle[1]][puddle[0]] = -1;
+
+        return map;
     }
 }
