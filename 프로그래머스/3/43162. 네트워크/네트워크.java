3327684e91d7a3af68a70ccd2151378d984c7056
euics <pjh575157@gmail.com>
2024-03-13 17:10:38 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,42 +1,45 @@
 class Solution {
-    static int[] unf, groupSize;
-    
+    public static int[] unf;
+    public static int[] groupSize;
+
     public int solution(int n, int[][] computers) {
         unf = new int[n];
         groupSize = new int[n];
-        for(int i = 0; i < n; i++){
+        init(n);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+
+                if(computers[i][j] == 1) union(i, j);
+            }
+        }
+
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            if(unf[i] == i) answer++;
+        }
+
+        return answer;
+    }
+
+    public void init(int n) {
+        for(int i = 0; i < n; i++) {
             unf[i] = i;
             groupSize[i] = 1;
         }
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i == j) continue;
-                
-                if(computers[i][j] == 1){
-                    union(i, j);
-                }
-            }
-        }
-        
-        int answer = 0;
-        for(int i = 0; i < n; i++){
-            if(unf[i] == i) answer++;
-        }
-        
-        return answer;
     }
-    
-    public int find(int v){
+
+    public int find(int v) {
         if(v == unf[v]) return unf[v];
         else return unf[v] = find(unf[v]);
     }
-    
-    public void union(int a, int b){
+
+    public void union(int a, int b) {
         int fa = find(a);
         int fb = find(b);
-        
-        if(fa != fb){
+
+        if(fa != fb) {
             unf[fa] = fb;
             groupSize[fb] += groupSize[fa];
         }
