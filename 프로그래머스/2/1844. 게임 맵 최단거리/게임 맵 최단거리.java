7be5689e30f7255c3841a577910991f30039fc82
euics <pjh575157@gmail.com>
2024-03-13 17:20:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.64 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,50 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-    static int[][] dis;
-    
-    static boolean[][] bool;
-    
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        BFS(maps);
-        
-        return dis[n - 1][m - 1] + 1 == 1 ? -1 : dis[n - 1][m - 1] + 1;
+        return BFS(maps);
     }
-    
+
     static int[] dix = {0, 0, -1, 1};
     static int[] diy = {-1, 1, 0, 0};
-    
-    public void BFS(int maps[][]){
+    static boolean[][] bool;
+
+    public int BFS(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
+
         bool = new boolean[n][m];
-        dis = new int[n][m];
-        
         Queue<int[]> q = new LinkedList<>();
+
         q.add(new int[]{0, 0});
         bool[0][0] = true;
-        
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            
-            for(int d = 0; d < 4; d++){
-                int nx = cur[0] + dix[d];
-                int ny = cur[1] + diy[d];
-                
-                if(nx >= 0 && ny >= 0 && nx < m && ny < n && maps[ny][nx] == 1){
-                    if(!bool[ny][nx]){
-                        bool[ny][nx] = true;
-                        dis[ny][nx] = dis[cur[1]][cur[0]] + 1;
-                        q.add(new int[]{nx, ny});
+
+        int L = 0;
+        while(!q.isEmpty()) {
+            int length = q.size();
+
+            for(int i = 0; i < length; i++) {
+                int[] cur = q.poll();
+
+                if(cur[0] == m - 1 && cur[1] == n - 1) return L + 1;
+
+                for(int d = 0; d < 4; d++) {
+                    int nx = cur[0] + dix[d];
+                    int ny = cur[1] + diy[d];
+
+                    if(nx >= 0 && ny >= 0 && nx < m && ny < n && maps[ny][nx] == 1) {
+                        if(!bool[ny][nx]) {
+                            bool[ny][nx] = true;
+                            q.add(new int[]{nx, ny});
+                        }
                     }
                 }
             }
+
+            L++;
         }
+
+        return -1;
     }
 }
