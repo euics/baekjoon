ae5298b441cb753ba3793b3b820bde4b3fb16a37
euics <pjh575157@gmail.com>
2024-03-13 16:59:39 +0900

    [level 2] Title: 타겟 넘버, Time: 0.49 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,19 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers, int target) {
         DFS(numbers, target, 0, 0);
+
         return answer;
     }
 
-    static int answer = 0;
+    public static int answer = 0;
 
-    public void DFS(int[] numbers, int target, int L, int sum){
-        if(L == numbers.length){
+    public void DFS(int[] numbers, int target, int L, int sum) {
+        if(L == numbers.length) {
             if(sum == target) answer++;
-            
+
             return;
         }
-        
+
         DFS(numbers, target, L + 1, sum + numbers[L]);
         DFS(numbers, target, L + 1, sum - numbers[L]);
     }
