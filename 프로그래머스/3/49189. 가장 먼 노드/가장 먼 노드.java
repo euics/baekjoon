5453375c3c74ace06a24857fdda33688d0cfba25
euics <pjh575157@gmail.com>
2024-03-27 14:10:25 +0900

    [level 3] Title: 가장 먼 노드, Time: 42.10 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,64 +1,79 @@
 import java.util.*;
 
 class Solution {
-    static int[] dis;
     static ArrayList<ArrayList<Edge>> graph;
+    static int[] dis;
 
     public int solution(int n, int[][] edge) {
-        dis = new int[n + 1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
-        graph = new ArrayList<>();
-        for(int i = 0; i <= n; i++) graph.add(new ArrayList<Edge>());
-
-        for(int i = 0; i < edge.length; i++){
-            graph.get(edge[i][0]).add(new Edge(edge[i][1], 1));
-            graph.get(edge[i][1]).add(new Edge(edge[i][0], 1));
-        }
-
-        dijkstra(1);
-
-        int max = Integer.MIN_VALUE;
-        for(int i = 1; i <= n; i++) max = Math.max(max, dis[i]);
-
-        System.out.println(max);
-
-        int answer = 0;
-        for(int i = 1; i <= n; i++){
-            if(max == dis[i]) answer++;
-        }
-
-        return answer;
+        initDis(n);
+        initGraph(n, edge);
+        dijkstra();
+        
+        return getMaxLengthNode(n, getMaxLength(n));
     }
 
-    public void dijkstra(int v){
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        dis[v] = 0;
-        pq.add(new Edge(v, 0));
+    public void initDis(int n) {
+        dis = new int[n + 1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+    }
 
-        while(!pq.isEmpty()){
-            Edge cur = pq.poll();
+    public void initGraph(int n, int[][] edge) {
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
 
-            if(dis[cur.v] < cur.c) continue;
+        for (int i = 0; i < edge.length; i++) {
+            int v1 = edge[i][0];
+            int v2 = edge[i][1];
 
-            for(Edge o : graph.get(cur.v)){
-                if(dis[o.v] > cur.c + o.c){
+            graph.get(v1).add(new Edge(v2, 1));
+            graph.get(v2).add(new Edge(v1, 1));
+        }
+    }
+
+    public void dijkstra() {
+        Queue<Edge> q = new LinkedList<>();
+        q.add(new Edge(1, 0));
+        dis[1] = 0;
+
+        while (!q.isEmpty()) {
+            Edge cur = q.poll();
+
+            for(Edge o : graph.get(cur.v)) {
+                if(dis[o.v] > cur.c + o.c) {
                     dis[o.v] = cur.c + o.c;
-                    pq.add(new Edge(o.v, cur.c + o.c));
+                    q.add(new Edge(o.v, cur.c + o.c));
                 }
             }
         }
     }
+
+    public int getMaxLength(int n) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i <= n; i++) max = Math.max(max, dis[i]);
+
+        return max;
+    }
+
+    public int getMaxLengthNode(int n, int max) {
+        int cnt = 0;
+        for(int i = 1; i <= n; i++) {
+            if(dis[i] == max) cnt++;
+        }
+        
+        return cnt;
+    }
 }
 
-class Edge implements Comparable<Edge>{
+class Edge implements Comparable<Edge> {
     int v, c;
-    public Edge(int v, int c){
+
+    public Edge(int v, int c) {
         this.v = v;
         this.c = c;
     }
 
     @Override
-    public int compareTo(Edge o){
+    public int compareTo(Edge o) {
         return this.c - o.c;
     }
 }
