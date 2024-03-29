999fdcab4414b06ac26e6e3a005fee08a4b67f00
euics <pjh575157@gmail.com>
2024-03-29 18:58:19 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.16 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,20 +1,20 @@
 class Solution {
     public int solution(int n, int k) {
-        String num = changeNum(n, k);
-        String[] nums = num.split("0");
-
         int answer = 0;
-        for(String tmp : nums) {
-            if(!tmp.equals("") && isPrime(Long.parseLong(tmp))) answer++;
+
+        String[] nums = changeFormation(n, k).split("0");
+
+        for (String num : nums) {
+            if (!num.equals("") && isPrime(Long.parseLong(num))) answer++;
         }
-        
+
         return answer;
     }
 
-    public String changeNum(int n, int k){
+    public String changeFormation(int n, int k) {
         StringBuilder sb = new StringBuilder();
 
-        while(n > 0){
+        while (n > 0) {
             sb.insert(0, n % k);
             n /= k;
         }
@@ -22,15 +22,16 @@ class Solution {
         return sb.toString();
     }
 
-    public boolean isPrime(Long num){
-        if(num <= 1) return false;
-        if(num == 2) return true;
-        if(num % 2 == 0) return false;
+    public boolean isPrime(Long n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
 
-        for(long i = 3; i * i <= num; i += 2){
-            if(num % i == 0) return false;
+        for (long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
         }
 
         return true;
     }
+
 }
