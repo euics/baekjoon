1c63ea45b6a6ffa70629098cbb08ed1259fb4655
euics <pjh575157@gmail.com>
2024-04-01 14:30:30 +0900

    [level 2] Title: [3차] 압축, Time: 16.27 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,28 +1,35 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
+    static Map<String, Integer> dictionary = new HashMap<>();
+    static List<Integer> answer = new ArrayList<>();
+
     public int[] solution(String msg) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < 26; i++)
-            map.put(String.valueOf((char) ('A' + i)), i + 1);
+        initDictionary();
 
         int idx = 0;
-        while(idx < msg.length()){
+        while (idx < msg.length()) {
             StringBuilder sb = new StringBuilder();
 
-            while(idx < msg.length()){
-                if(!map.containsKey(sb.toString() + msg.charAt(idx))) break;
+            while (idx < msg.length()) {
+                if(!dictionary.containsKey(sb.toString() + msg.charAt(idx))) break;
                 else sb.append(msg.charAt(idx));
 
                 idx++;
             }
 
-            answer.add(map.get(sb.toString()));
+            answer.add(dictionary.get(sb.toString()));
 
-            if(idx < msg.length()) map.put(sb.toString() + msg.charAt(idx), map.size() + 1);
+            if(idx < msg.length()) dictionary.put(sb.toString() + msg.charAt(idx), dictionary.size() + 1);
         }
 
         return answer.stream().mapToInt(i -> i).toArray();
+    }
+
+    public void initDictionary() {
+        for (int i = 0; i < 26; i++) dictionary.put(String.valueOf((char) ('A' + i)), i + 1);
     }
 }
