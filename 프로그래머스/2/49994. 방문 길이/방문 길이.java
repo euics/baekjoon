5233ad1b4d86985bed4e3ed5c7659af6feee5271
euics <pjh575157@gmail.com>
2024-04-07 15:00:32 +0900

    [level 2] Title: 방문 길이, Time: 16.34 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -6,23 +6,23 @@ class Solution {
         Set<String> set = new HashSet<>();
         
         int x = 0, y = 0;
-        for (char dir : dirs.toCharArray()) {
-            int nx = x, ny = y;
-
-            if(dir == 'U' && y + 1 <= 5) ny++;
-            else if(dir == 'D' && y - 1 >= -5) ny--;
-            else if(dir == 'L' && x - 1 >= -5) nx--;
-            else if(dir == 'R' && x + 1 <= 5) nx++;
-
-            if(nx == x && ny == y) continue;
+        for(char dir : dirs.toCharArray()) {
+            int prevX = x, prevY = y;
             
-            set.add("" + x + y + nx + ny);
-            set.add("" + nx + ny + x + y);
+            if(dir == 'U' && y + 1 <= 5) prevY++;
+            if(dir == 'D' && y - 1 >= -5) prevY--;
+            if(dir == 'R' && x + 1 <= 5) prevX++;
+            if(dir == 'L' && x - 1 >= -5) prevX--;
             
-            x = nx;
-            y = ny;
+            if(prevX == x && prevY == y) continue;
+            
+            set.add("" + x + y + prevX + prevY);
+            set.add("" + prevX + prevY + x + y);
+            
+            x = prevX;
+            y = prevY;
         }
-
+        
         return set.size() / 2;
     }
 }
