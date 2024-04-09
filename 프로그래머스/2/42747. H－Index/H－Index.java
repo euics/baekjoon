e74d97df706bc44325387fc7d81dbb8b70bd66cd
euics <pjh575157@gmail.com>
2024-04-09 20:19:48 +0900

    [level 2] Title: H-Index, Time: 2.90 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -10,19 +10,19 @@ class Solution {
         int h = tmp[0], max = Integer.MIN_VALUE;
         while(h >= 0) {
             int cnt = 0;
-
+            
             for(int i = 0; i < n; i++) {
                 if(citations[i] >= h) cnt++;
             }
-
+            
             if(cnt >= h) {
                 max = h;
                 break;
             }
-
+            
             h--;
         }
-
+        
         return max;
     }
 }
