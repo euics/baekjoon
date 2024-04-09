bb78e803623c2caa58dadae9c525e85481df20ae
euics <pjh575157@gmail.com>
2024-04-09 14:34:14 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -5,10 +5,9 @@ class Solution {
 
         int share = s / n;
         int remain = s % n;
-        
+
         int[] answer = new int[n];
         for(int i = 0; i < n; i++) answer[i] = share;
-        
         for(int i = n - 1; i >= n - remain; i--) answer[i]++;
         
         return answer;
