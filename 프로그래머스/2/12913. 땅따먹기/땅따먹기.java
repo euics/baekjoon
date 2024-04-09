5642d4478821e998fe84bedf3e5046c092cf95eb
euics <pjh575157@gmail.com>
2024-04-09 21:33:41 +0900

    [level 2] Title: 땅따먹기, Time: 18.80 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,24 +1,24 @@
 class Solution {
     int solution(int[][] land) {
         int n = land.length;
-        int m = land[0].length;
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n][4];
         dp[n - 1] = land[n - 1].clone();
 
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < m; j++) {
-                for(int l = 0; l < m; l++){
-                    if(j == l) continue;
+        for(int row = n - 2; row >= 0; row--) {
+            for(int i = 0; i < 4; i++) {
+                for(int j = 0; j < 4; j++) {
+                    if(i == j) continue;
 
-                    dp[i][j] = Integer.max(dp[i][j], land[i][j] + dp[i + 1][l]);
+                    dp[row][i] = Math.max(dp[row][i], land[row][i] + dp[row + 1][j]);
                 }
             }
+
         }
 
-        int max = Integer.MIN_VALUE;
-        for(int num : dp[0]) max = Integer.max(max, num);
+        int answer = Integer.MIN_VALUE;
+        for(int i = 0; i < 4; i++)  answer = Math.max(answer, dp[0][i]);
 
-        return max;
+        return answer;
     }
 }
