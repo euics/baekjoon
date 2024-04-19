718414c7f8fe8bc2485c1605d0a2ff79be0bd889
euics <pjh575157@gmail.com>
2024-04-19 14:27:06 +0900

    [level 2] Title: 롤케이크 자르기, Time: 193.17 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,35 +1,25 @@
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 class Solution {
-    public int solution(int[] topping) {
+    public int solution(int[] toppings) {
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+
+        for(int topping : toppings) right.put(topping, right.getOrDefault(topping, 0) + 1);
+
         int answer = 0;
-        HashMap<Integer, Integer> left = new HashMap<>();
-        HashMap<Integer, Integer> right = new HashMap<>();
-
-        // 전체 토핑을 right 해시맵에 추가
-        for (int t : topping) {
-            right.put(t, right.getOrDefault(t, 0) + 1);
+        for(int topping : toppings) {
+            left.put(topping, left.getOrDefault(topping, 0) + 1);
+            
+            if(right.get(topping) == 1) right.remove(topping);
+            else right.put(topping, right.get(topping) - 1);
+            
+            if(left.size() == right.size()) answer++;
         }
-
-        // 롤케이크를 순회하면서 토핑을 왼쪽으로 옮김
-        for (int i = 0; i < topping.length; i++) {
-            int currentTopping = topping[i];
-            // 왼쪽 해시맵에 현재 토핑 추가
-            left.put(currentTopping, left.getOrDefault(currentTopping, 0) + 1);
-
-            // 오른쪽 해시맵에서 현재 토핑 제거
-            if (right.get(currentTopping) == 1) {
-                right.remove(currentTopping);
-            } else {
-                right.put(currentTopping, right.get(currentTopping) - 1);
-            }
-
-            // 왼쪽과 오른쪽 해시맵의 크기가 같으면 정답 카운트 증가
-            if (left.size() == right.size()) {
-                answer++;
-            }
-        }
-
+        
         return answer;
     }
 }
