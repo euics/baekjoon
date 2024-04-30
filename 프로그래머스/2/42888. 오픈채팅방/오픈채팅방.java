3786897ef58cff1e004ef0b79f7442c50ab3c807
euics <pjh575157@gmail.com>
2024-04-30 14:41:17 +0900

    [level 2] Title: 오픈채팅방, Time: 188.54 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -6,21 +6,16 @@ import java.util.Map;
 class Solution {
     public String[] solution(String[] records) {
         Map<String, String> map = new HashMap<>();
-
         for(String record : records) {
-            if(record.split(" ")[0].equals("Enter") || record.split(" ")[0].equals("Change")) {
-                String uid = record.split(" ")[1];
-                String name = record.split(" ")[2];
+            if(record.split(" ")[0].equals("Leave")) continue;
 
-                map.put(uid, name);
-            }
+            map.put(record.split(" ")[1], record.split(" ")[2]);
         }
 
         List<String> answer = new ArrayList<>();
-        for(int i = 0; i < records.length; i++) {
-            String uid = records[i].split(" ")[1];
-            if(records[i].split(" ")[0].equals("Enter")) answer.add(map.get(uid) + "님이 들어왔습니다.");
-            if(records[i].split(" ")[0].equals("Leave")) answer.add(map.get(uid) + "님이 나갔습니다.");
+        for(String record : records) {
+            if(record.split(" ")[0].equals("Enter")) answer.add(map.get(record.split(" ")[1]) + "님이 들어왔습니다.");
+            if(record.split(" ")[0].equals("Leave")) answer.add(map.get(record.split(" ")[1]) + "님이 나갔습니다.");
         }
 
         return answer.stream().toArray(String[]::new);
