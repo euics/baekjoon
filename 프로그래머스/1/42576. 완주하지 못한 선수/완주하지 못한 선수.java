2d5917a8a52d0f53713c004ff671385816c2f39a
euics <pjh575157@gmail.com>
2024-04-30 14:31:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.47 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,20 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = initMap(participant);
-        countCompletion(map, completion);
-
-        return new ArrayList<>(map.keySet()).get(0);
-    }
-
-    public Map<String, Integer> initMap(String[] participants) {
+    public String solution(String[] participants, String[] completions) {
         Map<String, Integer> map = new HashMap<>();
         for(String participant : participants) map.put(participant, map.getOrDefault(participant, 0) + 1);
 
-        return map;
-    }
-
-    public void countCompletion(Map<String, Integer> map, String[] completions) {
         for(String completion : completions) {
-            if(map.containsKey(completion)) map.put(completion, map.get(completion) - 1);
+            map.put(completion, map.get(completion) - 1);
+
             if(map.get(completion) == 0) map.remove(completion);
         }
+
+        List<String> answer = new LinkedList<>(map.keySet());
+        return answer.get(0);
     }
 }
