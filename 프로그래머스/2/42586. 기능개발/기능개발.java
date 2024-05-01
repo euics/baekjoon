987f4cc90418b4cac29c907cb77c4c1084417295
euics <pjh575157@gmail.com>
2024-05-01 12:56:05 +0900

    [level 2] Title: 기능개발, Time: 2.08 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,35 +1,30 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> remainDays = new LinkedList<>();
-        int n = progresses.length;
-
-        for(int i = 0; i < n; i++) {
-            int days = 0;
-
-            if((100 - progresses[i]) % speeds[i] != 0) days = ((100 - progresses[i]) / speeds[i]) + 1;
-            else days = (100 - progresses[i]) / speeds[i];
-
-            remainDays.add(days);
-        }
-
-        List<Integer> answer = new ArrayList<>();
-        while(!remainDays.isEmpty()) {
-            int prevDays = remainDays.poll();
-            int cnt = 1;
-            
-            while(!remainDays.isEmpty() && prevDays >= remainDays.peek()) {
-                remainDays.poll();
-                cnt++;
+        int[] dueDates = new int[progresses.length];
+        for(int i = 0; i < progresses.length; i++) {
+            int dueDate = 0;
+            if((100 - progresses[i]) % speeds[i] == 0) {
+                dueDate = (100 - progresses[i]) / speeds[i];
+            } else {
+                dueDate = (100 - progresses[i]) / speeds[i] + 1;
             }
-            
-            answer.add(cnt);
+            dueDates[i] = dueDate;
         }
-
+        
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < dueDates.length; i++) {
+            int dueDate = dueDates[i];
+            int cnt = 0;
+            while(i < dueDates.length && dueDate >= dueDates[i]) {
+                cnt++;
+                i++;
+            }
+            answer.add(cnt);
+            i--;
+        }
+        
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
