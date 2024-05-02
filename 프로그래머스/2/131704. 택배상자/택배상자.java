e315187ad3a351fe823c511493e8dc95b8b38852
euics <pjh575157@gmail.com>
2024-05-02 10:25:11 +0900

    [level 2] Title: 택배상자, Time: 43.93 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,18 +3,18 @@ import java.util.Stack;
 class Solution {
     public int solution(int[] order) {
         Stack<Integer> subContainer = new Stack<>();
-        int cnt = 0, nextBox = 1;
+        int answer = 0, nextBox = 1;
 
         for(int target : order) {
 
-            while(nextBox <= target) subContainer.add(nextBox++);
+            while (nextBox <= target) subContainer.push(nextBox++);
 
             if(subContainer.peek() == target) {
+                answer++;
                 subContainer.pop();
-                cnt++;
             } else break;
         }
-
-        return cnt;
+        
+        return answer;
     }
 }
