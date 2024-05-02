89e4c1af380ac99883be3e1ba75497d92204a27a
euics <pjh575157@gmail.com>
2024-05-02 11:46:37 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.31 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,38 +1,28 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        for(int i = 0; i < operations.length; i++) {
-            String order = operations[i].split(" ")[0];
-
-            switch (order) {
+        List<Integer> heap = new ArrayList<>();
+        
+        for(String operation : operations) {
+            String order = operation.split(" ")[0];
+            
+            switch(order) {
                 case "I":
-                    int num = Integer.parseInt(operations[i].split(" ")[1]);
-                    maxHeap.add(num);
-                    minHeap.add(num);
+                    heap.add(Integer.parseInt(operation.split(" ")[1]));
                     break;
                 case "D":
-                    int x = Integer.parseInt(operations[i].split(" ")[1]);
-                    if(x == 1) {
-                        if(!maxHeap.isEmpty() && !minHeap.isEmpty()) {
-                            int max = maxHeap.poll();
-                            minHeap.remove(max);
-                        }
-                    } else if(x == -1){
-                        if(!maxHeap.isEmpty() && !minHeap.isEmpty()) {
-                            int min = minHeap.poll();
-                            maxHeap.remove(min);
-                        }
+                    if(heap.size() != 0) {
+                        if(Integer.parseInt(operation.split(" ")[1]) == 1) heap.remove(heap.size() - 1);
+                        else heap.remove(0);
+                        
+                        break;
                     }
-                    break;
             }
+            
+            Collections.sort(heap);
         }
-
-        if(maxHeap.isEmpty() && minHeap.isEmpty()) return new int[]{0, 0};
-        else return new int[]{maxHeap.poll(), minHeap.poll()};
+        
+        return heap.size() == 0 ? new int[]{0, 0} : new int[]{heap.get(heap.size() - 1), heap.get(0)};
     }
 }
