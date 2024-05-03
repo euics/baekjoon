9174ba11089982e04b6d689302123b8c64a800ff
euics <pjh575157@gmail.com>
2024-05-03 14:19:23 +0900

    [level 2] Title: 가장 큰 수, Time: 2.26 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,20 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] str = new String[numbers.length];
-        for(int i = 0; i < numbers.length; i++) str[i] = String.valueOf(numbers[i]);
-
-        Arrays.sort(str, new Comparator<String>(){
+        String[] number = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) number[i] = String.valueOf(numbers[i]);
+        
+        Arrays.sort(number, new Comparator<String>(){
             @Override
-            public int compare(String s1, String s2) {
-                return (s2 + s1).compareTo(s1 + s2);
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
             }
         });
-
+        
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < str.length; i++) sb.append(str[i]);
-
-        if(sb.toString().startsWith("0")) return "0";
-        else return sb.toString();
+        for(int i = 0; i < number.length; i++) sb.append(number[i]);
+        
+        return sb.toString().startsWith("0") ? "0" : sb.toString();
     }
 }
