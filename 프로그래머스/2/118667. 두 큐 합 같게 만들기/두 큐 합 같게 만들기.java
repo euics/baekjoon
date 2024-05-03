6b32e5e8b20b6e98fa3f2e760d2dd82860aceba3
euics <pjh575157@gmail.com>
2024-05-03 13:46:46 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 69.77 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -3,20 +3,20 @@ import java.util.Queue;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
-
         Queue<Integer> q1 = initQueue(queue1);
+        long sum1 = sumQueue(queue1);
+
         Queue<Integer> q2 = initQueue(queue2);
-        long sum1 = findSum(queue1);
-        long sum2 = findSum(queue2);
+        long sum2 = sumQueue(queue2);
 
-        if ((sum1 + sum2) % 2 != 0) return -1;
+        if((sum1 + sum2) % 2 != 0) return -1;
 
-        while (true) {
-            if (answer > (q1.size() + q2.size()) * 2) return -1;
+        int answer = 0;
+        while(true) {
+            if(answer > (q1.size() + q2.size()) * 2) return -1;
 
-            if (sum1 == sum2) break;
-            else if (sum1 < sum2) {
+            if(sum1 == sum2) return answer;
+            else if(sum1 < sum2) {
                 sum1 += q2.peek();
                 sum2 -= q2.peek();
                 q1.add(q2.poll());
@@ -25,23 +25,21 @@ class Solution {
                 sum2 += q1.peek();
                 q2.add(q1.poll());
             }
-            
+
             answer++;
         }
-        
-        return answer;
     }
 
     public Queue<Integer> initQueue(int[] queue) {
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < queue.length; i++) q.add(queue[i]);
+        for(int i = 0; i < queue.length; i++) q.add(queue[i]);
 
         return q;
     }
 
-    public long findSum(int[] queue) {
+    public long sumQueue(int[] queue) {
         int sum = 0;
-        for (int i = 0; i < queue.length; i++) sum += queue[i];
+        for(int i = 0; i < queue.length; i++) sum += queue[i];
 
         return sum;
     }
