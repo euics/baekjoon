3ed2fdd537949b7247d298298b71f29737c5a9a0
euics <pjh575157@gmail.com>
2024-05-06 14:45:08 +0900

    [level 1] Title: 모의고사, Time: 2.74 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,34 +1,27 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
+    static int[] first = new int[]{1, 2, 3, 4, 5}; // 5개
+    static int[] second = new int[]{2, 1, 2, 3, 2, 4, 2, 5}; // 8개
+    static int[] thrid = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10개
+    
     public int[] solution(int[] answers) {
-        int[] first = {1, 2, 3, 4, 5};
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] scores = new int[3];
-
-        int i = 0;
-        for(int answer : answers) {
-            if(answer == first[i % 5]) scores[0]++;
-            if(answer == second[i % 8]) scores[1]++;
-            if(answer == third[i % 10]) scores[2]++;
-            
-            i++;
-        }
-
-        int max = Integer.MIN_VALUE;
-        for(int score : scores) max = Math.max(max, score);
-
-        List<Integer> answer = new ArrayList<>();
-        for(int d = 0; d < 3; d++) {
-            if(scores[d] == max) answer.add(d + 1);
+        int[] peoples = new int[3];
+        
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == first[i % 5]) peoples[0]++;
+            if(answers[i] == second[i % 8]) peoples[1]++;
+            if(answers[i] == thrid[i % 10]) peoples[2]++;
         }
         
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < 3; i++) max = Math.max(max, peoples[i]);
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < 3; i++) {
+            if(max == peoples[i]) answer.add(i + 1);
+        }
         Collections.sort(answer);
         
-        return answer.stream().mapToInt(num -> num).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
