9186f8c5ae8c119187b69f1fff1e4ebe90243842
euics <pjh575157@gmail.com>
2024-05-06 12:34:52 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.08 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int n) {
+        return change(n, 3);
+    }
+
+    public String change(long num, long formation) {
+        StringBuilder sb = new StringBuilder();
+        while(num > 0) {
+            long remain = num % formation;
+            num /= formation;
+
+            if(remain == 0) {
+                remain = 4;
+                num -= 1;
+            }
+
+            sb.insert(0, remain);
+        }
+
+        return sb.toString();
+    }
+}
