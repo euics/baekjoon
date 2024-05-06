5a3e8aaed0ab250d47fb0f857054c6bec25a35a5
euics <pjh575157@gmail.com>
2024-05-06 16:16:51 +0900

    [Silver II] Title: 이동하기, Time: 520 ms, Memory: 78672 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] arr, tmp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,25 +9,25 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][m];
-        for(int i = 0; i < n; i++) {
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        tmp = new int[n][m];
-        tmp[0][0] = arr[0][0];
-        for(int i = 1; i < n; i++) tmp[i][0] = tmp[i - 1][0] + arr[i][0];
-        for(int j = 1; j < m; j++) tmp[0][j] = tmp[0][j - 1] + arr[0][j];
+        int[][] dp = new int[n][m];
+        dp[0][0] = arr[0][0];
+        for (int i = 1; i < n; i++) dp[i][0] = dp[i - 1][0] + arr[i][0];
+        for (int j = 1; j < m; j++) dp[0][j] = dp[0][j - 1] + arr[0][j];
 
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j < m; j++) {
-                tmp[i][j] = Math.max(Math.max(tmp[i - 1][j], tmp[i][j - 1]), tmp[i - 1][j - 1]) + arr[i][j];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = Math.max(Math.max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + arr[i][j];
             }
         }
 
-        System.out.println(tmp[n - 1][m - 1]);
+        System.out.println(dp[n - 1][m - 1]);
     }
 }
