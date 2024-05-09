4ac3bd41dab1d01c547614cb167284ced914e7da
euics <pjh575157@gmail.com>
2024-05-09 15:59:41 +0900

    [level 2] Title: 모음 사전, Time: 3.05 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,31 @@
 import java.util.*;
 
 class Solution {
+    static String vowels = "AEIOU";
+    static List<String> dictionary = new ArrayList<>();
+    
     public int solution(String word) {
-        for(int length = 1; length <= 5; length++) setDictionary(0, length, word, "AEIOU", "");
-
-        List<String> list = new ArrayList<>(set);
-        Collections.sort(list);
-
-        return list.indexOf(word) + 1;
+        int answer = 0;
+        
+        for(int length = 1; length <= 5; length++) {
+            char[] permutation = new char[length];
+            permutation(0, length, permutation);
+        }
+        
+        Collections.sort(dictionary);
+        return dictionary.indexOf(word) + 1;
     }
-
-    static Set<String> set = new HashSet<>();
-
-    private void setDictionary(int L, int length, String word, String vowel, String str) {
+    
+    public void permutation(int L, int length, char[] permutation) {
         if(L == length) {
-            set.add(str);
-
+            dictionary.add(new String(permutation));
+            
             return;
         }
-
-        for(int i = 0; i < vowel.length(); i++) setDictionary(L + 1, length, word, vowel, str + vowel.charAt(i));
+        
+        for(int i = 0; i < vowels.length(); i++) {
+            permutation[L] = vowels.charAt(i);
+            permutation(L + 1, length, permutation);
+        }
     }
 }
