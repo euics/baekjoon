5a19f96247b3fdf16b430c4d03b5273cb7784a86
euics <pjh575157@gmail.com>
2024-05-21 16:04:01 +0900

    [level 3] Title: 단어 변환, Time: 0.22 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,43 +2,48 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        if(!Arrays.asList(words).contains(target)) return 0;
-
+        return BFS(begin, target, words);
+    }
+    
+    public int BFS(String begin, String target, String[] words) {
+        List<String> wordList = new ArrayList<>(Arrays.asList(words));
+        
+        boolean[] bool = new boolean[wordList.size()];
         Queue<String> q = new LinkedList<>();
-        Set<String> set = new HashSet<>();
         q.add(begin);
-
+        
         int L = 0;
         while(!q.isEmpty()) {
             int length = q.size();
-
+            
             for(int i = 0; i < length; i++) {
                 String cur = q.poll();
-
+                
                 if(cur.equals(target)) return L;
-
-                for(String word : words) {
-                    if(!set.contains(word) && canTransform(cur, word)) {
-                        set.add(word);
-                        q.add(word);
+                
+                for(int d = 0; d < wordList.size(); d++) {
+                    if(!bool[d] && compare(cur, wordList.get(d))) {
+                        bool[d] = true;
+                        q.add(wordList.get(d));
                     }
                 }
             }
-
+            
             L++;
         }
         
-        return L;
+        return 0;
     }
-
-    public boolean canTransform(String a, String b) {
-        int diffCnt = 0;
-        for(int i = 0; i < a.length(); i++) {
-            if(a.charAt(i) != b.charAt(i)) diffCnt++;
-
-            if(diffCnt > 1) return false;
+    
+    public boolean compare(String x, String y) {
+        if(x.length() != y.length()) return false;
+        
+        int cnt = 0;
+        for(int i = 0; i < x.length(); i++) {
+            
+            if(x.charAt(i) != y.charAt(i)) cnt++;
         }
-
-        return diffCnt == 1;
+        
+        return cnt == 1 ? true : false;
     }
 }
