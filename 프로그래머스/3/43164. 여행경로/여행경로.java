d4e432d493e9aff285ea0d77ae5852ee23800e6f
euics <pjh575157@gmail.com>
2024-05-21 17:47:42 +0900

    [level 3] Title: 여행경로, Time: 0.43 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,25 +1,33 @@
 import java.util.*;
 
 class Solution {
+    static Map<String, PriorityQueue<String>> graph;
+    static LinkedList<String> answer;
+
     public String[] solution(String[][] tickets) {
-        Deque<String> answer = new LinkedList<>();
-        Map<String, PriorityQueue<String>> flights = new HashMap<>();
+        init(tickets);
+        DFS("ICN");
 
-        for(String[] ticket : tickets) {
-            flights.putIfAbsent(ticket[0], new PriorityQueue<String>());
-            flights.get(ticket[0]).add(ticket[1]);
-        }
-
-        DFS("ICN", flights, answer);
-
-        return answer.stream().map(i -> i).toArray(String[]::new);
+        // LinkedList를 배열로 변환하여 반환 (역순으로 저장되었으므로 뒤집지 않음)
+        return answer.toArray(new String[0]);
     }
 
-    public void DFS(String departure, Map<String, PriorityQueue<String>> flights, Deque<String> answer) {
-        PriorityQueue<String> arrivals = flights.get(departure);
+    public void init(String[][] tickets) {
+        answer = new LinkedList<>();
+        graph = new HashMap<>();
+        for (String[] ticket : tickets) {
+            String pre = ticket[0];
+            String next = ticket[1];
 
-        while(arrivals != null && !arrivals.isEmpty()) DFS(arrivals.poll(), flights, answer);
+            graph.putIfAbsent(pre, new PriorityQueue<>());
+            graph.get(pre).add(next);
+        }
+    }
 
-        answer.addFirst(departure);
+    public void DFS(String pre) {
+        while (graph.containsKey(pre) && !graph.get(pre).isEmpty()) {
+            DFS(graph.get(pre).poll());
+        }
+        answer.addFirst(pre); // LinkedList의 맨 앞에 추가하여 역순으로 저장
     }
 }
