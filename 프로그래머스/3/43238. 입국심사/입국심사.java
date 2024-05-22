0ad22eafc0de25ff9bb0aea866a162a3e174b341
euics <pjh575157@gmail.com>
2024-05-22 14:17:36 +0900

    [level 3] Title: 입국심사, Time: 0.07 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,34 +1,34 @@
 class Solution {
     public long solution(int n, int[] times) {
-        return BinarySearch(n, times);
+        return binarySearch(n, times);
     }
-
-    public long getMax(int[] times) {
-        long max = Integer.MIN_VALUE;
-
-        for (int time : times) max = Math.max(max, time);
-
+    
+    public long maxTime(int[] times) {
+        long max = Long.MIN_VALUE;
+        
+        for(int time : times) max = Math.max(max, time);
+        
         return max;
     }
-
-    public long BinarySearch(int n, int[] times) {
+    
+    public long binarySearch(int n, int[] times) {
         long answer = Long.MAX_VALUE;
-
-        long left = 1;
-        long right = (long) n * getMax(times);
-
-        while (left <= right) {
+        
+        long left = 1, right = (long) n * maxTime(times);
+        while(left <= right) {
             long mid = (left + right) / 2;
             long sum = 0;
-
-            for (int time : times) sum += mid / time;
-
-            if (sum >= n) {
-                right = mid - 1;
+            
+            for(int time : times) sum += mid / time;
+            
+            if(sum >= n) {
                 answer = Math.min(answer, mid);
-            } else left = mid + 1;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-
+        
         return answer;
     }
 }
