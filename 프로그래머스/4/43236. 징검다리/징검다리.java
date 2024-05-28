f2af9c3e309eb6a4954df154e14f3b252e7d24a9
euics <pjh575157@gmail.com>
2024-05-28 18:43:14 +0900

    [level 4] Title: 징검다리, Time: 22.20 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,35 +1,35 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public int solution(int distance, int[] rocks, int n) {
-        Arrays.sort(rocks);
-        return binarySearch(distance, rocks, n);
-    }
 
-    public int binarySearch(int distance, int[] rocks, int n) {
-        int answer = Integer.MIN_VALUE;
+	public int solution(int distance, int[] rocks, int n) {
+		Arrays.sort(rocks);
 
-        int left = 0;
-        int right = distance;
+		int left = 0, right = distance, answer = Integer.MIN_VALUE;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			int remove = 0;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int removedRock = 0;
-            int prev = 0;
+			int pre = 0;
+			for(int i = 0; i < rocks.length; i++) {
+				if(Math.abs(rocks[i] - pre) < mid) remove++;
+				else pre = rocks[i];
+			}
 
-            for (int rock : rocks) {
-                if (rock - prev < mid) removedRock++;
-                else prev = rock;
-            }
+			if(Math.abs(distance - pre) < mid) remove++;
 
-            if (distance - prev < mid) removedRock++;
-
-            if (removedRock <= n) {
-                answer = Math.max(answer, mid);
-                left = mid + 1;
-            } else right = mid - 1;
-        }
-
-        return answer;
-    }
+			if(remove <= n) {
+				answer = Math.max(answer, mid);
+				left = mid + 1;
+			} else right = mid - 1;
+		}
+		
+		return answer;
+	}
 }
+
+/*
+ 0 ~ 2 2 ~ 11 11 ~ 14 14 ~ 17 17 ~ 21 21 ~ 25
+ 2 9 3 3 4 4
+
+*/
