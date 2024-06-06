65841f4db5b8f2a5174e8686f32a6b3244c97db8
euics <pjh575157@gmail.com>
2024-06-06 21:13:15 +0900

    [level 2] Title: 방문 길이, Time: 15.37 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,27 +2,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public int solution(String dirs) {
-        Set<String> set = new HashSet<>();
-        
-        int x = 0, y = 0;
-        for(char dir : dirs.toCharArray()) {
-            int prevX = x, prevY = y;
-            
-            if(dir == 'U' && y + 1 <= 5) prevY++;
-            if(dir == 'D' && y - 1 >= -5) prevY--;
-            if(dir == 'R' && x + 1 <= 5) prevX++;
-            if(dir == 'L' && x - 1 >= -5) prevX--;
-            
-            if(prevX == x && prevY == y) continue;
-            
-            set.add("" + x + y + prevX + prevY);
-            set.add("" + prevX + prevY + x + y);
-            
-            x = prevX;
-            y = prevY;
-        }
-        
-        return set.size() / 2;
-    }
+	public int solution(String dirs) {
+		Set<String> route = new HashSet<>();
+		int x = 0, y = 0;
+
+		for(char dir : dirs.toCharArray()) {
+			int nx = x, ny = y;
+
+			if(dir == 'U' && y + 1 <= 5) ny++;
+			if(dir == 'D' && y - 1 >= -5) ny--;
+			if(dir == 'L' && x - 1 >= -5) nx--;
+			if(dir == 'R' && x + 1 <= 5) nx++;
+			
+			if(nx == x && ny == y) continue;
+			
+			route.add("" + x + y + nx + ny);
+			route.add("" + nx + ny + x + y);
+			
+			x = nx;
+			y = ny;
+		}
+		
+		return route.size() / 2;
+	}
 }
