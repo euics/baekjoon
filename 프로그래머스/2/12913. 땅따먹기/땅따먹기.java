0eee143c6b365dbb969a9fc9bb02ed285b1dd933
euics <pjh575157@gmail.com>
2024-06-07 16:25:41 +0900

    [level 2] Title: 땅따먹기, Time: 20.11 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,24 +1,34 @@
 class Solution {
-    int solution(int[][] land) {
-        int n = land.length;
+	int solution(int[][] land) {
+		int n = land.length;
+		int m = land[0].length;
 
-        int[][] dp = new int[n][4];
-        dp[n - 1] = land[n - 1].clone();
+		int[][] dp = new int[n][m];
+		dp[0] = land[0].clone();
 
-        for(int row = n - 2; row >= 0; row--) {
-            for(int i = 0; i < 4; i++) {
-                for(int j = 0; j < 4; j++) {
-                    if(i == j) continue;
+		for (int row = 1; row < n; row++) {
+			for (int col = 0; col < m; col++) {
 
-                    dp[row][i] = Math.max(dp[row][i], land[row][i] + dp[row + 1][j]);
-                }
-            }
+				int max = Integer.MIN_VALUE;
+				for (int tmp = 0; tmp < m; tmp++) {
 
-        }
+					if (col == tmp)
+						continue;
 
-        int answer = Integer.MIN_VALUE;
-        for(int i = 0; i < 4; i++)  answer = Math.max(answer, dp[0][i]);
+					max = Math.max(max, land[row][col] + dp[row - 1][tmp]);
+				}
 
-        return answer;
-    }
+				dp[row][col] = max;
+			}
+		}
+
+		int answer = Integer.MIN_VALUE;
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < m; col++) {
+				answer = Math.max(answer, dp[row][col]);
+			}
+		}
+
+		return answer;
+	}
 }
