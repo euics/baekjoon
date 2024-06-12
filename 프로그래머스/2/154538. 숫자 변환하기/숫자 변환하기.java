c6e7b744fe4b67e6cd11d603cb8967b4fdce61bc
euics <pjh575157@gmail.com>
2024-06-12 13:51:32 +0900

    [level 2] Title: 숫자 변환하기, Time: 2.13 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,34 +2,38 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-    public int solution(int x, int y, int n) {
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] bool = new boolean[y + 1];
-        q.add(x);
-        int L = 0;
+	public int solution(int x, int y, int n) {
+		return BFS(x, y, n);
+	}
 
-        while(!q.isEmpty()) {
-            int length = q.size();
+	public int BFS(int x, int y, int n) {
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] bool = new boolean[y + 1];
+		q.add(x);
+		bool[x] = true;
 
-            for(int i = 0; i < length; i++) {
-                int cur = q.poll();
-                if(cur == y) return L;
+		int L = 0;
+		while (!q.isEmpty()) {
+			int length = q.size();
 
-                int[] next = new int[]{cur + n, cur * 2, cur * 3};
-                for(int d = 0; d < 3; d++) {
-                    if(next[d] > y || bool[next[d]]) continue;
-                    if(next[d] <= y && !bool[next[d]]) {
-                        q.add(next[d]);
-                        bool[next[d]] = true;
-                    }
-                }
-            }
+			for (int i = 0; i < length; i++) {
+				int cur = q.poll();
 
-            L++;
-        }
+				if (cur == y)
+					return L;
 
-        return -1;
-    }
+				int[] dix = {cur + n, cur * 2, cur * 3};
+				for (int d = 0; d < 3; d++) {
+					if (dix[d] <= y && !bool[dix[d]]) {
+						q.add(dix[d]);
+						bool[dix[d]] = true;
+					}
+				}
+			}
+
+			L++;
+		}
+
+		return -1;
+	}
 }
-
-// +n *2 *3
