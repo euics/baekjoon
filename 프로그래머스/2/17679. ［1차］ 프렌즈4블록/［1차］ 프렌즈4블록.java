15886684353aedfb8fbcfa798b01af64ca94f4d0
euics <pjh575157@gmail.com>
2024-06-12 14:14:08 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.22 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,80 +1,83 @@
 import java.util.Stack;
 
 class Solution {
-    static char[][] arr;
-    static boolean[][] bool;
+	static char[][] arr;
+	static boolean[][] bool;
 
-    public int solution(int m, int n, String[] board) {
-        int answer = 0;
+	public int solution(int m, int n, String[] board) {
+		int answer = 0;
 
-        initArr(m, n, board);
+		arr = new char[m][n];
+		for (int i = 0; i < m; i++)
+			arr[i] = board[i].toCharArray();
 
-        while (true) {
-            bool = new boolean[m][n];
+		while (true) {
+			bool = new boolean[m][n];
 
-            for (int i = 0; i < m - 1; i++) {
-                for (int j = 0; j < n - 1; j++) {
-                    if (arr[i][j] == ' ') continue;
+			for (int row = 0; row < m - 1; row++) {
+				for (int col = 0; col < n - 1; col++) {
+					if (arr[row][col] == ' ')
+						continue;
 
-                    findSameBlock(j, i, arr[i][j]);
-                }
-            }
+					findSameBlocks(row, col, arr[row][col]);
+				}
+			}
 
-            int cnt = countSameBlock(m, n);
-            if (cnt == 0) break;
-            answer += cnt;
+			int cnt = removeBlocks(m, n);
+			answer += cnt;
+			if (cnt == 0)
+				break;
 
-            dropBlock(m, n);
-        }
+			dropBlocks(m, n);
+		}
 
-        return answer;
-    }
+		return answer;
+	}
 
-    public void initArr(int m, int n, String[] board) {
-        arr = new char[m][n];
-        for (int i = 0; i < m; i++) arr[i] = board[i].toCharArray();
-    }
+	public void findSameBlocks(int row, int col, char block) {
+		for (int i = row; i < row + 2; i++) {
+			for (int j = col; j < col + 2; j++) {
+				if (arr[i][j] != block)
+					return;
+			}
+		}
 
-    public void findSameBlock(int x, int y, char ch) {
-        for (int i = y; i <= y + 1; i++) {
-            for (int j = x; j <= x + 1; j++) {
-                if (arr[i][j] != ch) return;
-            }
-        }
+		for (int i = row; i < row + 2; i++) {
+			for (int j = col; j < col + 2; j++) {
+				bool[i][j] = true;
+			}
+		}
+	}
 
-        for (int i = y; i <= y + 1; i++) {
-            for (int j = x; j <= x + 1; j++) {
-                bool[i][j] = true;
-            }
-        }
-    }
+	public int removeBlocks(int m, int n) {
+		int cnt = 0;
 
-    public int countSameBlock(int m, int n) {
-        int cnt = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (bool[i][j]) {
-                    arr[i][j] = ' ';
-                    cnt++;
-                }
-            }
-        }
+		for (int row = 0; row < m; row++) {
+			for (int col = 0; col < n; col++) {
+				if (bool[row][col]) {
+					arr[row][col] = ' ';
+					cnt++;
+				}
+			}
+		}
 
-        return cnt;
-    }
+		return cnt;
+	}
 
-    public void dropBlock(int m, int n) {
-        Stack<Character> stack = new Stack<>();
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m; i++) {
-                if (arr[i][j] != ' ') {
-                    stack.push(arr[i][j]);
-                    arr[i][j] = ' ';
-                }
-            }
+	public void dropBlocks(int m, int n) {
+		for (int col = 0; col < n; col++) {
+			Stack<Character> stack = new Stack<>();
 
-            int index = m - 1;
-            while (!stack.isEmpty()) arr[index--][j] = stack.pop();
-        }
-    }
+			for (int row = 0; row < m; row++) {
+				if (arr[row][col] != ' ') {
+					stack.push(arr[row][col]);
+					arr[row][col] = ' ';
+				}
+			}
+
+			int index = m - 1;
+			while (!stack.isEmpty())
+				arr[index--][col] = stack.pop();
+		}
+	}
 }
