781c589cec7acca7d8d1d6cd9055b6c39ce73a8f
euics <pjh575157@gmail.com>
2024-06-15 15:47:35 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.41 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,13 +1,14 @@
 class Solution {
-    static int MOD = 1_000_000_007;
-    
-    public int solution(int n) {
-        int[] fibo = new int[n + 1];
-        fibo[1] = 1;
-        fibo[2] = 2;
+	static int MOD = 1_000_000_007;
 
-        for(int i = 3; i <= n; i++) fibo[i] = (fibo[i - 1] + fibo[i - 2]) % MOD;
+	public int solution(int n) {
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
 
-        return fibo[n];
-    }
+		for (int i = 3; i <= n; i++)
+			dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+
+		return dp[n];
+	}
 }
