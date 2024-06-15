34563f6e304022842fee947ead85861ce5c4159b
euics <pjh575157@gmail.com>
2024-06-15 16:08:06 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 2.85 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,33 +1,36 @@
 class Solution {
-    public int[] solution(int[][] arr) {
-        quad(arr, 0, 0, arr.length);
+	static int[] answer = new int[2];
 
-        return answer;
-    }
+	public int[] solution(int[][] arr) {
+		quad(arr, 0, 0, arr.length);
 
-    static int[] answer = new int[2];
+		return answer;
+	}
 
-    public void quad(int[][] arr, int x, int y, int size) {
-        if(zip(arr, x, y, size, arr[y][x])) {
-            if(arr[y][x] == 0) answer[0]++;
-            if(arr[y][x] == 1) answer[1]++;
+	public void quad(int[][] arr, int x, int y, int size) {
+		if (zip(arr, x, y, size, arr[y][x])) {
+			if (arr[y][x] == 0)
+				answer[0]++;
+			else
+				answer[1]++;
 
-            return;
-        }
+			return;
+		}
 
-        quad(arr, x, y, size / 2);
-        quad(arr, x, y + size / 2, size / 2);
-        quad(arr, x + size / 2, y, size / 2);
-        quad(arr,x + size / 2, y + size / 2, size / 2);
-    }
+		quad(arr, x, y, size / 2);
+		quad(arr, x + size / 2, y, size / 2);
+		quad(arr, x, y + size / 2, size / 2);
+		quad(arr, x + size / 2, y + size / 2, size / 2);
+	}
 
-    public boolean zip(int[][] arr, int x, int y, int size, int val) {
-        for(int i = y; i < y + size; i++) {
-            for(int j = x; j < x + size; j++) {
-                if(arr[i][j] != val) return false;
-            }
-        }
+	public boolean zip(int[][] arr, int x, int y, int size, int val) {
+		for (int i = y; i < y + size; i++) {
+			for (int j = x; j < x + size; j++) {
+				if (arr[i][j] != val)
+					return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
