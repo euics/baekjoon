a45d0531fe595b852dff71dd1fe9e4fd17078a1a
euics <pjh575157@gmail.com>
2024-06-19 16:43:36 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,28 +1,26 @@
 class Solution {
-    public int[] solution(int[] sequence, int k) {
-        int[] answer = new int[]{-1, -1};
-        
-        int start = 0, end = 0, sum = sequence[0], length = Integer.MAX_VALUE;
-        while (end < sequence.length) {
-            if (sum < k) {
-                end++;
+	public int[] solution(int[] sequence, int k) {
+		int[] answer = new int[] {-1, -1};
+		int start = 0, end = 0, sum = sequence[0], length = Integer.MAX_VALUE;
+		while (end < sequence.length) {
+			if (sum < k) {
+				end++;
 
-                if (end < sequence.length) {
-                    sum += sequence[end];
-                }
-            } else if (sum > k) {
-                sum -= sequence[start++];
-            } else {
-                if (length > end - start + 1) {
-                    length = end - start + 1;
-                    answer[0] = start;
-                    answer[1] = end;
-                }
-                
-                sum -= sequence[start++];
-            }
-        }
-        
-        return answer;
-    }
+				if (end < sequence.length)
+					sum += sequence[end];
+			} else if (sum > k)
+				sum -= sequence[start++];
+			else {
+				if (length > end - start + 1) {
+					length = end - start + 1;
+					answer[0] = start;
+					answer[1] = end;
+				}
+
+				sum -= sequence[start++];
+			}
+		}
+
+		return answer;
+	}
 }
