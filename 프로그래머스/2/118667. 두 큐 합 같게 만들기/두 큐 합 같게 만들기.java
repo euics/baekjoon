3279a3d8264a44ffec5c6630e9712491fe417b24
euics <pjh575157@gmail.com>
2024-06-19 15:19:00 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 79.56 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,45 +2,56 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-    public int solution(int[] queue1, int[] queue2) {
-        Queue<Integer> q1 = initQueue(queue1);
-        long sum1 = sumQueue(queue1);
+	public int solution(int[] queue1, int[] queue2) {
+		int answer = 0;
 
-        Queue<Integer> q2 = initQueue(queue2);
-        long sum2 = sumQueue(queue2);
+		Queue<Integer> q1 = initQueue(queue1);
+		Queue<Integer> q2 = initQueue(queue2);
 
-        if((sum1 + sum2) % 2 != 0) return -1;
+		long sum1 = getSum(q1);
+		long sum2 = getSum(q2);
 
-        int answer = 0;
-        while(true) {
-            if(answer > (q1.size() + q2.size()) * 2) return -1;
+		if ((sum1 + sum2) % 2 != 0)
+			return -1;
 
-            if(sum1 == sum2) return answer;
-            else if(sum1 < sum2) {
-                sum1 += q2.peek();
-                sum2 -= q2.peek();
-                q1.add(q2.poll());
-            } else {
-                sum1 -= q1.peek();
-                sum2 += q1.peek();
-                q2.add(q1.poll());
-            }
+		while (true) {
+			if (answer > (q1.size() + q2.size()) * 2)
+				return -1;
 
-            answer++;
-        }
-    }
+			if (sum1 == sum2)
+				break;
+			else if (sum1 < sum2) {
+				sum1 += q2.peek();
+				sum2 -= q2.peek();
+				q1.add(q2.poll());
+			} else {
+				sum1 -= q1.peek();
+				sum2 += q1.peek();
+				q2.add(q1.poll());
+			}
 
-    public Queue<Integer> initQueue(int[] queue) {
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < queue.length; i++) q.add(queue[i]);
+			answer++;
+		}
 
-        return q;
-    }
+		return answer;
+	}
 
-    public long sumQueue(int[] queue) {
-        int sum = 0;
-        for(int i = 0; i < queue.length; i++) sum += queue[i];
+	public Queue<Integer> initQueue(int[] queues) {
+		Queue<Integer> q = new LinkedList<>();
+		for (int queue : queues)
+			q.add(queue);
 
-        return sum;
-    }
+		return q;
+	}
+
+	public int getSum(Queue<Integer> q) {
+		int sum = 0, length = q.size();
+		for (int i = 0; i < length; i++) {
+			int cur = q.poll();
+			sum += cur;
+			q.add(cur);
+		}
+
+		return sum;
+	}
 }
