f65f18e3f552ffe497bc2108573e1af2d2f49e7b
euics <pjh575157@gmail.com>
2024-06-24 13:37:13 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 55.14 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,33 +1,39 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 class Solution {
-    public int[] solution(String[] gems) {
-        Set<String> types = new HashSet<>();
-        for(String gem : gems) types.add(gem);
+	public int[] solution(String[] gems) {
+		int[] answer = new int[2];
 
-        int start = 0, end = 0, minLength = Integer.MAX_VALUE;
-        int[] answer = new int[2];
-        Map<String, Integer> map = new HashMap<>();
+		Set<String> types = new HashSet<>();
+		for (String gem : gems)
+			types.add(gem);
 
-        while(end < gems.length) {
-            map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
+		int start = 0, end = 0, length = Integer.MAX_VALUE;
+		Map<String, Integer> map = new HashMap<>();
 
-            while(map.size() == types.size()) {
-                if(end - start + 1 < minLength) {
-                    minLength = end - start + 1;
-                    answer[0] = start + 1;
-                    answer[1] = end + 1;
-                }
+		while (end < gems.length) {
+			map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
 
-                map.put(gems[start], map.get(gems[start]) - 1);
-                if(map.get(gems[start]) == 0) map.remove(gems[start]);
+			while (map.size() == types.size()) {
+				if (end - start + 1 < length) {
+					length = end - start + 1;
+					answer[0] = start + 1;
+					answer[1] = end + 1;
+				}
 
-                start++;
-            }
+				map.put(gems[start], map.get(gems[start]) - 1);
+				if (map.get(gems[start]) == 0)
+					map.remove(gems[start]);
 
-            end++;
-        }
+				start++;
+			}
 
-        return answer;
-    }
+			end++;
+		}
+
+		return answer;
+	}
 }
