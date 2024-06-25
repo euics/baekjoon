c5e354c5f0816237ff5bdbe7441e4cb13f20ab73
euics <pjh575157@gmail.com>
2024-06-25 13:10:51 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 48.22 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,48 +1,55 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
-    
-    public String[] solution(String[] orders, int[] course) {
-        for(int i = 0; i < orders.length; i++) {
-            char[] ch = orders[i].toCharArray();
-            Arrays.sort(ch);
-            orders[i] = new String(ch);
-        }
-        
-        Map<String, Integer> map = new HashMap<>();
-        for(String order : orders) permutation(order, "", 0, map);
-        
-        List<String> answer = new ArrayList<>();
-        for(int length : course) {
-            int max = 2;
-            List<String> combination = new ArrayList<>();
-            
-            for(String key : map.keySet()) {
-                
-                if(length == key.length() && map.get(key) >= max) {
-                    if(max < map.get(key)) {
-                        max = map.get(key);
-                        combination.clear();
-                    }
-                    combination.add(key);
-                }
-            }
-            
-            answer.addAll(combination);
-        }
-        
-        Collections.sort(answer);
-        return answer.stream().toArray(String[]::new);
-    }
-    
-    public void permutation(String orders, String str, int index, Map<String, Integer> map) {
-        if(index == orders.length()) {
-            map.put(str, map.getOrDefault(str, 0) + 1);
-            
-            return;
-        }
-        
-        permutation(orders, str + orders.charAt(index), index + 1, map);
-        permutation(orders, str, index + 1, map);
-    }
+	public String[] solution(String[] orders, int[] course) {
+		for (int i = 0; i < orders.length; i++) {
+			char[] order = orders[i].toCharArray();
+			Arrays.sort(order);
+			orders[i] = new String(order);
+		}
+
+		Map<String, Integer> map = new HashMap<>();
+		for (String order : orders)
+			combination(0, order, "", map);
+
+		List<String> answer = new ArrayList<>();
+		for (int len : course) {
+			List<String> candidate = new ArrayList<>();
+			int max = 2;
+
+			for (String combinationKey : map.keySet()) {
+				if (len == combinationKey.length() && max <= map.get(combinationKey)) {
+					if (max < map.get(combinationKey)) {
+						max = map.get(combinationKey);
+						candidate.clear();
+					}
+
+					candidate.add(combinationKey);
+				}
+			}
+
+			answer.addAll(candidate);
+		}
+		
+		Collections.sort(answer);
+
+		return answer.stream().toArray(String[]::new);
+	}
+
+	public void combination(int L, String order, String cur, Map<String, Integer> map) {
+		if (L == order.length()) {
+			if (cur.length() >= 2)
+				map.put(cur, map.getOrDefault(cur, 0) + 1);
+
+			return;
+		}
+
+		combination(L + 1, order, cur + order.charAt(L), map);
+		combination(L + 1, order, cur, map);
+	}
 }
