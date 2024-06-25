fd2f3e3402ce7659ff00923be36d2c03aa97197f
euics <pjh575157@gmail.com>
2024-06-25 15:46:20 +0900

    [level 2] Title: 배달, Time: 1.12 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,64 +1,69 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution {
-    static int[] dis;
-    static ArrayList<ArrayList<Edge>> graph;
+	static ArrayList<ArrayList<Edge>> graph;
+	static int[] dis;
 
-    public int solution(int N, int[][] road, int K) {
-        init(N, road);
-        dijkstra(1);
+	public int solution(int N, int[][] road, int K) {
+		int answer = 0;
 
-        return countAnswer(K);
-    }
+		init(N, road);
+		dijkstra(1);
 
-    public void init(int N, int[][] roads) {
-        dis = new int[N + 1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
+		for (int i = 1; i <= N; i++) {
+			if (dis[i] <= K)
+				answer++;
+		}
 
-        graph = new ArrayList<ArrayList<Edge>>();
-        for(int i = 0; i <= N; i++) graph.add(new ArrayList<Edge>());
-        for(int[] road : roads) {
-            graph.get(road[0]).add(new Edge(road[1], road[2]));
-            graph.get(road[1]).add(new Edge(road[0], road[2]));
-        }
-    }
+		return answer;
+	}
 
-    public void dijkstra(int v) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(v, 0));
-        dis[v] = 0;
+	public void init(int N, int[][] roads) {
+		dis = new int[N + 1];
+		Arrays.fill(dis, Integer.MAX_VALUE);
 
-        while(!pq.isEmpty()) {
-            Edge cur = pq.poll();
+		graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++)
+			graph.add(new ArrayList<Edge>());
+		for (int i = 0; i < roads.length; i++) {
+			graph.get(roads[i][0]).add(new Edge(roads[i][1], roads[i][2]));
+			graph.get(roads[i][1]).add(new Edge(roads[i][0], roads[i][2]));
+		}
+	}
 
-            if(cur.c > dis[cur.v]) continue;
+	public void dijkstra(int v) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(v, 0));
+		dis[v] = 0;
 
-            for(Edge o : graph.get(cur.v)) {
-                if(dis[o.v] > cur.c + o.c) {
-                    dis[o.v] = cur.c + o.c;
-                    pq.add(new Edge(o.v, cur.c + o.c));
-                }
-            }
-        }
-    }
-    
-    public int countAnswer(int K) {
-        int answer = 0;
-        for(int x : dis) if(x <= K) answer++;
+		while (!pq.isEmpty()) {
+			Edge cur = pq.poll();
 
-        return answer;
-    }
+			if (dis[cur.v] < cur.c)
+				continue;
+
+			for (Edge o : graph.get(cur.v)) {
+				if (dis[o.v] > cur.c + o.c) {
+					dis[o.v] = cur.c + o.c;
+					pq.add(new Edge(o.v, cur.c + o.c));
+				}
+			}
+		}
+	}
 }
 
 class Edge implements Comparable<Edge> {
-    int v, c;
-    public Edge(int v, int c) {
-        this.v = v;
-        this.c = c;
-    }
+	int v, c;
 
-    @Override
-    public int compareTo(Edge o) {
-        return this.c - o.c;
-    }
+	public Edge(int v, int c) {
+		this.v = v;
+		this.c = c;
+	}
+
+	@Override
+	public int compareTo(Edge o) {
+		return this.c - o.c;
+	}
 }
