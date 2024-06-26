0d8caaca8a75daf17a658588cb02319afbde9869
euics <pjh575157@gmail.com>
2024-06-26 13:15:21 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.27 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,35 +1,37 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    public String solution(String m, String[] musicinfos) {
-        int playTime = Integer.MIN_VALUE;
-        String answer = "(None)";
-        m = replaceSharp(m);
+	public String solution(String m, String[] musicinfos) {
+		String answer = "(None)";
+		m = replaceSharp(m);
+		int maxLength = Integer.MIN_VALUE;
 
-        for(int i = 0; i < musicinfos.length; i++) {
-            int timeLength = changeTime(musicinfos[i].split(",")[1]) - changeTime(musicinfos[i].split(",")[0]);
+		for (String musicInfo : musicinfos) {
+			int musicLength = changeTime(musicInfo.split(",")[1]) - changeTime(musicInfo.split(",")[0]);
 
-            StringBuilder sb = new StringBuilder();
-            String basicMelody = replaceSharp(musicinfos[i].split(",")[3]);
-            for(int time = 0; time < timeLength; time++) sb.append(basicMelody.charAt(time % basicMelody.length()));
+			StringBuilder sb = new StringBuilder();
+			String melody = replaceSharp(musicInfo.split(",")[3]);
+			for (int i = 0; i < musicLength; i++)
+				sb.append(melody.charAt(i % melody.length()));
 
-            if(sb.toString().contains(m) && playTime < timeLength) {
-                playTime = timeLength;
-                answer = musicinfos[i].split(",")[2];
-            }
-        }
+			if (sb.toString().contains(m) && maxLength < musicLength) {
+				maxLength = musicLength;
+				answer = musicInfo.split(",")[2];
+			}
+		}
 
-        return answer;
-    }
+		return answer;
+	}
 
-    public int changeTime(String time) {
-        return Integer.parseInt(time.split(":")[0]) * 60 + Integer.parseInt(time.split(":")[1]);
-    }
+	public String replaceSharp(String melody) {
+		return melody
+			.replace("A#", "H")
+			.replace("B#", "I")
+			.replace("C#", "J")
+			.replace("D#", "K")
+			.replace("F#", "L")
+			.replace("G#", "M");
+	}
 
-    public String replaceSharp(String melody) {
-        return melody.replace("C#", "H").replace("D#", "I")
-                .replace("F#", "J").replace("G#", "K")
-                .replace("A#", "L").replace("B#", "M");
-    }
+	public int changeTime(String time) {
+		return Integer.parseInt(time.split(":")[0]) * 60 + Integer.parseInt(time.split(":")[1]);
+	}
 }
