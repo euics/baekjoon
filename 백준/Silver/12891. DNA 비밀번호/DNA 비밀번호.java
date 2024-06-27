38544cc3c43e30a5ea7d4edbc5654efd1f8b272e
euics <pjh575157@gmail.com>
2024-06-27 17:07:07 +0900

    [Silver II] Title: DNA 비밀번호, Time: 348 ms, Memory: 21752 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,63 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
+class Main {
+	static int[] rules = new int[4];
+	static int[] cur = new int[4];
 
-        String str = br.readLine();
-        int[] rules = new int[4];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 4; i++) rules[i] = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int S = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
 
-        int answer = 0;
-        int[] tmp = new int[4];
-        for(int i = 0; i < p; i++) {
-            if(str.charAt(i) == 'A') tmp[0]++;
-            if(str.charAt(i) == 'C') tmp[1]++;
-            if(str.charAt(i) == 'G') tmp[2]++;
-            if(str.charAt(i) == 'T') tmp[3]++;
-        }
+		String DNA = br.readLine();
 
-        if(checkArr(tmp, rules)) answer++;
+		rules = new int[4];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++)
+			rules[i] = Integer.parseInt(st.nextToken());
 
-        for(int end = p, start = 0; end < s; end++) {
-            if(str.charAt(start) == 'A') tmp[0]--;
-            if(str.charAt(start) == 'C') tmp[1]--;
-            if(str.charAt(start) == 'G') tmp[2]--;
-            if(str.charAt(start) == 'T') tmp[3]--;
+		cur = new int[4];
+		for (int i = 0; i < P; i++)
+			addCur(DNA.charAt(i));
 
-            if(str.charAt(end) == 'A') tmp[0]++;
-            if(str.charAt(end) == 'C') tmp[1]++;
-            if(str.charAt(end) == 'G') tmp[2]++;
-            if(str.charAt(end) == 'T') tmp[3]++;
+		int answer = 0;
+		if (check(rules, cur))
+			answer++;
 
-            if(checkArr(tmp, rules)) answer++;
-            start++;
-        }
+		int start = 0, end = P;
+		while (end < DNA.length()) {
+			addCur(DNA.charAt(end));
+			removeCur(DNA.charAt(start));
 
-        System.out.println(answer);
-    }
+			if (check(rules, cur))
+				answer++;
 
-    public static boolean checkArr(int[] tmp, int[] rules) {
-        for(int i = 0; i < 4; i++) {
-            if(tmp[i] < rules[i]) return false;
-        }
+			end++;
+			start++;
+		}
 
-        return true;
-    }
+		System.out.println(answer);
+	}
+
+	public static boolean check(int[] rules, int[] cur) {
+		for (int i = 0; i < 4; i++) {
+			if (cur[i] < rules[i])
+				return false;
+		}
+
+		return true;
+	}
+
+	public static void addCur(char ch) {
+		if (ch == 'A')
+			cur[0]++;
+		if (ch == 'C')
+			cur[1]++;
+		if (ch == 'G')
+			cur[2]++;
+		if (ch == 'T')
+			cur[3]++;
+	}
+
+	public static void removeCur(char ch) {
+		if (ch == 'A')
+			cur[0]--;
+		if (ch == 'C')
+			cur[1]--;
+		if (ch == 'G')
+			cur[2]--;
+		if (ch == 'T')
+			cur[3]--;
+	}
 }
-
-// 4
-// AACCTTG
-// 2 2 2 1
-// 1 0 0 1
-// 1 2 2 0
-// AG *
-
-// A C G T
