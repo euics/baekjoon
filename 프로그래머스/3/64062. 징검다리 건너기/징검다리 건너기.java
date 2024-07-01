dae438b3453a48b3374a1a145f3e341fe2fa760f
euics <pjh575157@gmail.com>
2024-07-01 17:00:01 +0900

    [level 3] Title: 징검다리 건너기, Time: 22.22 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,35 +1,38 @@
 class Solution {
-    public int solution(int[] stones, int k) {
-        int low = 0, high = 0;
-        for(int stone : stones) high = Math.max(high, stone);
+	public int solution(int[] stones, int k) {
+		int low = 0, high = 0;
+		for (int stone : stones)
+			high = Math.max(high, stone);
 
-        int answer = 0;
-        while(low <= high) {
-            int mid = (low + high) / 2;
+		int answer = 0;
+		while (low <= high) {
+			int mid = (low + high) / 2;
 
-            if(canCross(stones, k, mid)) {
-                answer = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
+			if (canCross(stones, k, mid)) {
+				answer = mid;
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
 
-        return answer;
-    }
+		return answer;
+	}
 
-    public boolean canCross(int[] stones, int k, int mid) {
-        int cnt = 0;
-        for(int stone : stones) {
-            if(stone - mid < 0) {
-                cnt++;
+	public boolean canCross(int[] stones, int k, int mid) {
+		int cnt = 0;
 
-                if(cnt >= k) return false;
-            } else {
-                cnt = 0;
-            }
-        }
+		for (int stone : stones) {
+			if (stone - mid < 0) {
+				cnt++;
 
-        return true;
-    }
+				if (cnt >= k)
+					return false;
+			} else {
+				cnt = 0;
+			}
+		}
+
+		return true;
+	}
 }
