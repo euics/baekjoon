b0c7a1be9bab44a1dba16eb4c3e5bd6809e5c8d8
euics <pjh575157@gmail.com>
2024-07-01 20:43:35 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45500 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,20 +1,21 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-        Deque<Integer> deque = new LinkedList<>();
-        for(int i = 1; i <= n; i++)
-            deque.addLast(i);
+		Deque<Integer> dq = new LinkedList<>();
+		for (int i = 1; i <= n; i++)
+			dq.addLast(i);
 
-        while(deque.size() != 1){
-            deque.pollFirst();
-            deque.addLast(deque.pollFirst());
-        }
+		while (!dq.isEmpty() && dq.size() > 1) {
+			int remove = dq.pollFirst();
+			dq.addLast(dq.pollFirst());
+		}
 
-        System.out.println(deque.peek());
-    }
+		System.out.println(dq.pollFirst());
+	}
 }
