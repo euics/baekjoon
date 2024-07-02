5d09a21f7e9f12f924257428cda1ce960f4fa91a
euics <pjh575157@gmail.com>
2024-07-02 14:55:59 +0900

    [Bronze II] Title: 수 정렬하기, Time: 248 ms, Memory: 18520 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
+
+		for (int x : arr)
+			System.out.printf("%d ", x);
+	}
+}
