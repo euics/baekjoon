94dbfe87159fb46529908d947f2424ff56f6400a
euics <pjh575157@gmail.com>
2024-07-02 14:38:22 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,75 +1,82 @@
+import java.lang.management.OperatingSystemMXBean;
 import java.util.*;
 
 class Solution {
-    public long solution(String expression) {
-        List<Long> numbers = new ArrayList<>();
-        List<Character> operations = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
+	static long max = Integer.MIN_VALUE;
 
-        for (char ch : expression.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                sb.append(ch);
-            } else {
-                operations.add(ch);
-                numbers.add(Long.parseLong(sb.toString()));
-                sb = new StringBuilder();
-            }
-        }
-        numbers.add(Long.parseLong(sb.toString()));
+	public long solution(String expression) {
+		List<Long> numbers = new ArrayList<>();
+		List<Character> operations = new ArrayList<>();
 
-        char[] oper = new char[]{'+', '-', '*'};
-        char[] sequences = new char[3];
-        permutation(0, oper, sequences, new boolean[3], numbers, operations);
+		StringBuilder sb = new StringBuilder();
+		for (char ch : expression.toCharArray()) {
+			if (Character.isDigit(ch))
+				sb.append(ch);
+			else {
+				numbers.add(Long.parseLong(sb.toString()));
+				operations.add(ch);
+				sb = new StringBuilder();
+			}
+		}
+		numbers.add(Long.parseLong(sb.toString()));
 
-        return max;
-    }
+		char[] permutations = new char[3];
+		permutation(0, numbers, operations, new char[] {'+', '-', '*'}, permutations, new boolean[3]);
 
-    static long max = Long.MIN_VALUE;
+		return max;
+	}
 
-    public void permutation(int L, char[] oper, char[] sequences, boolean[] bool, List<Long> numbers, List<Character> operations) {
-        if (L == 3) {
-            max = Math.max(max, Math.abs(calculate(numbers, operations, sequences)));
+	public void permutation(
+		int L,
+		List<Long> numbers,
+		List<Character> operations,
+		char[] sequences,
+		char[] permutations,
+		boolean[] bool
+	) {
+		if (L == 3) {
+			max = Math.max(max, Math.abs(calculate(numbers, operations, permutations)));
 
-            return;
-        }
+			return;
+		}
 
-        for (int i = 0; i < 3; i++) {
-            if (!bool[i]) {
-                bool[i] = true;
-                sequences[L] = oper[i];
-                permutation(L + 1, oper, sequences, bool, numbers, operations);
-                bool[i] = false;
-            }
-        }
-    }
+		for (int i = 0; i < 3; i++) {
+			if (!bool[i]) {
+				bool[i] = true;
+				permutations[L] = sequences[i];
+				permutation(L + 1, numbers, operations, sequences, permutations, bool);
+				bool[i] = false;
+			}
+		}
+	}
 
+	public long calculate(List<Long> numbers, List<Character> operations, char[] permutations) {
+		LinkedList<Long> numList = new LinkedList<>(numbers);
+		LinkedList<Character> operList = new LinkedList<>(operations);
+			
+		for (char permutation : permutations) {
+			for (int i = 0; i < operList.size(); i++) {
+				if (permutation == operList.get(i)) {
+					long result = applyOperation(numList.remove(i), numList.remove(i), operList.remove(i));
+					numList.add(i, result);
+					i--;
+				}
+			}
+		}
 
-    private long calculate(List<Long> numbers, List<Character> operations, char[] sequences) {
-        LinkedList<Long> numList = new LinkedList<>(numbers);
-        LinkedList<Character> opList = new LinkedList<>(operations);
+		return numList.get(0);
+	}
 
-        for (char sequence : sequences) {
-            for (int i = 0; i < opList.size(); i++) {
-                if (opList.get(i) == sequence) {
-                    long result = applyOperation(numList.remove(i), numList.remove(i), opList.remove(i));
-                    numList.add(i, result);
-                    i--;
-                }
-            }
-        }
-        return numList.get(0);
-    }
-
-    private long applyOperation(long a, long b, char op) {
-        switch (op) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            default:
-                throw new IllegalArgumentException("Unknown operation: " + op);
-        }
-    }
+	public long applyOperation(long a, long b, char operation) {
+		switch (operation) {
+			case '+':
+				return a + b;
+			case '-':
+				return a - b;
+			case '*':
+				return a * b;
+			default:
+				throw new IllegalStateException("Apply Operation Exception Occur");
+		}
+	}
 }
