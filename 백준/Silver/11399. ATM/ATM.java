e23749d5ae99c95686e33977d6178a3f89c95191
euics <pjh575157@gmail.com>
2024-07-03 14:48:58 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,40 +1,35 @@
-import com.sun.source.tree.BinaryTree;
-
 import java.io.*;
-import java.math.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int[] P = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			P[i] = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+		for (int target = 1; target < N; target++) {
+			for (int compare = target; compare > 0; compare--) {
+				if (P[compare] < P[compare - 1]) {
+					int tmp = P[compare];
+					P[compare] = P[compare - 1];
+					P[compare - 1] = tmp;
+				} else
+					break;
+			}
+		}
 
-        Arrays.sort(arr); // 1 2 3 3 4
-        int sum = 0, total = 0;
-        for(int i = 1; i <= n; i++){
-            sum = sum + arr[i];
-            total += sum;
-        }
+		int[] accumulate = new int[N + 1];
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			accumulate[i + 1] = accumulate[i] + P[i];
+			answer += accumulate[i];
+		}
 
-        System.out.println(total);
-    }
+		answer += accumulate[N];
+		System.out.println(answer);
+	}
 }
-
-/*
- P1 = 3
- P2 = 1
- P3 = 4
- P4 = 3
- P5 = 2
-
- 1
- 1 2
- 1 2 3
- 1 2 3 3
- 1 2 3 3 4
-*/
