1de9956b6bdd5a4ba4ae21c1b913d47b2c9cde4b
euics <pjh575157@gmail.com>
2024-07-07 19:57:29 +0900

    [Silver IV] Title: 수 찾기, Time: 648 ms, Memory: 50980 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,39 +1,79 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
+class Main {
 
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < m; i++){
-            boolean bool = false;
-            int tmp = Integer.parseInt(st.nextToken());
-            int lt = 0, rt = arr.length - 1;
-            while(lt <= rt){
-                int mid = (lt + rt) / 2;
-                if(arr[mid] > tmp)
-                    rt = mid - 1;
-                else if(arr[mid] < tmp)
-                    lt = mid + 1;
-                else {
-                    sb.append(1).append("\n");
-                    bool = true;
-                    break;
-                }
-            }
-            if(!bool)
-                sb.append(0).append("\n");
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			A[i] = Integer.parseInt(st.nextToken());
 
-        System.out.println(sb);
-    }
+		// int max = getMax(A);
+		// for (int exp = 1; max / exp > 0; exp *= 10)
+		// 	countingSort(A, exp);
+
+		Arrays.sort(A);
+
+		StringBuilder sb = new StringBuilder();
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			int result = binarySearch(A, num);
+			sb.append(result).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	public static int getMax(int[] A) {
+		int max = A[0];
+		for (int i = 1; i < A.length; i++) {
+			if (max < A[i])
+				max = A[i];
+		}
+
+		return max;
+	}
+
+	public static void countingSort(int[] A, int exp) {
+		int n = A.length;
+		int[] output = new int[n];
+		int[] counting = new int[10];
+
+		for (int i = 0; i < n; i++) {
+			int digit = (A[i] / exp) % 10;
+			counting[digit]++;
+		}
+
+		for (int i = 1; i < 10; i++)
+			counting[i] += counting[i - 1];
+
+		for (int i = n - 1; i >= 0; i--) {
+			int digit = (A[i] / exp) % 10;
+			output[counting[digit] - 1] = A[i];
+			counting[digit]--;
+		}
+
+		System.arraycopy(output, 0, A, 0, n);
+	}
+
+	public static int binarySearch(int[] A, int num) {
+		int start = 0, end = A.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (A[mid] == num)
+				return 1;
+			else if (A[mid] > num)
+				end = mid - 1;
+			else
+				start = mid + 1;
+		}
+
+		return 0;
+	}
 }
