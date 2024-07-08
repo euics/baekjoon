84bdb4e9ff2632bf71c6ce71793ee14ad58360a4
euics <pjh575157@gmail.com>
2024-07-08 14:33:36 +0900

    [Gold I] Title: K번째 수, Time: 172 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		long N = Integer.parseInt(br.readLine());
+		long k = Integer.parseInt(br.readLine());
+
+		long start = 1, end = N * N, answer = -1;
+		while (start <= end) {
+			long mid = (start + end) / 2;
+			long sum = 0;
+			for (int i = 1; i <= N; i++)
+				sum += Math.min(mid / i, N);
+
+			if (sum >= k) {
+				answer = mid;
+				end = mid - 1;
+			} else
+				start = mid + 1;
+		}
+
+		System.out.println(answer);
+	}
+}
