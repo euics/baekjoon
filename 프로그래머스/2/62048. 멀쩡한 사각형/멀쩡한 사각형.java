179e2c891b0d4736f8ffd43b6dd07e718d797174
euics <pjh575157@gmail.com>
2024-07-14 17:42:23 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.14 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,19 +1,20 @@
 class Solution {
 	public long solution(int w, int h) {
-		long gcd = gcd(w, h);
-		long totalSquare = (long) w * h;
-		long unusableSquares = w + h - gcd;
-		
-		return totalSquare - unusableSquares;
+		long totalSquares = (long)w * h;
+		long GCD = GCD((long)w, (long)h);
+		long unusableSquares = w + h - GCD;
+
+		return totalSquares - unusableSquares;
 	}
 
-	public long gcd(long a, long b) {
-		while (b != 0) {
-			long tmp = b;
-			b = a % b;
-			a = tmp;
-		}
+	public long GCD(long a, long b) {
+		if (b == 0)
+			return a;
 
-		return a;
+		return GCD(b, a % b);
+	}
+
+	public long LCM(long a, long b) {
+		return a * b / GCD(a, b);
 	}
 }
