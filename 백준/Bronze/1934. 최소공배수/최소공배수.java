0c2f3287d8efdbb6ee59f15fbadbd9aece4ba288
euics <pjh575157@gmail.com>
2024-07-15 15:37:29 +0900

    [Bronze I] Title: 최소공배수, Time: 128 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,18 +1,33 @@
 import java.io.*;
-import java.math.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+class Main {
 
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            BigInteger a = new BigInteger(st.nextToken());
-            BigInteger b = new BigInteger(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-            System.out.println(a.multiply(b).intValue() / a.gcd(b).intValue());
-        }
-    }
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			sb.append(LCM(a, b)).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	public static int GCD(int a, int b) {
+		if (b == 0)
+			return a;
+
+		return GCD(b, a % b);
+	}
+
+	public static int LCM(int a, int b) {
+		return a * b / GCD(a, b);
+	}
 }
