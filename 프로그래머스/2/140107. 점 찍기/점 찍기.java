582335dbaf46bdd5958eefff25cbce04591a25e8
euics <pjh575157@gmail.com>
2024-07-18 21:07:11 +0900

    [level 2] Title: 점 찍기, Time: 13.59 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -2,7 +2,7 @@ class Solution {
 	public long solution(int k, int d) {
 		long answer = 0;
 
-		for(int x = 0; x <= d; x += k) {
+		for (int x = 0; x <= d; x += k) {
 			int maxY = (int)Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2));
 			answer += (maxY / k) + 1;
 		}
