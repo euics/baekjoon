15f5a031b27eac779558e48203eb0744232ef61c
euics <pjh575157@gmail.com>
2024-07-30 14:20:15 +0900

    [level 3] Title: 이중우선순위큐, Time: 173.14 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,28 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] operations) {
-        List<Integer> heap = new ArrayList<>();
-        
-        for(String operation : operations) {
-            String order = operation.split(" ")[0];
-            
-            switch(order) {
-                case "I":
-                    heap.add(Integer.parseInt(operation.split(" ")[1]));
-                    break;
-                case "D":
-                    if(heap.size() != 0) {
-                        if(Integer.parseInt(operation.split(" ")[1]) == 1) heap.remove(heap.size() - 1);
-                        else heap.remove(0);
-                        
-                        break;
-                    }
-            }
-            
-            Collections.sort(heap);
-        }
-        
-        return heap.size() == 0 ? new int[]{0, 0} : new int[]{heap.get(heap.size() - 1), heap.get(0)};
-    }
+	public int[] solution(String[] operations) {
+		List<Integer> list = new ArrayList<>();
+		for (String operation : operations) {
+			char order = operation.split(" ")[0].charAt(0);
+			int num = Integer.parseInt(operation.split(" ")[1]);
+
+			if (order == 'I')
+				list.add(num);
+			else {
+				if (list.isEmpty())
+					continue;
+
+				if (num == 1)
+					list.remove(list.size() - 1);
+				else
+					list.remove(0);
+			}
+
+			Collections.sort(list);
+		}
+
+		if (list.isEmpty())
+			return new int[] {0, 0};
+		else
+			return new int[] {list.get(list.size() - 1), list.get(0)};
+	}
 }
