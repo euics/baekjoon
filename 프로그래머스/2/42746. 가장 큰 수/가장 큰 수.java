f4af4550b6f5e04bf404529e1f964c5cf44caad0
euics <pjh575157@gmail.com>
2024-07-31 13:49:39 +0900

    [level 2] Title: 가장 큰 수, Time: 227.01 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,20 +1,26 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
-    public String solution(int[] numbers) {
-        String[] number = new String[numbers.length];
-        for(int i = 0; i < numbers.length; i++) number[i] = String.valueOf(numbers[i]);
-        
-        Arrays.sort(number, new Comparator<String>(){
-            @Override
-            public int compare(String o1, String o2) {
-                return (o2 + o1).compareTo(o1 + o2);
-            }
-        });
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < number.length; i++) sb.append(number[i]);
-        
-        return sb.toString().startsWith("0") ? "0" : sb.toString();
-    }
+	public String solution(int[] numbers) {
+		String[] tmp = new String[numbers.length];
+		for (int i = 0; i < numbers.length; i++)
+			tmp[i] = String.valueOf(numbers[i]);
+
+		Arrays.sort(tmp, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return (o2 + o1).compareTo(o1 + o2);
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < tmp.length; i++)
+			sb.append(tmp[i]);
+
+		if (sb.toString().startsWith("0"))
+			return "0";
+		else
+			return sb.toString();
+	}
 }
