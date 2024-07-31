9daceb1c0e6b6de02a6ac2e1667c9831bdd13578
euics <pjh575157@gmail.com>
2024-07-31 15:54:26 +0900

    [Gold V] Title: 최소비용 구하기, Time: 556 ms, Memory: 51740 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,66 +2,72 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static int[] dis;
-    static ArrayList<ArrayList<Edge>> graph;
+	public static ArrayList<ArrayList<Edge>> graph;
+	public static int[] dis;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        dis = new int[n + 1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
-        graph = new ArrayList<ArrayList<Edge>>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int V = Integer.parseInt(br.readLine());
+		int E = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i <= n; i++)
-            graph.add(new ArrayList<Edge>());
+		init(V);
 
-        for(int i = 0; i < m; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            graph.get(Integer.parseInt(st.nextToken()))
-                    .add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
+		for (int i = 0; i < E; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			graph.get(A).add(new Edge(B, cost));
+		}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
+		dijkstra(start);
 
-        dijkstra(start);
-        System.out.println(dis[end]);
-    }
+		System.out.println(dis[end]);
+	}
 
-    private static void dijkstra(int v){
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(v, 0));
-        dis[v] = 0;
+	public static void init(int V) {
+		dis = new int[V + 1];
+		Arrays.fill(dis, Integer.MAX_VALUE);
 
-        while(!pq.isEmpty()){
-            Edge cur = pq.poll();
+		graph = new ArrayList<>();
+		for (int i = 0; i <= V; i++)
+			graph.add(new ArrayList<Edge>());
+	}
 
-            if(cur.cost > dis[cur.vertex])
-                continue;
+	public static void dijkstra(int v) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.c - b.c);
+		pq.add(new Edge(v, 0));
+		dis[v] = 0;
 
-            for(Edge o : graph.get(cur.vertex)){
-                if(dis[o.vertex] > cur.cost + o.cost){
-                    pq.add(new Edge(o.vertex, cur.cost + o.cost));
-                    dis[o.vertex] = cur.cost + o.cost;
-                }
-            }
-        }
-    }
+		while (!pq.isEmpty()) {
+			Edge cur = pq.poll();
+			if (dis[cur.v] < cur.c)
+				continue;
+
+			for (Edge o : graph.get(cur.v)) {
+				if (dis[o.v] > o.c + cur.c) {
+					dis[o.v] = o.c + cur.c;
+					pq.add(new Edge(o.v, o.c + cur.c));
+				}
+			}
+		}
+	}
 }
 
-class Edge implements Comparable<Edge>{
-    int vertex, cost;
-    public Edge(int vertex, int cost){
-        this.vertex = vertex;
-        this.cost = cost;
-    }
+class Edge implements Comparable<Edge> {
+	int v, c;
 
-    @Override
-    public int compareTo(Edge o){
-        return this.cost - o.cost;
-    }
+	public Edge(int v, int c) {
+		this.v = v;
+		this.c = c;
+	}
+
+	@Override
+	public int compareTo(Edge o) {
+		return this.c - o.c;
+	}
 }
