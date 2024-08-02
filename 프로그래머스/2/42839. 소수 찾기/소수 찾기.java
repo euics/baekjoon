1bd2b3012a940ecd4d0d3101d57be4eeb21b7360
euics <pjh575157@gmail.com>
2024-08-02 12:19:20 +0900

    [level 2] Title: 소수 찾기, Time: 35.16 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,31 +1,42 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public int solution(String numbers) {
-        Set<Long> permutation = new HashSet<>();
-        permutation("", numbers, permutation);
-        
-        return (int)permutation.stream().filter(this::isPrime).count();
-    }
-    
-    public void permutation(String prefix, String str, Set<Long> permutation) {
-        if(!prefix.equals("")) permutation.add(Long.parseLong(prefix));
-        
-        int n = str.length();
-        for(int i = 0; i < n; i++) {
-            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), permutation);
-        }
-    }
-    
-    public boolean isPrime(long num) {
-        if(num <= 1) return false;
-        if(num == 2) return true;
-        if(num % 2 == 0) return false;
-        
-        for(int i = 3; i * i <= num; i += 2) {
-            if(num % i == 0) return false;
-        }
-        
-        return true;
-    }
+	public int solution(String numbers) {
+		int answer = 0;
+
+		Set<Integer> permutation = new HashSet<>();
+		permutation("", numbers, permutation);
+		for (int num : permutation) {
+			if (isPrime(num))
+				answer++;
+		}
+		
+		return answer;
+	}
+
+	public void permutation(String prefix, String str, Set<Integer> permutation) {
+		if (!prefix.equals(""))
+			permutation.add(Integer.parseInt(prefix));
+
+		int n = str.length();
+		for (int i = 0; i < n; i++)
+			permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), permutation);
+	}
+
+	public boolean isPrime(int num) {
+		if (num <= 1)
+			return false;
+		if (num == 2)
+			return true;
+		if (num % 2 == 0)
+			return false;
+
+		for (int i = 3; i * i <= num; i += 2) {
+			if (num % i == 0)
+				return false;
+		}
+
+		return true;
+	}
 }
