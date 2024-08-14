e0fad559aad686427495eb94c7e2b9ca170fd087
euics <pjh575157@gmail.com>
2024-08-14 13:12:46 +0900

    [level 2] Title: 큰 수 만들기, Time: 900.02 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,29 +1,27 @@
 import java.util.Stack;
 
 class Solution {
-    public String solution(String number, int k) {
-        Stack<Integer> stack = new Stack<>();
+	public String solution(String number, int k) {
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < number.length(); i++) {
+			int cur = Integer.parseInt(String.valueOf(number.charAt(i)));
+			while (!stack.isEmpty() && k > 0 && stack.peek() < cur) {
+				stack.pop();
+				k--;
+			}
 
-        for(int i = 0; i < number.length(); i++) {
-            int cur = Integer.parseInt(String.valueOf(number.charAt(i)));
+			stack.push(cur);
+		}
 
-            while(!stack.isEmpty() && stack.peek() < cur && k > 0) {
-                stack.pop();
-                k--;
-            }
+		while (!stack.isEmpty() && k > 0) {
+			stack.pop();
+			k--;
+		}
 
-            stack.push(cur);
-        }
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty())
+			sb.insert(0, stack.pop());
 
-        while(k > 0) {
-            stack.pop();
-            k--;
-        }
-
-
-        StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()) sb.insert(0, stack.pop());
-
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 }
