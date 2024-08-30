6770c5240d5baa8bd89fd1ee74b3ed0c187d2898
euics <pjh575157@gmail.com>
2024-08-30 16:20:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 420 ms, Memory: 112120 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,32 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
 	static int[] unf;
+	static int[] groupSize;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+
 		init(N);
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			union(a, b);
 		}
 
-		Set<Integer> set = new HashSet<>();
-		for (int i = 1; i <= N; i++)
-			set.add(find(i));
+		int answer = 0;
+		for (int i = 1; i <= N; i++) {
+			if (unf[i] == i)
+				answer++;
+		}
 
-		System.out.println(set.size());
+		System.out.println(answer);
 	}
 
 	public static void init(int N) {
 		unf = new int[N + 1];
-		for (int i = 0; i <= N; i++)
+		groupSize = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
 			unf[i] = i;
+			groupSize[i] = 1;
+		}
 	}
 
 	public static int find(int v) {
@@ -40,7 +50,9 @@ public class Main {
 		int fa = find(a);
 		int fb = find(b);
 
-		if (fa != fb)
+		if (fa != fb) {
 			unf[fa] = fb;
+			groupSize[fb] += groupSize[fa];
+		}
 	}
 }
