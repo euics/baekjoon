241dcfb499ce4a3d48f29af12dacac2cbe8ddc4a
euics <pjh575157@gmail.com>
2024-09-03 16:22:53 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,26 +1,28 @@
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Main {
 
-        String[] splitBySubtraction = br.readLine().split("-");
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		String input = br.readLine();
+		String[] splitByMinus = input.split("-");
 
-        int sum = Integer.MAX_VALUE;
-        for (String value : splitBySubtraction) {
-            int tmp = 0;
+		int sum = Integer.MAX_VALUE;
+		for (String value : splitByMinus) {
+			int tmp = 0;
 
-            String[] splitByAddition = value.split("\\+");
+			String[] splitByPlus = value.split("\\+");
+			for (String plus : splitByPlus)
+				tmp += Integer.parseInt(plus);
 
-            for (String s : splitByAddition)
-                tmp += Integer.parseInt(s);
+			if (sum == Integer.MAX_VALUE)
+				sum = tmp;
+			else
+				sum -= tmp;
+		}
 
-            if (sum == Integer.MAX_VALUE)
-                sum = tmp;
-            else
-                sum -= tmp;
-        }
-
-        System.out.println(sum);
-    }
+		System.out.println(sum);
+	}
 }
