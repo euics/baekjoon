5639849d9ea96458c320e7ca2341260b70783a26
euics <pjh575157@gmail.com>
2024-09-13 17:02:35 +0900

    [Gold III] Title: 게임 개발, Time: 248 ms, Memory: 22460 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -6,45 +6,40 @@ class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++)
-			graph.add(new ArrayList<Integer>());
+		for(int i = 0; i <= N; i++) graph.add(new ArrayList<Integer>());
 		int[] inDegree = new int[N + 1];
 		int[] costs = new int[N + 1];
 		int[] results = new int[N + 1];
 
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			costs[i] = Integer.parseInt(st.nextToken());
+			results[i] = costs[i];
 
 			while (true) {
-				int pre = Integer.parseInt(st.nextToken());
-				if (pre == -1)
-					break;
+				int input = Integer.parseInt(st.nextToken());
+				if(input == -1) break;
 
-				graph.get(pre).add(i);
+				graph.get(input).add(i);
 				inDegree[i]++;
 			}
 		}
 
 		Queue<Integer> q = new LinkedList<>();
-		for (int i = 1; i <= N; i++) {
-			if (inDegree[i] == 0) {
-				q.add(i);
-				results[i] = costs[i];
-			}
+		for(int i = 1; i <= N; i++) {
+			if(inDegree[i] == 0) q.add(i);
 		}
 
 		while (!q.isEmpty()) {
-			int cur = q.poll();
+			int prev = q.poll();
 
-			for (int next : graph.get(cur)) {
+			for(int next : graph.get(prev)) {
 				inDegree[next]--;
-				results[next] = Math.max(results[next], results[cur] + costs[next]);
-				if (inDegree[next] == 0)
-					q.add(next);
+				results[next] = Math.max(results[next], results[prev] + costs[next]);
+				if(inDegree[next] == 0) q.add(next);
 			}
 		}
 
