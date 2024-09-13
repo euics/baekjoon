c8d9ab30f850bb7b6f5eaf3b0ba4c6f2aab114f0
euics <pjh575157@gmail.com>
2024-09-13 12:54:04 +0900

    [Gold III] Title: 게임 개발, Time: 252 ms, Memory: 21916 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,30 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// StringTokenizer st = new StringTokenizer(br.readLine());
+class Main {
+	static ArrayList<ArrayList<Integer>> graph;
 
-		int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++)
+			graph.add(new ArrayList<Integer>());
 		int[] inDegree = new int[N + 1];
 		int[] costs = new int[N + 1];
-		int[] result = new int[N + 1]; // 각 건물의 완성 시간을 저장할 배열
-		List<List<Integer>> graph = new ArrayList<>();
-
-		for (int i = 0; i <= N; i++)
-			graph.add(new ArrayList<>());
+		int[] results = new int[N + 1];
 
 		for (int i = 1; i <= N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			costs[i] = Integer.parseInt(st.nextToken());
 
 			while (true) {
-				int building = Integer.parseInt(st.nextToken());
-				if (building == -1)
+				int pre = Integer.parseInt(st.nextToken());
+				if (pre == -1)
 					break;
 
-				graph.get(building).add(i);
+				graph.get(pre).add(i);
 				inDegree[i]++;
 			}
 		}
@@ -33,22 +33,22 @@ public class Main {
 		for (int i = 1; i <= N; i++) {
 			if (inDegree[i] == 0) {
 				q.add(i);
-				result[i] = costs[i];
+				results[i] = costs[i];
 			}
 		}
 
 		while (!q.isEmpty()) {
-			int prev = q.poll();
+			int cur = q.poll();
 
-			for (int next : graph.get(prev)) {
+			for (int next : graph.get(cur)) {
 				inDegree[next]--;
-				result[next] = Math.max(result[next], result[prev] + costs[next]);
+				results[next] = Math.max(results[next], results[cur] + costs[next]);
 				if (inDegree[next] == 0)
 					q.add(next);
 			}
 		}
 
-		for (int i = 1; i <= N; i++)
-			System.out.println(result[i]);
+		for(int i = 1; i <= N; i++)
+			System.out.println(results[i]);
 	}
 }
