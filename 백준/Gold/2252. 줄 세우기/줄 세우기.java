d023bca75bda81aa701e9c6f42e59ad30bb27c30
euics <pjh575157@gmail.com>
2024-09-13 12:19:00 +0900

    [Gold III] Title: 줄 세우기, Time: 448 ms, Memory: 47268 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,19 +2,18 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static List<List<Integer>> graph;
+	static ArrayList<ArrayList<Integer>> graph;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
 		graph = new ArrayList<>();
 		for (int i = 0; i <= N; i++)
 			graph.add(new ArrayList<Integer>());
-
 		int[] inDegree = new int[N + 1];
+
+		int M = Integer.parseInt(st.nextToken());
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
