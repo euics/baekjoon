26d0ea3655b5a4ab8527b41341454779966ed5d4
euics <pjh575157@gmail.com>
2024-09-14 16:12:21 +0900

    [Gold V] Title: 최소비용 구하기, Time: 464 ms, Memory: 51044 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,57 +1,54 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	public static ArrayList<ArrayList<Edge>> graph;
-	public static int[] dis;
+class Main {
+	static ArrayList<ArrayList<Edge>> graph;
+	static int[] dis;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(br.readLine());
-		int E = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
 
-		init(V);
+		graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++)
+			graph.add(new ArrayList<Edge>());
+		dis = new int[N + 1];
+		Arrays.fill(dis, Integer.MAX_VALUE);
 
-		for (int i = 0; i < E; i++) {
+		for(int i = 0; i < M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			graph.get(A).add(new Edge(B, cost));
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+
+			graph.get(u).add(new Edge(v, w));
 		}
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
+
 		dijkstra(start);
 
 		System.out.println(dis[end]);
 	}
-
-	public static void init(int V) {
-		dis = new int[V + 1];
-		Arrays.fill(dis, Integer.MAX_VALUE);
-
-		graph = new ArrayList<>();
-		for (int i = 0; i <= V; i++)
-			graph.add(new ArrayList<Edge>());
-	}
-
-	public static void dijkstra(int v) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.c - b.c);
-		pq.add(new Edge(v, 0));
-		dis[v] = 0;
-
+	
+	public static void dijkstra(int start) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(start , 0));
+		dis[start] = 0;
+		
 		while (!pq.isEmpty()) {
 			Edge cur = pq.poll();
-			if (dis[cur.v] < cur.c)
-				continue;
+			
+			if(dis[cur.v] < cur.c) continue;
 
-			for (Edge o : graph.get(cur.v)) {
-				if (dis[o.v] > o.c + cur.c) {
-					dis[o.v] = o.c + cur.c;
-					pq.add(new Edge(o.v, o.c + cur.c));
+			for(Edge o : graph.get(cur.v)) {
+				if(dis[o.v] > cur.c + o.c) {
+					dis[o.v] = cur.c + o.c;
+					pq.add(new Edge(o.v, cur.c + o.c));
 				}
 			}
 		}
