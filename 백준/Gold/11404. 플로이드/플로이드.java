31459926be2c69f9ec723efcc422bde0dde2b3c5
euics <pjh575157@gmail.com>
2024-09-15 20:19:55 +0900

    [Gold IV] Title: 플로이드, Time: 996 ms, Memory: 74868 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,22 +1,21 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	public static int[][] dis;
+class Main {
+	static int[][] dis;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-
-		dis = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
+		int n = Integer.parseInt(br.readLine());
+		dis = new int[n + 1][n + 1];
+		for (int i = 0; i <= n; i++) {
 			Arrays.fill(dis[i], Integer.MAX_VALUE / 2);
 			dis[i][i] = 0;
 		}
 
-		for (int i = 0; i < M; i++) {
+		int m = Integer.parseInt(br.readLine());
+		for (int i = 0; i < m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
@@ -25,19 +24,20 @@ public class Main {
 			dis[A][B] = Math.min(dis[A][B], C);
 		}
 
-		for (int K = 1; K <= N; K++) {
-			for (int S = 1; S <= N; S++) {
-				for (int E = 1; E <= N; E++) {
+		// 플로이드-워셜 알고리즘
+		for (int K = 1; K <= n; K++) {
+			for (int S = 1; S <= n; S++) {
+				for (int E = 1; E <= n; E++) {
 					if (dis[S][E] > dis[S][K] + dis[K][E])
 						dis[S][E] = dis[S][K] + dis[K][E];
 				}
 			}
 		}
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
 				if (dis[i][j] == Integer.MAX_VALUE / 2)
-					System.out.printf("%d ", 0);
+					System.out.printf("0 ");
 				else
 					System.out.printf("%d ", dis[i][j]);
 			}
