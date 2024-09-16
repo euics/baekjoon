1fd45efd5e701d8caee4547a115fdbd4b5208ea7
euics <pjh575157@gmail.com>
2024-09-16 15:17:08 +0900

    [Silver I] Title: 경로 찾기, Time: 328 ms, Memory: 23664 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,35 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	public static int[][] dis;
+class Main {
+	static int[][] dis;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(br.readLine());
-
 		dis = new int[N][N];
+		for (int i = 0; i < N; i++)
+			Arrays.fill(dis[i], Integer.MAX_VALUE / 2);
+
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
+
 			for (int j = 0; j < N; j++) {
-				int edge = Integer.parseInt(st.nextToken());
-				dis[i][j] = edge;
+				int input = Integer.parseInt(st.nextToken());
+
+				if (input == 1)
+					dis[i][j] = Math.min(dis[i][j], 1);
 			}
 		}
 
 		for (int K = 0; K < N; K++) {
 			for (int S = 0; S < N; S++) {
 				for (int E = 0; E < N; E++) {
-					if (dis[S][K] == 1 && dis[K][E] == 1)
-						dis[S][E] = 1;
+					if (dis[S][E] > dis[S][K] + dis[K][E]) {
+						dis[S][E] = dis[S][K] + dis[K][E];
+					}
 				}
 			}
 		}
 
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++)
-				System.out.printf("%d ", dis[i][j]);
+			for (int j = 0; j < N; j++) {
+				if (dis[i][j] != Integer.MAX_VALUE / 2)
+					System.out.printf("1 ");
+				else
+					System.out.printf("0 ");
+			}
 			System.out.println();
 		}
 	}
