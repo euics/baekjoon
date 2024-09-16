d5024bedd97c5bbb1e7846e1876a12306269008b
euics <pjh575157@gmail.com>
2024-09-16 15:32:20 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 120 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,27 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	public static int[][] dis;
+class Main {
+	static int[][] dis;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
 		dis = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i <= N; i++)
 			Arrays.fill(dis[i], Integer.MAX_VALUE / 2);
-			dis[i][i] = 0;
-		}
+
+		int M = Integer.parseInt(st.nextToken());
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
-			dis[A][B] = 1;
-			dis[B][A] = 1;
+
+			dis[A][B] = Math.min(dis[A][B], 1);
+			dis[B][A] = Math.min(dis[B][A], 1);
 		}
 
 		for (int K = 1; K <= N; K++) {
@@ -33,18 +32,23 @@ public class Main {
 			}
 		}
 
-		int min = Integer.MAX_VALUE, index = -1;
-		for (int i = 1; i <= N; i++) {
+		int answer = -1;
+		for (int i = 1, min = Integer.MAX_VALUE; i <= N; i++) {
 			int sum = 0;
-			for (int j = 1; j <= N; j++)
-				sum += dis[i][j];
 
-			if (min > sum) {
+			for (int j = 1; j <= N; j++) {
+				if (i == j)
+					continue;
+
+				sum += dis[i][j];
+			}
+
+			if (sum < min) {
 				min = sum;
-				index = i;
+				answer = i;
 			}
 		}
 
-		System.out.println(index);
+		System.out.println(answer);
 	}
 }
