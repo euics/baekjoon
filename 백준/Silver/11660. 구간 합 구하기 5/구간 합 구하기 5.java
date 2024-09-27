6c05f318b52489f4b2db52897e6a585a4ddb833c
euics <pjh575157@gmail.com>
2024-09-27 16:08:58 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 736 ms, Memory: 118940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -6,30 +6,47 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		int[][] arr = new int[n][n];
-		for (int i = 0; i < n; i++) {
+		int[][] sum = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++)
-				arr[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 1; j <= N; j++)
+				sum[i][j] = Integer.parseInt(st.nextToken());
 		}
 
-		int[][] D = new int[n + 1][n + 1];
-		for (int row = 1; row < n + 1; row++) {
-			for (int col = 1; col < n + 1; col++)
-				D[row][col] = D[row - 1][col] + D[row][col - 1] - D[row - 1][col - 1] + arr[row - 1][col - 1];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				sum[i][j] += sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+			}
 		}
 
-		for (int i = 0; i < m; i++) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 
-			System.out.println(D[y2][x2] - D[y1 - 1][x2] - D[y2][x1 - 1] + D[y1 - 1][x1 - 1]);
+			int answer = sum[y2][x2] - sum[y1 - 1][x2] - sum[y2][x1 - 1] + sum[y1 - 1][x1 - 1];
+			sb.append(answer).append("\n");
 		}
+
+		System.out.println(sb.toString());
 	}
 }
+
+/*
+
+1	2	3	4
+2	3	4	5
+3	4	5	6
+4	5	6	7
+
+1	3	6	10
+3	8	15	24
+6
+10
+*/
