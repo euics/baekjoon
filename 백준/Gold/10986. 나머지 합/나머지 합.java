4b1aaec8495d89476c0a893a9835ac4dbc63fc71
euics <pjh575157@gmail.com>
2024-09-27 16:38:51 +0900

    [Gold III] Title: 나머지 합, Time: 448 ms, Memory: 120688 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -2,36 +2,46 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+	static Map<Integer, Integer> map = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		long answer = 0;
-		long[] S = new long[n];
-		long[] C = new long[m];
 		st = new StringTokenizer(br.readLine());
+		long[] S = new long[N];
 		S[0] = Integer.parseInt(st.nextToken());
-
-		for (int i = 1; i < n; i++)
+		for (int i = 1; i < N; i++)
 			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < n; i++) {
-			int remain = (int)(S[i] % m);
+		long answer = 0;
+		long[] C = new long[M];
+		for (int i = 0; i < N; i++) {
+			long remain = S[i] % M;
 
 			if (remain == 0)
 				answer++;
 
-			C[remain]++;
+			C[(int)remain]++;
 		}
 
-		for (int i = 0; i < m; i++) {
-			if (C[i] > 1)
+		for (int i = 0; i < M; i++) {
+			if (C[i] >= 2)
 				answer += (C[i] * (C[i] - 1)) / 2;
 		}
 
 		System.out.println(answer);
 	}
 }
+
+/*
+
+ (Sa - Sb) % M == 0
+ Sa % M == Sb % M
+
+ 1 2 3 1 2
+ 1 3 6 7 9
+
+*/
