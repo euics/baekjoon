0bd8d6fa30d25460c0c0ab49918ac1807beec3ad
euics <pjh575157@gmail.com>
2024-09-30 17:55:48 +0900

    [Silver V] Title: 수들의 합 5, Time: 188 ms, Memory: 53676 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,28 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
+	static int[] arr;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-
-		int[] num = new int[n + 1];
-		for (int i = 0; i <= n; i++)
-			num[i] = i;
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N + 1];
+		for (int i = 1; i <= N; i++)
+			arr[i] = i;
 
 		int start = 1, end = 1, sum = 0, answer = 0;
-		while (start < n + 1) {
-
-			if (sum + num[end] < n)
-				sum += num[end++];
-			else if (sum + num[end] == n) {
-				sum += num[end++];
-				sum -= num[start++];
+		while (start <= N) {
+			if (sum + arr[end] < N)
+				sum += arr[end++];
+			else if (sum + arr[end] == N) {
 				answer++;
+				sum += arr[end++];
+				sum -= arr[start++];
 			} else
-				sum -= num[start++];
+				sum -= arr[start++];
 		}
 
 		System.out.println(answer);
