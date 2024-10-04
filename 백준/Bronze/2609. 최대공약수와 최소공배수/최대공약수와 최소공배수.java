cfeed468b7d01c227d61d2f2deff0ba239c5ae86
euics <pjh575157@gmail.com>
2024-10-04 20:21:17 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,15 +1,25 @@
 import java.io.*;
-import java.math.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        BigInteger a = new BigInteger(st.nextToken());
-        BigInteger b= new BigInteger(st.nextToken());
 
-        System.out.println(a.gcd(b).intValue());
-        System.out.println(b.multiply(a).intValue() / a.gcd(b).intValue());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		System.out.println(GCD(N, M));
+		System.out.println(LCM(N, M));
+	}
+
+	public static int GCD(int a, int b) {
+		if (b == 0)
+			return a;
+		else
+			return GCD(b, a % b);
+	}
+
+	public static int LCM(int a, int b) {
+		return (a * b) / GCD(a, b);
+	}
 }
