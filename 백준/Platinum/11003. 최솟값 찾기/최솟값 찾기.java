1373d954cb74af2eef7720ebbc5fcdacb49bd027
euics <pjh575157@gmail.com>
2024-10-06 20:39:31 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2192 ms, Memory: 728308 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,27 +1,29 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
-class Main {
-	public static void main(String[] args) throws Exception {
+public class Main {
+	static Deque<int[]> dq = new LinkedList<>();
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
 
-		Deque<int[]> dq = new LinkedList<>();
-		st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			int now = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
 
-			while (!dq.isEmpty() && i - dq.peekFirst()[0] + 1 > L)
+			while (!dq.isEmpty() && i - dq.peekFirst()[0] + 1 > L) {
 				dq.pollFirst();
-			while (!dq.isEmpty() && dq.peekLast()[1] > now)
+			}
+
+			while (!dq.isEmpty() && dq.peekLast()[1] > num) {
 				dq.pollLast();
+			}
 
-			dq.addLast(new int[] {i, now});
-
+			dq.addLast(new int[] {i, num});
 			sb.append(dq.peekFirst()[1]).append(" ");
 		}
 
