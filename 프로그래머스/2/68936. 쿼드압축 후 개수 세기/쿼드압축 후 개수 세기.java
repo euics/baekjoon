39faf40e3b9d1b83efca7edff55ab56a231b8762
euics <pjh575157@gmail.com>
2024-10-07 20:23:59 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 9.95 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -3,16 +3,17 @@ class Solution {
 
 	public int[] solution(int[][] arr) {
 		quad(arr, 0, 0, arr.length);
-
+		
 		return answer;
 	}
 
 	public void quad(int[][] arr, int x, int y, int size) {
 		if (zip(arr, x, y, size, arr[y][x])) {
-			if (arr[y][x] == 0)
+			if (arr[y][x] == 0) {
 				answer[0]++;
-			else
+			} else {
 				answer[1]++;
+			}
 
 			return;
 		}
@@ -26,8 +27,9 @@ class Solution {
 	public boolean zip(int[][] arr, int x, int y, int size, int val) {
 		for (int i = y; i < y + size; i++) {
 			for (int j = x; j < x + size; j++) {
-				if (arr[i][j] != val)
+				if (arr[i][j] != val) {
 					return false;
+				}
 			}
 		}
 
