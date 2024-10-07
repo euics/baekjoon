2eb58cb194ec2326bf80da7a6549b559daada06a
euics <pjh575157@gmail.com>
2024-10-07 20:51:17 +0900

    [level 2] Title: 모음 사전, Time: 10.48 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,35 @@
 import java.util.*;
 
 class Solution {
-    static String vowels = "AEIOU";
-    static List<String> dictionary = new ArrayList<>();
-    
-    public int solution(String word) {
-        int answer = 0;
-        
-        for(int length = 1; length <= 5; length++) {
-            char[] permutation = new char[length];
-            permutation(0, length, permutation);
-        }
-        
-        Collections.sort(dictionary);
-        return dictionary.indexOf(word) + 1;
-    }
-    
-    public void permutation(int L, int length, char[] permutation) {
-        if(L == length) {
-            dictionary.add(new String(permutation));
-            
-            return;
-        }
-        
-        for(int i = 0; i < vowels.length(); i++) {
-            permutation[L] = vowels.charAt(i);
-            permutation(L + 1, length, permutation);
-        }
-    }
+	static ArrayList<String> dictionary = new ArrayList<>();
+	static char[] dfs;
+
+	public int solution(String word) {
+		for (int depth = 1; depth <= 5; depth++) {
+			dfs = new char[depth];
+			dictionary(0, depth, new char[] {'A', 'E', 'I', 'O', 'U'});
+		}
+
+		Collections.sort(dictionary);
+
+		return dictionary.indexOf(word) + 1;
+	}
+
+	public void dictionary(int L, int depth, char[] vowels) {
+		if (L == depth) {
+			StringBuilder sb = new StringBuilder();
+			for (char x : dfs) {
+				sb.append(x);
+			}
+
+			dictionary.add(sb.toString());
+
+			return;
+		}
+
+		for (int i = 0; i < 5; i++) {
+			dfs[L] = vowels[i];
+			dictionary(L + 1, depth, vowels);
+		}
+	}
 }
