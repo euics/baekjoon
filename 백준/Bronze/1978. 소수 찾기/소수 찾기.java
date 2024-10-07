0f1cd7fde1712e2d432f89a2dc04de154fc655fb
euics <pjh575157@gmail.com>
2024-10-07 16:55:51 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -2,46 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(arr[i], max);
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-        List<Integer> prime = findPrime(max);
-        int cnt = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(prime.contains(arr[i]))
-                cnt++;
-        }
+		int cnt = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			if (isPrime(Integer.parseInt(st.nextToken()))) {
+				cnt++;
+			}
+		}
 
-        System.out.println(cnt);
-    }
+		System.out.println(cnt);
+	}
 
-    private static List<Integer> findPrime(int max){
-        boolean[] bool = new boolean[max + 1];
-        for(int i = 1; i <= max; i++)
-            bool[i] = true;
-        bool[1] = false;
+	public static boolean isPrime(int n) {
+		if (n <= 1)
+			return false;
+		if (n == 2)
+			return true;
+		if (n % 2 == 0)
+			return false;
 
-        for(int i = 2; i * i <= max; i++){
-            for(int j = i * i; j <= max; j += i){
-                bool[j] = false;
-            }
-        }
+		for (int i = 3; i * i <= n; i += 2) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
 
-        List<Integer> list = new LinkedList<>();
-        for(int i = 1; i <= max; i++){
-            if(bool[i])
-                list.add(i);
-        }
-
-        return list;
-    }
+		return true;
+	}
 }
