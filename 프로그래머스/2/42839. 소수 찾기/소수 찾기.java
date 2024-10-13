5e78b85f5b2a4c514da3c1214bbdfc8bfa2ddca7
euics <pjh575157@gmail.com>
2024-10-13 19:51:19 +0900

    [level 2] Title: 소수 찾기, Time: 30.76 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,38 +3,38 @@ import java.util.Set;
 
 class Solution {
 	public int solution(String numbers) {
-		int answer = 0;
-
 		Set<Integer> permutation = new HashSet<>();
 		permutation("", numbers, permutation);
-		for (int num : permutation) {
-			if (isPrime(num))
-				answer++;
-		}
-		
-		return answer;
+
+		return (int)permutation.stream().filter(this::isPrime).count();
 	}
 
 	public void permutation(String prefix, String str, Set<Integer> permutation) {
-		if (!prefix.equals(""))
+		if (!prefix.equals("")) {
 			permutation.add(Integer.parseInt(prefix));
+		}
 
 		int n = str.length();
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++) {
 			permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), permutation);
+		}
 	}
 
-	public boolean isPrime(int num) {
-		if (num <= 1)
+	public boolean isPrime(int n) {
+		if (n <= 1) {
 			return false;
-		if (num == 2)
+		}
+		if (n == 2) {
 			return true;
-		if (num % 2 == 0)
+		}
+		if (n % 2 == 0) {
 			return false;
+		}
 
-		for (int i = 3; i * i <= num; i += 2) {
-			if (num % i == 0)
+		for (int i = 3; i * i <= n; i += 2) {
+			if (n % i == 0) {
 				return false;
+			}
 		}
 
 		return true;
