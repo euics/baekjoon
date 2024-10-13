c5f99d2cba53a8ab52b5d92fe3667486d5eb2ad1
euics <pjh575157@gmail.com>
2024-10-13 15:37:31 +0900

    [level 1] Title: 모의고사, Time: 4.16 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,27 +1,38 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    static int[] first = new int[]{1, 2, 3, 4, 5}; // 5개
-    static int[] second = new int[]{2, 1, 2, 3, 2, 4, 2, 5}; // 8개
-    static int[] thrid = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10개
-    
-    public int[] solution(int[] answers) {
-        int[] peoples = new int[3];
-        
-        for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == first[i % 5]) peoples[0]++;
-            if(answers[i] == second[i % 8]) peoples[1]++;
-            if(answers[i] == thrid[i % 10]) peoples[2]++;
-        }
-        
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < 3; i++) max = Math.max(max, peoples[i]);
-        List<Integer> answer = new ArrayList<>();
-        for(int i = 0; i < 3; i++) {
-            if(max == peoples[i]) answer.add(i + 1);
-        }
-        Collections.sort(answer);
-        
-        return answer.stream().mapToInt(i -> i).toArray();
-    }
+	static List<Integer> answer = new ArrayList<>();
+	static int max = Integer.MIN_VALUE;
+	static int[] cnt = new int[3];
+	private final int[] one = {1, 2, 3, 4, 5};
+	private final int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+	private final int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+	public int[] solution(int[] answers) {
+		for (int i = 0; i < answers.length; i++) {
+			if (answers[i] == one[i % 5]) {
+				cnt[0]++;
+			}
+
+			if (answers[i] == two[i % 8]) {
+				cnt[1]++;
+			}
+
+			if (answers[i] == three[i % 10]) {
+				cnt[2]++;
+			}
+		}
+
+		max = Math.max(cnt[0], cnt[1]);
+		max = Math.max(max, cnt[2]);
+
+		for (int i = 0; i < 3; i++) {
+			if (cnt[i] == max) {
+				answer.add(i + 1);
+			}
+		}
+
+		return answer.stream().mapToInt(i -> i).toArray();
+	}
 }
