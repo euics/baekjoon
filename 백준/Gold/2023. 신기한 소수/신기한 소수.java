1d230649fa1ba962857e472e5c9e7c7d0b050554
euics <pjh575157@gmail.com>
2024-10-14 11:04:30 +0900

    [Gold V] Title: 신기한 소수, Time: 96 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,48 +1,56 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	public static void main(String[] args) throws Exception {
+public class Main {
+
+	static List<Integer> list = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(br.readLine());
-		List<Integer> answer = new ArrayList<>();
-
-		DFS(0, 0, N, answer);
+		DFS(0, N, 0);
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < answer.size(); i++)
-			sb.append(answer.get(i)).append("\n");
+		for (int num : list) {
+			sb.append(num).append("\n");
+		}
+
 		System.out.println(sb.toString());
 	}
 
 	public static boolean isPrime(int n) {
-		if (n <= 1)
+		if (n <= 1) {
 			return false;
-		if (n == 2)
+		}
+		if (n == 2) {
 			return true;
-		if (n % 2 == 0)
+		}
+		if (n % 2 == 0) {
 			return false;
+		}
 
 		for (int i = 3; i * i <= n; i += 2) {
-			if (n % i == 0)
+			if (n % i == 0) {
 				return false;
+			}
 		}
 
 		return true;
 	}
 
-	public static void DFS(int L, int num, int N, List<Integer> answer) {
-		if (L == N) {
-			answer.add(num);
+	public static void DFS(int L, int n, int num) {
+		if (L == n) {
+			list.add(num);
 
 			return;
 		}
 
 		for (int i = 1; i <= 9; i++) {
-			int newNum = num * 10 + i;
-			if (isPrime(newNum))
-				DFS(L + 1, newNum, N, answer);
+			int newNum = 10 * num + i;
+			if (isPrime(newNum)) {
+				DFS(L + 1, n, newNum);
+			}
 		}
 	}
 }
