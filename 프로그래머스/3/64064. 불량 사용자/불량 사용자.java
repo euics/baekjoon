5f3a24a565c854a976376ab96206607de1b2f8ae
euics <pjh575157@gmail.com>
2024-10-14 14:09:46 +0900

    [level 3] Title: 불량 사용자, Time: 505.41 ms, Memory: 384 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -2,28 +2,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
+	static Set<Set<String>> result = new HashSet<>();
+
 	public int solution(String[] user_id, String[] banned_id) {
-		DFS(0, user_id, banned_id, new boolean[user_id.length], new HashSet<>());
+		DFS(0, user_id, banned_id, new HashSet<String>());
+
 		return result.size();
 	}
 
-	static Set<Set<String>> result = new HashSet<>();
-
-	public void DFS(int L, String[] user_id, String[] banned_id, boolean[] bool, Set<String> cur) {
+	public void DFS(int L, String[] user_id, String[] banned_id, Set<String> set) {
 		if (L == banned_id.length) {
-			result.add(new HashSet<>(cur));
+			result.add(new HashSet<>(set));
 
 			return;
 		}
 
-		String pattern = banned_id[L].replace('*', '.');
+		String pattern = banned_id[L].replace("*", ".");
 		for (int i = 0; i < user_id.length; i++) {
-			if (!bool[i] && user_id[i].matches(pattern)) {
-				bool[i] = true;
-				cur.add(user_id[i]);
-				DFS(L + 1, user_id, banned_id, bool, cur);
-				bool[i] = false;
-				cur.remove(user_id[i]);
+			if (user_id[i].matches(pattern) && !set.contains(user_id[i])) {
+				set.add(user_id[i]);
+				DFS(L + 1, user_id, banned_id, set);
+				set.remove(user_id[i]);
 			}
 		}
 	}
