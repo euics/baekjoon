365e9127e1e34312ef3cf812561bdfb144a39fc7
euics <pjh575157@gmail.com>
2024-10-16 20:29:05 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 31.36 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,55 +1,56 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-	public String[] solution(String[] orders, int[] course) {
+	static ArrayList<String> answer = new ArrayList<>();
+	static Map<String, Integer> map = new HashMap<>();
+
+	public String[] solution(String[] orders, int[] courses) {
 		for (int i = 0; i < orders.length; i++) {
-			char[] order = orders[i].toCharArray();
-			Arrays.sort(order);
-			orders[i] = new String(order);
+			char[] ch = orders[i].toCharArray();
+			Arrays.sort(ch);
+
+			orders[i] = new String(ch);
 		}
 
-		Map<String, Integer> map = new HashMap<>();
-		for (String order : orders)
-			combination(0, order, "", map);
+		for (String order : orders) {
+			DFS(0, order, "");
+		}
 
-		List<String> answer = new ArrayList<>();
-		for (int len : course) {
-			List<String> candidate = new ArrayList<>();
-			int max = 2;
+		for (int course : courses) {
+			int maxOrder = 2;
+			ArrayList<String> candidate = new ArrayList<>();
 
-			for (String combinationKey : map.keySet()) {
-				if (len == combinationKey.length() && max <= map.get(combinationKey)) {
-					if (max < map.get(combinationKey)) {
-						max = map.get(combinationKey);
-						candidate.clear();
+			for (String keySet : map.keySet()) {
+				if (keySet.length() == course) {
+					if (map.get(keySet) >= maxOrder) {
+						if (map.get(keySet) > maxOrder) {
+							candidate.clear();
+							maxOrder = map.get(keySet);
+						}
+
+						candidate.add(keySet);
 					}
-
-					candidate.add(combinationKey);
 				}
 			}
 
 			answer.addAll(candidate);
 		}
-		
+
 		Collections.sort(answer);
 
 		return answer.stream().toArray(String[]::new);
 	}
 
-	public void combination(int L, String order, String cur, Map<String, Integer> map) {
+	public void DFS(int L, String order, String cur) {
 		if (L == order.length()) {
-			if (cur.length() >= 2)
+			if (cur.length() >= 2) {
 				map.put(cur, map.getOrDefault(cur, 0) + 1);
+			}
 
 			return;
 		}
 
-		combination(L + 1, order, cur + order.charAt(L), map);
-		combination(L + 1, order, cur, map);
+		DFS(L + 1, order, cur + order.charAt(L));
+		DFS(L + 1, order, cur);
 	}
 }
