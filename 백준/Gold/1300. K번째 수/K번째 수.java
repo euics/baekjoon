9a92ace50f7b66c5d761015be74af1f527b817a6
euics <pjh575157@gmail.com>
2024-10-16 10:22:14 +0900

    [Gold I] Title: K번째 수, Time: 140 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,27 +1,28 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-
-	public static void main(String[] args) throws Exception {
+public class Main {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 		long N = Long.parseLong(br.readLine());
-		long k = Long.parseLong(br.readLine());
+		long K = Long.parseLong(br.readLine());
 
-		long start = 1, end = N * N, answer = -1;
+		long start = 1, end = N * N, answer = Long.MAX_VALUE;
 		while (start <= end) {
 			long mid = (start + end) / 2;
-
 			long sum = 0;
-			for (int i = 1; i <= N; i++)
-				sum += Math.min(mid / i, N);
 
-			if (sum >= k) {
-				answer = mid;
+			for (long i = 1; i <= N; i++) {
+				sum += Math.min(mid / i, N);
+			}
+
+			if (sum >= K) {
+				answer = Math.min(answer, mid);
 				end = mid - 1;
-			} else
+			} else {
 				start = mid + 1;
+			}
 		}
 
 		System.out.println(answer);
