ce87c053090b1e244dcb3a21d3f41afaa759433f
euics <pjh575157@gmail.com>
2024-10-16 19:32:17 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.20 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,12 +1,16 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
 	public String[] solution(String[] strings, int n) {
-		Arrays.sort(strings, (a, b) -> {
-			if (a.charAt(n) != b.charAt(n)) {
-				return a.charAt(n) - b.charAt(n);
-			} else {
-				return a.compareTo(b);
+		Arrays.sort(strings, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if (o1.charAt(n) == o2.charAt(n)) {
+					return o1.compareTo(o2);
+				} else {
+					return o1.charAt(n) - o2.charAt(n);
+				}
 			}
 		});
 		
