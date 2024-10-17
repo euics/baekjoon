4a9ca5adbb37abb66b41256077a3a396c9dda9a4
euics <pjh575157@gmail.com>
2024-10-17 15:41:03 +0900

    [Gold IV] Title: 수 묶기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,38 +1,59 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
+public class Main {
+	static int ones = 0, answer = 0;
+	static ArrayList<Integer> positive = new ArrayList<>();
+	static ArrayList<Integer> negative = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-
-		PriorityQueue<Integer> positiveNum = new PriorityQueue<>(Collections.reverseOrder());
-		PriorityQueue<Integer> negativeNum = new PriorityQueue<>();
-
-		int answer = 0;
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
 			int input = Integer.parseInt(br.readLine());
 
-			if (input > 1)
-				positiveNum.add(input);
-			else if (input == 1)
-				answer += 1;
-			else
-				negativeNum.add(input);
+			if (input > 1) {
+				positive.add(input);
+			} else if (input == 1) {
+				ones++;
+			} else {
+				negative.add(input);
+			}
 		}
 
-		while (!positiveNum.isEmpty() && positiveNum.size() >= 2)
-			answer += positiveNum.poll() * positiveNum.poll();
-		while (!positiveNum.isEmpty())
-			answer += positiveNum.poll();
+		Collections.sort(positive, Collections.reverseOrder());
+		Collections.sort(negative);
 
-		while (!negativeNum.isEmpty() && negativeNum.size() >= 2)
-			answer += negativeNum.poll() * negativeNum.poll();
-		while (!negativeNum.isEmpty())
-			answer += negativeNum.poll();
+		for (int i = 0; i < positive.size(); i++) {
+			if (i + 1 < positive.size()) {
+				answer += positive.get(i) * positive.get(i + 1);
+				i++;
+			} else {
+				answer += positive.get(i);
+			}
+		}
 
+		for (int i = 0; i < negative.size(); i++) {
+			if (i + 1 < negative.size()) {
+				answer += negative.get(i) * negative.get(i + 1);
+				i++;
+			} else {
+				answer += negative.get(i);
+			}
+		}
+
+		answer += ones;
 		System.out.println(answer);
 	}
 }
+
+/*
+ 3 2 1 -1
+
+ 5 4 3 1 0 -1 -2
+
+ 5 4 1 0 -1 -2
+
+ 20 26
+*/
