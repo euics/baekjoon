faa1b046b73524a1cbc938f40d18ff2dd709020a
euics <pjh575157@gmail.com>
2024-10-23 16:21:37 +0900

    [level 2] Title: 올바른 괄호, Time: 17.68 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,17 +1,21 @@
 import java.util.Stack;
 
 class Solution {
-    boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(char ch : s.toCharArray()) {
-            if(ch == '(') stack.push('(');
-            else {
-                if(stack.isEmpty()) return false;
-                
-                stack.pop();
-            }
-        }
-        
-        return stack.size() != 0 ? false : true;
-    }
+	static Stack<Character> stack = new Stack<>();
+
+	boolean solution(String s) {
+		for (char ch : s.toCharArray()) {
+			if (ch == '(') {
+				stack.push(ch);
+			} else {
+				if (stack.isEmpty()) {
+					return false;
+				}
+
+				stack.pop();
+			}
+		}
+		
+		return stack.isEmpty() ? true : false;
+	}
 }
