433401a304858a79f447731d17174919e8d047ce
euics <pjh575157@gmail.com>
2024-10-23 15:42:21 +0900

    [level 4] Title: 징검다리, Time: 50.10 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,35 +1,45 @@
 import java.util.*;
 
 class Solution {
+	static int answer = -1;
 
 	public int solution(int distance, int[] rocks, int n) {
 		Arrays.sort(rocks);
 
-		int left = 0, right = distance, answer = Integer.MIN_VALUE;
-		while (left <= right) {
-			int mid = (left + right) / 2;
+		int start = 1, end = distance;
+		while (start <= end) {
+			int mid = (start + end) / 2;
 			int remove = 0;
 
-			int pre = 0;
-			for(int i = 0; i < rocks.length; i++) {
-				if(Math.abs(rocks[i] - pre) < mid) remove++;
-				else pre = rocks[i];
+			int prev = 0;
+			for (int i = 0; i < rocks.length; i++) {
+				if (Math.abs(rocks[i] - prev) < mid) {
+					remove++;
+				} else {
+					prev = rocks[i];
+				}
 			}
 
-			if(Math.abs(distance - pre) < mid) remove++;
+			if (Math.abs(prev - distance) < mid) {
+				remove++;
+			}
 
-			if(remove <= n) {
+			if (remove <= n) {
 				answer = Math.max(answer, mid);
-				left = mid + 1;
-			} else right = mid - 1;
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
 		}
-		
+
 		return answer;
 	}
 }
 
 /*
- 0 ~ 2 2 ~ 11 11 ~ 14 14 ~ 17 17 ~ 21 21 ~ 25
- 2 9 3 3 4 4
+
+ 0 2 11 14 17 21 25
+ 0 2 11 14 25
+ 2 9 3 11
 
 */
