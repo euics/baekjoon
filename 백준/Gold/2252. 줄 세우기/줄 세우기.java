26a35c1d11d6af50d764c95cb2430b1b8ea1d846
euics <pjh575157@gmail.com>
2024-11-10 22:07:36 +0900

    [Gold III] Title: 줄 세우기, Time: 436 ms, Memory: 47672 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,31 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static ArrayList<ArrayList<Integer>> graph;
 	static int[] inDegree;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		inDegree = new int[n + 1];
-		for (int i = 0; i <= n; i++) {
+		int N = Integer.parseInt(st.nextToken());
+		graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
+		inDegree = new int[N + 1];
 
-		int m = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < m; i++) {
+		int M = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
 
-			graph.get(a).add(b);
-			inDegree[b]++;
+			graph.get(A).add(B);
+			inDegree[B]++;
 		}
 
 		Queue<Integer> q = new LinkedList<>();
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= N; i++) {
 			if (inDegree[i] == 0) {
 				q.add(i);
 			}
@@ -33,17 +34,18 @@ public class Main {
 
 		StringBuilder sb = new StringBuilder();
 		while (!q.isEmpty()) {
-			int cur = q.poll();
-			sb.append(cur).append(" ");
+			int prev = q.poll();
+			sb.append(prev).append(" ");
 
-			for (int next : graph.get(cur)) {
-				inDegree[next]--;
-				if (inDegree[next] == 0) {
-					q.add(next);
+			for (int cur : graph.get(prev)) {
+				inDegree[cur]--;
+
+				if (inDegree[cur] == 0) {
+					q.add(cur);
 				}
 			}
 		}
 
-		System.out.println(sb.toString().trim());
+		System.out.println(sb.toString());
 	}
 }
