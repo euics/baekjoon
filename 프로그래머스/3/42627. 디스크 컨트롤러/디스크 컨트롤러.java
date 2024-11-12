5b19e8a46d882b45f7ef5c386b6035fcecc0227f
euics <pjh575157@gmail.com>
2024-11-12 12:35:57 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.23 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,27 +1,28 @@
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
-    public int solution(int[][] jobs) {
-        PriorityQueue<int[]> programs = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        for(int i = 0; i < jobs.length; i++) programs.add(new int[]{jobs[i][0], jobs[i][1]});
+	public int solution(int[][] jobs) {
+		Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        int finishTime = 0, totalTime = 0;
-        while(!programs.isEmpty() || !pq.isEmpty()) {
-            if(pq.isEmpty()) finishTime = Math.max(finishTime, programs.peek()[0]);
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
-            while(!programs.isEmpty() && programs.peek()[0] <= finishTime) {
-                int[] possibleProgram = programs.poll();
-                pq.add(new int[]{possibleProgram[0], possibleProgram[1]});
-            }
+		int idx = 0, cnt = 0, sum = 0, end = 0;
+		while (cnt < jobs.length) {
 
-            int[] cur = pq.poll();
-            finishTime += cur[1];
-            totalTime += finishTime - cur[0];
-        }
+			while (idx < jobs.length && jobs[idx][0] <= end) {
+				pq.add(jobs[idx++]);
+			}
 
-        return totalTime / jobs.length;
-    }
+			if (pq.isEmpty()) {
+				end = jobs[idx][0];
+			} else {
+				int[] cur = pq.poll();
+				sum += end + cur[1] - cur[0];
+				end += cur[1];
+				cnt++;
+			}
+		}
+
+		return sum / jobs.length;
+	}
 }
