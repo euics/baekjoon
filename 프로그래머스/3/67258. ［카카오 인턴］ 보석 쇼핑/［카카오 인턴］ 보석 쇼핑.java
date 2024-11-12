56467f37673557ce15cae035b41d0d927b1b6c79
euics <pjh575157@gmail.com>
2024-11-12 12:52:30 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 78.11 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,32 +1,28 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
+	static HashMap<String, Integer> map = new HashMap<>();
+	static Set<String> set = new HashSet<>();
+
 	public int[] solution(String[] gems) {
 		int[] answer = new int[2];
 
-		Set<String> types = new HashSet<>();
-		for (String gem : gems)
-			types.add(gem);
+		set.addAll(Arrays.asList(gems));
 
-		int start = 0, end = 0, length = Integer.MAX_VALUE;
-		Map<String, Integer> map = new HashMap<>();
-
+		int start = 0, end = 0, min = Integer.MAX_VALUE;
 		while (end < gems.length) {
 			map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
 
-			while (map.size() == types.size()) {
-				if (end - start + 1 < length) {
-					length = end - start + 1;
-					answer[0] = start + 1;
-					answer[1] = end + 1;
+			while (start <= end && set.size() == map.keySet().size()) {
+				if (end - start + 1 < min) {
+					min = end - start + 1;
+					answer = new int[] {start + 1, end + 1};
 				}
 
 				map.put(gems[start], map.get(gems[start]) - 1);
-				if (map.get(gems[start]) == 0)
+				if (map.get(gems[start]) == 0) {
 					map.remove(gems[start]);
+				}
 
 				start++;
 			}
