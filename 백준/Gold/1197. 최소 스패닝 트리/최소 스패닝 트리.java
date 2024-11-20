5217947a6a943206f953f191deef66748c0525c4
euics <pjh575157@gmail.com>
2024-11-20 16:21:51 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 500 ms, Memory: 50408 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,19 +1,21 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
+public class Main {
+	static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
 	static int[] unf;
+	static int sum = 0;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(st.nextToken());
 		unf = new int[V + 1];
-		for (int i = 1; i <= V; i++)
+		for (int i = 0; i <= V; i++) {
 			unf[i] = i;
+		}
 
 		int E = Integer.parseInt(st.nextToken());
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
@@ -23,17 +25,16 @@ class Main {
 			pq.add(new int[] {A, B, C});
 		}
 
-		int answer = 0;
 		while (!pq.isEmpty()) {
 			int[] cur = pq.poll();
 
 			if (find(cur[0]) != find(cur[1])) {
 				union(cur[0], cur[1]);
-				answer += cur[2];
+				sum += cur[2];
 			}
 		}
 
-		System.out.println(answer);
+		System.out.println(sum);
 	}
 
 	public static int find(int v) {
@@ -47,7 +48,8 @@ class Main {
 		int fa = find(a);
 		int fb = find(b);
 
-		if (fa != fb)
+		if (fa != fb) {
 			unf[fa] = fb;
+		}
 	}
 }
