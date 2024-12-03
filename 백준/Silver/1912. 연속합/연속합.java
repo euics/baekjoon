043390d77bf8c3f8b576e9672b0e74f6f33263ea
euics <pjh575157@gmail.com>
2024-12-03 14:55:57 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 23644 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,22 +1,24 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int[] arr;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		arr = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
 
-		System.out.println(kadaneAlgorithm(arr));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(kadane(arr));
 	}
 
-	public static int kadaneAlgorithm(int[] arr) {
+	public static int kadane(int[] arr) {
 		int maxSum = arr[0];
 		int curSum = arr[0];
 
