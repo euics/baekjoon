8a36ba77eeda8be0be4754ef4ec81400a784005a
euics <pjh575157@gmail.com>
2024-12-03 16:20:22 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,46 +1,49 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
-    static int[] unf, groupSize;
-    
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        
-        init(n);
-        for(int index = 0; index < n; index++) {
-            for(int computer = 0; computer < n; computer++) {
-                if(index == computer) continue;
-                
-                if(computers[index][computer] == 1) union(index, computer);
-            }
-        }
-        
-        for(int computer = 0; computer < n; computer++) {
-            if(computer == find(computer)) answer++;
-        }
-        
-        return answer;
-    }
-    
-    public void init(int n) {
-        unf = new int[n];
-        groupSize = new int[n];
-        for(int i = 0; i < n; i++) {
-            unf[i] = i;
-            groupSize[i] = 1;
-        }
-    }
-    
-    public int find(int v) {
-        if(v == unf[v]) return unf[v];
-        else return unf[v] = find(unf[v]);
-    }
-    
-    public void union(int a, int b) {
-        int fa = find(a);
-        int fb = find(b);
-        
-        if(fa != fb) {
-            unf[fa] = fb;
-            groupSize[fb] += groupSize[fa];
-        }
-    }
+	static int[] unf;
+	static Set<Integer> root = new HashSet<>();
+
+	public int solution(int n, int[][] computers) {
+		unf = new int[n];
+		for (int i = 0; i < n; i++) {
+			unf[i] = i;
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == j) {
+					continue;
+				}
+
+				if (computers[i][j] == 1) {
+					union(i, j);
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			root.add(find(i));
+		}
+
+		return root.size();
+	}
+
+	public int find(int v) {
+		if (v == unf[v]) {
+			return unf[v];
+		} else {
+			return unf[v] = find(unf[v]);
+		}
+	}
+
+	public void union(int a, int b) {
+		int fa = find(a);
+		int fb = find(b);
+
+		if (fa != fb) {
+			unf[fa] = fb;
+		}
+	}
 }
