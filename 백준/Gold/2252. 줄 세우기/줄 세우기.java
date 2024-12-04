3c0ea76be4d32c2a39385a5bfad39a88c0dba9d0
euics <pjh575157@gmail.com>
2024-12-04 15:20:33 +0900

    [Gold III] Title: 줄 세우기, Time: 408 ms, Memory: 45660 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,19 +1,20 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static ArrayList<ArrayList<Integer>> graph;
 	static int[] inDegree;
+	static StringBuilder sb = new StringBuilder();
+	static Queue<Integer> q = new LinkedList<>();
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		graph = new ArrayList<>();
+		inDegree = new int[N + 1];
 		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
-		inDegree = new int[N + 1];
 
 		int M = Integer.parseInt(st.nextToken());
 		for (int i = 0; i < M; i++) {
@@ -25,23 +26,21 @@ public class Main {
 			inDegree[B]++;
 		}
 
-		Queue<Integer> q = new LinkedList<>();
 		for (int i = 1; i <= N; i++) {
 			if (inDegree[i] == 0) {
 				q.add(i);
 			}
 		}
 
-		StringBuilder sb = new StringBuilder();
 		while (!q.isEmpty()) {
-			int prev = q.poll();
-			sb.append(prev).append(" ");
+			int cur = q.poll();
+			sb.append(cur).append(" ");
 
-			for (int cur : graph.get(prev)) {
-				inDegree[cur]--;
+			for (int next : graph.get(cur)) {
+				inDegree[next]--;
 
-				if (inDegree[cur] == 0) {
-					q.add(cur);
+				if (inDegree[next] == 0) {
+					q.add(next);
 				}
 			}
 		}
