2fb9aa792d0206e09384d690ae894b26c2543bc7
euics <pjh575157@gmail.com>
2024-12-11 15:40:26 +0900

    [level 3] Title: 단속카메라, Time: 9.68 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,32 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] routes) {
-        PriorityQueue<int[]> pq = initQueue(routes);
-        
-        int camera = Integer.MIN_VALUE;
-        int answer = 0;
-        while(!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            
-            if(cur[0] > camera) {
-                camera = cur[1];
-                answer++;
-            }
-        }
-        
-        return answer;
-    }
-    
-    public PriorityQueue<int[]> initQueue(int[][] routes) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        for(int[] route : routes) pq.add(new int[]{route[0], route[1]});
-        
-        return pq;
-    }
+	static int answer = 0;
+	static int camera = Integer.MIN_VALUE;
+
+	public int solution(int[][] routes) {
+		Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+
+		for (int[] route : routes) {
+			if (route[0] > camera) {
+				camera = route[1];
+				answer++;
+			}
+		}
+
+		return answer;
+	}
 }
 
-// -20 -15
-// -18 -13
-// -14 -5
-// -5  -3
+/*
+ [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
+  -20 	-15
+  	-18		  -13
+  			-14		-5
+  					-5		-3
+*/
