7921c93efdc6989c9de636ed79ab5d33d59f31e5
euics <pjh575157@gmail.com>
2025-01-12 14:21:59 +0900

    [level 2] Title: 할인 행사, Time: 47.43 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,31 +1,47 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
-        int n = want.length;
-        int m = discount.length;
+	static int answer = 0;
 
-        for(int i = 0; i <= m - 10; i++){
-            Map<String, Integer> discountInfo = new HashMap<>();
-            int cnt = 0, index = i;
-            while(index < m && cnt < 10) {
-                discountInfo.put(discount[index], discountInfo.getOrDefault(discount[index], 0) + 1);
-                index++;
-                cnt++;
-            }
+	public int solution(String[] want, int[] number, String[] discount) {
+		Map<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < 10; i++) {
+			map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
+		}
 
-            boolean bool = true;
-            for(int j = 0; j < n; j++) {
-                if(!discountInfo.containsKey(want[j]) || discountInfo.get(want[j]) < number[j]) {
-                    bool = false;
-                    break;
-                }
-            }
+		if (buyMembership(map, want, number)) {
+			answer++;
+		}
 
-            if(bool) answer++;
-        }
+		if (discount.length > 10) {
+			for (int i = 10; i < discount.length; i++) {
+				map.put(discount[i - 10], map.get(discount[i - 10]) - 1);
+				if (map.get(discount[i - 10]) == 0) {
+					map.remove(discount[i - 10]);
+				}
 
-        return answer;
-    }
+				map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
+
+				if (buyMembership(map, want, number)) {
+					answer++;
+				}
+			}
+		}
+
+		return answer;
+	}
+
+	public boolean buyMembership(Map<String, Integer> map, String[] want, int[] number) {
+		for (int i = 0; i < want.length; i++) {
+			if (!map.containsKey(want[i])) {
+				return false;
+			} else {
+				if (map.get(want[i]) < number[i]) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
 }
