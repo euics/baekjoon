835254fe06b934f074842ac117e1926ba649c3b2
euics <pjh575157@gmail.com>
2025-01-14 19:39:16 +0900

    [level 2] Title: 영어 끝말잇기, Time: 7.79 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,24 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = {0, 0};
+	static Set<String> set = new HashSet<>();
 
-        Set<String> set = new HashSet<>();
-        set.add(words[0]);
-        for(int i = 1; i < words.length; i++){
-            
-            String prevWord = words[i - 1];
-            String curWord = words[i];
-            
-            if(!set.contains(words[i]) && prevWord.charAt(prevWord.length() - 1) == curWord.charAt(0)){
-                set.add(curWord);
-            }
-            else {
-                return new int[]{i % n + 1 , i / n + 1};
-            }
-        }
+	public static void main(String[] args) {
+		Solution T = new Solution();
+		System.out.println(
+			Arrays.toString(T.solution(2, new String[] {"hello", "one", "even", "never", "now", "world", "draw"}))
+		);
+	}
 
-        return answer;
-    }
+	public int[] solution(int n, String[] words) {
+		String prev = "", cur = "";
+
+		for (int i = 0; i < words.length / n; i++) {
+			for (int j = 0; j < n; j++) {
+				cur = words[n * i + j];
+
+				if (i != 0 || j != 0) {
+					if (prev.charAt(prev.length() - 1) != cur.charAt(0)) {
+						return new int[] {j + 1, i + 1};
+					}
+				}
+
+				if (set.contains(cur)) {
+					return new int[] {j + 1, i + 1};
+				} else {
+					set.add(cur);
+				}
+
+				System.out.printf("%s %s\n", prev, cur);
+
+				prev = cur;
+			}
+		}
+
+		return new int[] {0, 0};
+	}
 }
