d366b962030d2b1144cd57ad4b06071a21230a0c
euics <pjh575157@gmail.com>
2025-01-15 17:53:57 +0900

    [Gold IV] Title: 최단경로, Time: 636 ms, Memory: 108132 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,24 +1,24 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static ArrayList<ArrayList<Edge>> graph;
+public class Main {
 	static int[] dis;
+	static StringBuilder sb = new StringBuilder();
+	static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int start = Integer.parseInt(br.readLine());
-
-		graph = new ArrayList<>();
-		for (int i = 0; i <= V; i++)
-			graph.add(new ArrayList<Edge>());
 		dis = new int[V + 1];
 		Arrays.fill(dis, Integer.MAX_VALUE);
+		for (int i = 0; i <= V; i++) {
+			graph.add(new ArrayList<Edge>());
+		}
+		int E = Integer.parseInt(st.nextToken());
+		int src = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < K; i++) {
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
@@ -27,31 +27,34 @@ class Main {
 			graph.get(u).add(new Edge(v, w));
 		}
 
-		dijkstra(start);
-
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i <= V; i++) {
-			if(dis[i] == Integer.MAX_VALUE) sb.append("INF").append("\n");
-			else sb.append(dis[i]).append("\n");
+		dijkstra(src);
+		for (int i = 1; i <= V; i++) {
+			if (dis[i] == Integer.MAX_VALUE) {
+				sb.append("INF\n");
+			} else {
+				sb.append(dis[i]).append("\n");
+			}
 		}
 
 		System.out.println(sb.toString());
 	}
-	
-	public static void dijkstra(int start) {
+
+	public static void dijkstra(int src) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.add(new Edge(start , 0));
-		dis[start] = 0;
-		
+		pq.add(new Edge(src, 0));
+		dis[src] = 0;
+
 		while (!pq.isEmpty()) {
 			Edge cur = pq.poll();
-			
-			if(dis[cur.v] < cur.c) continue;
 
-			for(Edge o : graph.get(cur.v)) {
-				if(dis[o.v] > cur.c + o.c) {
-					dis[o.v] = cur.c + o.c;
-					pq.add(new Edge(o.v, cur.c + o.c));
+			if (dis[cur.v] < cur.c) {
+				continue;
+			}
+
+			for (Edge o : graph.get(cur.v)) {
+				if (dis[o.v] > o.c + cur.c) {
+					dis[o.v] = o.c + cur.c;
+					pq.add(new Edge(o.v, o.c + cur.c));
 				}
 			}
 		}
