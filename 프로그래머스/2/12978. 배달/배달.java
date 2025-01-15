63b582f8d818aecdb4574d55fb3a5b23974c638e
euics <pjh575157@gmail.com>
2025-01-15 17:40:11 +0900

    [level 2] Title: 배달, Time: 6.11 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,53 +1,49 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
-	static ArrayList<ArrayList<Edge>> graph;
+	static int answer = 0;
 	static int[] dis;
+	static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
 
 	public int solution(int N, int[][] road, int K) {
-		int answer = 0;
+		dis = new int[N + 1];
+		Arrays.fill(dis, Integer.MAX_VALUE);
+		for (int i = 0; i <= N; i++) {
+			graph.add(i, new ArrayList<Edge>());
+		}
 
-		init(N, road);
+		for (int i = 0; i < road.length; i++) {
+			graph.get(road[i][0]).add(new Edge(road[i][1], road[i][2]));
+			graph.get(road[i][1]).add(new Edge(road[i][0], road[i][2]));
+		}
+
 		dijkstra(1);
 
 		for (int i = 1; i <= N; i++) {
-			if (dis[i] <= K)
+			if (dis[i] != Integer.MAX_VALUE && dis[i] <= K) {
 				answer++;
+			}
 		}
 
 		return answer;
 	}
 
-	public void init(int N, int[][] roads) {
-		dis = new int[N + 1];
-		Arrays.fill(dis, Integer.MAX_VALUE);
-
-		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++)
-			graph.add(new ArrayList<Edge>());
-		for (int i = 0; i < roads.length; i++) {
-			graph.get(roads[i][0]).add(new Edge(roads[i][1], roads[i][2]));
-			graph.get(roads[i][1]).add(new Edge(roads[i][0], roads[i][2]));
-		}
-	}
-
 	public void dijkstra(int v) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.add(new Edge(v, 0));
+		PriorityQueue<Edge> q = new PriorityQueue<>();
+		q.add(new Edge(v, 0));
 		dis[v] = 0;
 
-		while (!pq.isEmpty()) {
-			Edge cur = pq.poll();
+		while (!q.isEmpty()) {
+			Edge cur = q.poll();
 
-			if (dis[cur.v] < cur.c)
+			if (dis[cur.v] < cur.c) {
 				continue;
+			}
 
 			for (Edge o : graph.get(cur.v)) {
 				if (dis[o.v] > cur.c + o.c) {
 					dis[o.v] = cur.c + o.c;
-					pq.add(new Edge(o.v, cur.c + o.c));
+					q.add(new Edge(o.v, cur.c + o.c));
 				}
 			}
 		}
