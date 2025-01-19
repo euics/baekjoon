5f638e93975312fb6fe628263b130aba320bcfde
euics <pjh575157@gmail.com>
2025-01-19 16:24:30 +0900

    [level 2] Title: 튜플, Time: 69.78 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,27 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-	public int[] solution(String s) {
-		String[] split = split(s);
+	static ArrayList<Integer> answer = new ArrayList<>();
 
-		List<Integer> answer = new ArrayList<>();
-		for(String seq : split) {
-			String[] num = seq.split(",");
-			
-			for(int i = 0; i < num.length; i++) {
-				if(!answer.contains(Integer.parseInt(num[i]))) answer.add(Integer.parseInt(num[i]));
+	public int[] solution(String s) {
+		String[] split = s.substring(2, s.length() - 2).split("\\},\\{");
+		Arrays.sort(split, (a, b) -> a.length() - b.length());
+
+		for (int i = 0; i < split.length; i++) {
+			String[] numbers = split[i].split(",");
+
+			for (int j = 0; j < numbers.length; j++) {
+				if (!answer.contains(Integer.parseInt(numbers[j]))) {
+					answer.add(Integer.parseInt(numbers[j]));
+				}
 			}
 		}
 
 		return answer.stream().mapToInt(i -> i).toArray();
-	}
-
-	public String[] split(String s) {
-		String[] split = s.substring(2, s.length() - 2).split("\\},\\{");
-		Arrays.sort(split, (a, b) -> a.length() - b.length());
-
-		return split;
 	}
 }
