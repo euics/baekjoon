ae404accc8266566f8ddaad2d968703fd581b991
euics <pjh575157@gmail.com>
2025-01-23 17:36:36 +0900

    [level 2] Title: 카펫, Time: 15.30 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,14 @@
 class Solution {
 	public int[] solution(int brown, int yellow) {
-		for (int col = 1; ; col++) {
-			for (int row = 1; row <= col; row++) {
-				if (col * row == brown + yellow) {
-					if (2 * (row + col - 2) == brown) {
-						return new int[] {col, row};
-					}
+		for (int height = 3; ; height++) {
+			for (int width = 3; width * height <= brown + yellow; width++) {
+				int y = (height - 2) * (width - 2);
+				int b = (height * width) - y;
+
+				if (b == brown && y == yellow) {
+					return new int[] {width, height};
 				}
 			}
 		}
 	}
 }
-
-/*
- 가로 A 세로 B
-
- A * B = BROWN + YELLOW
- 2A + 2(B - 2) = BROWN
- 2A + 2B - 4
- 2(A + B - 2) = BROWN
-*/
