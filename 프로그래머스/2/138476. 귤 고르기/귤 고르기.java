6bdfc3d13fb11ba2f7ef9d6acb91d50034752474
euics <pjh575157@gmail.com>
2025-01-27 18:06:59 +0900

    [level 2] Title: 귤 고르기, Time: 65.18 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,28 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int k, int[] tangerine) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < tangerine.length; i++){
-            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
-        }
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int key : map.keySet()){
-            pq.add(map.get(key));
-        }
-        
-        int cnt = 0, sum = 0;
-        while(!pq.isEmpty()){
-            int cur = pq.poll();
-            sum += cur;
-            cnt++;
-            
-            if(sum >= k){
-                break;
-            }
-        }
-        
-        return cnt;
-    }
+	static int answer = 0;
+
+	public int solution(int k, int[] tangerine) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int type : tangerine) {
+			map.put(type, map.getOrDefault(type, 0) + 1);
+		}
+
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+		for (int keySet : map.keySet()) {
+			pq.add(new int[] {keySet, map.get(keySet)});
+		}
+
+		while (!pq.isEmpty() && k > 0) {
+			int[] cur = pq.poll();
+
+			if (cur[1] <= k) {
+				k -= cur[1];
+			} else {
+				k = 0;
+			}
+
+			answer++;
+		}
+
+		return answer;
+	}
 }
