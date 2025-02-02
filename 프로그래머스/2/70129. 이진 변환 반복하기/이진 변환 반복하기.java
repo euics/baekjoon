3913b857beb69a949cff700ddc982ff5b7b1e54b
euics <pjh575157@gmail.com>
2025-02-02 16:14:35 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.95 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,24 +1,23 @@
-import java.util.*;
-
 class Solution {
-	static int cnt = 0, loop = 0;
-
-	public int[] solution(String s) {
-
-		while (!s.equals("1")) {
-			int zero = 0;
-
-			for (int i = 0; i < s.length(); i++) {
-				if (s.charAt(i) == '0') {
-					zero++;
-				}
-			}
-
-			s = Integer.toString(s.length() - zero, 2);
-			loop++;
-			cnt += zero;
-		}
-
-		return new int[] {loop, cnt};
-	}
+    static int cnt = 0, loop = 0;
+    
+    public int[] solution(String s) {
+        
+        while(!s.equals("1")) {
+            StringBuilder sb = new StringBuilder();
+            
+            for(int i = 0; i < s.length(); i++) {
+                if(s.charAt(i) == '0') {
+                    cnt++;
+                } else {
+                    sb.append("1");
+                }
+            }
+            
+            s = Integer.toString(sb.toString().length(), 2);
+            loop++;
+        }
+        
+        return new int[]{loop, cnt};
+    }
 }
