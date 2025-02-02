5d476159aa48bb79898ab4373a362e29560d2b32
euics <pjh575157@gmail.com>
2025-02-02 15:22:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 4.51 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,15 +1,36 @@
 class Solution {
-	private static final String[] words = {
-		"zero", "one", "two", "three", "four",
-		"five", "six", "seven", "eight", "nine"
-	};
-
-	public int solution(String s) {
-
-		for (int i = 0; i < 10; i++) {
-			s = s.replaceAll(words[i], String.valueOf(i));
-		}
-
-		return Integer.parseInt(s);
-	}
+    static StringBuilder answer = new StringBuilder();
+    static String[] dictionary = {
+        "zero", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine"
+    };
+    
+    public int solution(String s) {
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(Character.isDigit(s.charAt(i))) {
+                answer.append(s.charAt(i));
+            } else {
+                StringBuilder sb = new StringBuilder();
+                
+                while(i < s.length() && Character.isAlphabetic(s.charAt(i))) {
+                    sb.append(s.charAt(i));
+                    
+                    for(int j = 0; j < 10; j++) {
+                        if(dictionary[j].matches(sb.toString())) {
+                            answer.append(j);
+                            sb = new StringBuilder();
+                            break;
+                        }
+                    }
+                    
+                    i++;
+                }
+                
+                i--;
+            }
+        }
+        
+        return Integer.parseInt(answer.toString());
+    }
 }
