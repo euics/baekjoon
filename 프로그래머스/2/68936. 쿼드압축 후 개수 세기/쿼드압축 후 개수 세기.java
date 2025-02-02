28b20364c6dcd52b9679c8d050805b503e757d8f
euics <pjh575157@gmail.com>
2025-02-02 16:30:26 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 12.52 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,38 +1,38 @@
 class Solution {
-	static int[] answer = new int[2];
-
-	public int[] solution(int[][] arr) {
-		quad(arr, 0, 0, arr.length);
-		
-		return answer;
-	}
-
-	public void quad(int[][] arr, int x, int y, int size) {
-		if (zip(arr, x, y, size, arr[y][x])) {
-			if (arr[y][x] == 0) {
-				answer[0]++;
-			} else {
-				answer[1]++;
-			}
-
-			return;
-		}
-
-		quad(arr, x, y, size / 2);
-		quad(arr, x + size / 2, y, size / 2);
-		quad(arr, x, y + size / 2, size / 2);
-		quad(arr, x + size / 2, y + size / 2, size / 2);
-	}
-
-	public boolean zip(int[][] arr, int x, int y, int size, int val) {
-		for (int i = y; i < y + size; i++) {
-			for (int j = x; j < x + size; j++) {
-				if (arr[i][j] != val) {
-					return false;
-				}
-			}
-		}
-
-		return true;
-	}
+    static int[] answer = new int[2];
+    
+    public int[] solution(int[][] arr) {
+        quad(arr, arr.length, 0, 0);
+        
+        return answer;
+    }
+    
+    public void quad(int[][] arr, int compress, int x, int y) {
+        if(canQuad(arr, compress, arr[y][x], x, y)) {
+            if(arr[y][x] == 0) {
+                answer[0]++;
+            } else {
+                answer[1]++;
+            }
+            
+            return;
+        }
+        
+        quad(arr, compress / 2, x, y);
+        quad(arr, compress / 2, x + compress / 2, y);
+        quad(arr, compress / 2, x, y + compress / 2);
+        quad(arr, compress / 2, x + compress / 2, y + compress / 2);
+    }
+    
+    public boolean canQuad(int[][] arr, int compress, int val, int x, int y) {
+        for(int ny = y; ny < y + compress; ny++) {
+            for(int nx = x; nx < x + compress; nx++) {
+                if(arr[ny][nx] != val) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
 }
