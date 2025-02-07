680801885aae95411d1912021b68e54fc91a47e9
euics <pjh575157@gmail.com>
2025-02-07 17:46:05 +0900

    [level 3] Title: 불량 사용자, Time: 400.72 ms, Memory: 396 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,16 +1,15 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
 	static Set<Set<String>> result = new HashSet<>();
 
 	public int solution(String[] user_id, String[] banned_id) {
-		DFS(0, user_id, banned_id, new HashSet<String>());
+		matchPattern(0, user_id, banned_id, new HashSet<String>());
 
 		return result.size();
 	}
 
-	public void DFS(int L, String[] user_id, String[] banned_id, Set<String> set) {
+	public void matchPattern(int L, String[] user_id, String[] banned_id, Set<String> set) {
 		if (L == banned_id.length) {
 			result.add(new HashSet<>(set));
 
@@ -21,7 +20,7 @@ class Solution {
 		for (int i = 0; i < user_id.length; i++) {
 			if (user_id[i].matches(pattern) && !set.contains(user_id[i])) {
 				set.add(user_id[i]);
-				DFS(L + 1, user_id, banned_id, set);
+				matchPattern(L + 1, user_id, banned_id, set);
 				set.remove(user_id[i]);
 			}
 		}
