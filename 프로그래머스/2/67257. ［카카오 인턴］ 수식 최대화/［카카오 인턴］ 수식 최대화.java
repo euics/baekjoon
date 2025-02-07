fda1b9bfc7cd5e872921856ee1037e4c1b83a518
euics <pjh575157@gmail.com>
2025-02-07 17:16:26 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 29.78 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,67 +1,87 @@
 import java.util.*;
 
 class Solution {
-
-	private static final String[][] precedences = {
-		"+-*".split(""),
-		"+*-".split(""),
-		"*+-".split(""),
-		"*-+".split(""),
-		"-*+".split(""),
-		"-+*".split("")
-	};
-
-	static long max = Long.MIN_VALUE;
+	static long answer = Long.MIN_VALUE;
+	static char[] permutations = new char[3];
+	static char[] oper = new char[] {'+', '*', '-'};
 
 	public long solution(String expression) {
-		List<String> numbers = new ArrayList<>();
-		StringTokenizer st = new StringTokenizer(expression, "+-*", true);
-		while (st.hasMoreTokens()) {
-			numbers.add(st.nextToken());
-		}
+		permutation(expression, 0, new boolean[3]);
 
-		for (String[] precedence : precedences) {
-			long result = Math.abs(calculate(new ArrayList<>(numbers), precedence));
-			max = Math.max(max, result);
-		}
-
-		return max;
+		return answer;
 	}
 
-	public long calculate(List<String> numbers, String[] precedence) {
-		for (String operation : precedence) {
-			for (int i = 0; i < numbers.size(); i++) {
-				if (operation.equals(numbers.get(i))) {
-					long a = Long.parseLong(numbers.get(i - 1));
-					long b = Long.parseLong(numbers.get(i + 1));
-					long result = calculate(a, b, operation);
+	public List<Long> parseNumbers(String expressions) {
+		List<Long> numbers = new ArrayList<>();
 
-					numbers.remove(i - 1);
-					numbers.remove(i - 1);
-					numbers.remove(i - 1);
-					numbers.add(i - 1, String.valueOf(result));
-					i -= 2;
+		String[] formulationNumbers = expressions.split("[-||+||*]");
+		for (String formulationNumber : formulationNumbers) {
+			formulationNumber = formulationNumber.trim();
+
+			if (!formulationNumber.isEmpty()) {
+				numbers.add(Long.parseLong(formulationNumber));
+			}
+		}
+
+		return numbers;
+	}
+
+	public List<Character> parseOperations(String expressions) {
+		List<Character> operations = new ArrayList<>();
+
+		String[] formulationOperations = expressions.split("[0-9]");
+		for (String formulationOperation : formulationOperations) {
+			formulationOperation = formulationOperation.trim();
+
+			if (!formulationOperation.isEmpty()) {
+				operations.add(formulationOperation.charAt(0));
+			}
+		}
+
+		return operations;
+	}
+
+	public void permutation(String expressions, int L, boolean[] bool) {
+		if (L == 3) {
+			List<Long> numbers = parseNumbers(expressions);
+			List<Character> operations = parseOperations(expressions);
+
+			for (char permutation : permutations) {
+				int p = 0;
+
+				while (p < operations.size()) {
+					if (operations.get(p) == permutation) {
+						long result = calculate(numbers.get(p), numbers.get(p + 1), permutation);
+						numbers.set(p, result);
+						numbers.remove(p + 1);
+						operations.remove(p);
+					} else {
+						p++;
+					}
 				}
 			}
+
+			answer = Math.max(answer, Math.abs(numbers.get(0)));
+
+			return;
 		}
 
-		return Long.parseLong(numbers.get(0));
+		for (int i = 0; i < 3; i++) {
+			if (!bool[i]) {
+				bool[i] = true;
+				permutations[L] = oper[i];
+				permutation(expressions, L + 1, bool);
+				bool[i] = false;
+			}
+		}
 	}
 
-	public long calculate(long a, long b, String operation) {
-		switch (operation) {
-			case "+" -> {
-				return a + b;
-			}
-			case "-" -> {
-				return a - b;
-			}
-			case "*" -> {
-				return a * b;
-			}
-			default -> {
-				return 0;
-			}
-		}
+	public long calculate(long a, long b, char permutation) {
+		return switch (permutation) {
+			case '+' -> a + b;
+			case '-' -> a - b;
+			case '*' -> a * b;
+			default -> throw new IllegalArgumentException("calculate Method Error");
+		};
 	}
 }
