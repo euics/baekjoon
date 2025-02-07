9b05a994d7d480427fd7719b3b0e8581b41e9ba4
euics <pjh575157@gmail.com>
2025-02-07 18:00:02 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 3.54 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
 	static long answer = Long.MIN_VALUE;
 	static char[] permutations = new char[3];
-	static char[] oper = new char[] {'+', '*', '-'};
+	static char[] operations = {'+', '-', '*'};
 
 	public long solution(String expression) {
 		permutation(expression, 0, new boolean[3]);
@@ -11,40 +11,10 @@ class Solution {
 		return answer;
 	}
 
-	public List<Long> parseNumbers(String expressions) {
-		List<Long> numbers = new ArrayList<>();
-
-		String[] formulationNumbers = expressions.split("[-||+||*]");
-		for (String formulationNumber : formulationNumbers) {
-			formulationNumber = formulationNumber.trim();
-
-			if (!formulationNumber.isEmpty()) {
-				numbers.add(Long.parseLong(formulationNumber));
-			}
-		}
-
-		return numbers;
-	}
-
-	public List<Character> parseOperations(String expressions) {
-		List<Character> operations = new ArrayList<>();
-
-		String[] formulationOperations = expressions.split("[0-9]");
-		for (String formulationOperation : formulationOperations) {
-			formulationOperation = formulationOperation.trim();
-
-			if (!formulationOperation.isEmpty()) {
-				operations.add(formulationOperation.charAt(0));
-			}
-		}
-
-		return operations;
-	}
-
-	public void permutation(String expressions, int L, boolean[] bool) {
+	public void permutation(String expression, int L, boolean[] bool) {
 		if (L == 3) {
-			List<Long> numbers = parseNumbers(expressions);
-			List<Character> operations = parseOperations(expressions);
+			List<Long> numbers = parseNumbers(expression);
+			List<Character> operations = parseOperations(expression);
 
 			for (char permutation : permutations) {
 				int p = 0;
@@ -66,14 +36,44 @@ class Solution {
 			return;
 		}
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < operations.length; i++) {
 			if (!bool[i]) {
 				bool[i] = true;
-				permutations[L] = oper[i];
-				permutation(expressions, L + 1, bool);
+				permutations[L] = operations[i];
+				permutation(expression, L + 1, bool);
 				bool[i] = false;
 			}
 		}
+	}
+
+	public List<Long> parseNumbers(String expression) {
+		List<Long> numbers = new ArrayList<>();
+
+		String[] formulationNumbers = expression.split("[-||+||*]");
+		for (String formulationNumber : formulationNumbers) {
+			formulationNumber = formulationNumber.trim();
+
+			if (!formulationNumber.isEmpty()) {
+				numbers.add(Long.parseLong(formulationNumber));
+			}
+		}
+
+		return numbers;
+	}
+
+	public List<Character> parseOperations(String expression) {
+		List<Character> operations = new ArrayList<>();
+
+		String[] formulationOperations = expression.split("[0-9]");
+		for (String formulationOperation : formulationOperations) {
+			formulationOperation = formulationOperation.trim();
+
+			if (!formulationOperation.isEmpty()) {
+				operations.add(formulationOperation.charAt(0));
+			}
+		}
+
+		return operations;
 	}
 
 	public long calculate(long a, long b, char permutation) {
