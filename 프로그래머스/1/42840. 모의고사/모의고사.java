09d35a04f51c69be7c390e7173aa806533f30ecc
euics <pjh575157@gmail.com>
2025-02-07 15:21:13 +0900

    [level 1] Title: 모의고사, Time: 3.03 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,31 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
 	static List<Integer> answer = new ArrayList<>();
-	static int max = Integer.MIN_VALUE;
-	static int[] cnt = new int[3];
-	private final int[] one = {1, 2, 3, 4, 5};
-	private final int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-	private final int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+	static int[][] guess = new int[][] {
+		{1, 2, 3, 4, 5},
+		{2, 1, 2, 3, 2, 4, 2, 5},
+		{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+	};
 
 	public int[] solution(int[] answers) {
+		int[] cnt = new int[3];
+		int max = Integer.MIN_VALUE;
+
 		for (int i = 0; i < answers.length; i++) {
-			if (answers[i] == one[i % 5]) {
+			if (answers[i] == guess[0][i % 5]) {
 				cnt[0]++;
 			}
 
-			if (answers[i] == two[i % 8]) {
+			if (answers[i] == guess[1][i % 8]) {
 				cnt[1]++;
 			}
 
-			if (answers[i] == three[i % 10]) {
+			if (answers[i] == guess[2][i % 10]) {
 				cnt[2]++;
 			}
 		}
 
-		max = Math.max(cnt[0], cnt[1]);
-		max = Math.max(max, cnt[2]);
+		max = Math.max(max, Math.max(Math.max(cnt[0], cnt[1]), cnt[2]));
 
 		for (int i = 0; i < 3; i++) {
 			if (cnt[i] == max) {
