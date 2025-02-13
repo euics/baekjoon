d637b1205494fcf9eb9b0f42d5c81739e69c20f8
euics <pjh575157@gmail.com>
2025-02-13 21:09:25 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -7,10 +7,10 @@ class Solution {
 		answer = new int[commands.length];
 
 		for (int i = 0; i < commands.length; i++) {
-			int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-			Arrays.sort(arr);
+			int[] copyArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+			Arrays.sort(copyArray);
 
-			answer[i] = arr[commands[i][2] - 1];
+			answer[i] = copyArray[commands[i][2] - 1];
 		}
 
 		return answer;
