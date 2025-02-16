b1b8dba1a046b1f71302c571be2cb8ab3fa64c75
euics <pjh575157@gmail.com>
2025-02-16 20:30:31 +0900

    [level 2] Title: 튜플, Time: 80.57 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,18 +1,23 @@
 import java.util.*;
 
 class Solution {
-	static ArrayList<Integer> answer = new ArrayList<>();
+	static List<Integer> answer = new ArrayList<>();
 
 	public int[] solution(String s) {
-		String[] split = s.substring(2, s.length() - 2).split("\\},\\{");
-		Arrays.sort(split, (a, b) -> a.length() - b.length());
+		String[] str = s.substring(2, s.length() - 2).split("\\},\\{");
+		Arrays.sort(str, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.length() - o2.length();
+			}
+		});
 
-		for (int i = 0; i < split.length; i++) {
-			String[] numbers = split[i].split(",");
+		for (String string : str) {
+			String[] tmp = string.split(",");
 
-			for (int j = 0; j < numbers.length; j++) {
-				if (!answer.contains(Integer.parseInt(numbers[j]))) {
-					answer.add(Integer.parseInt(numbers[j]));
+			for (int i = 0; i < tmp.length; i++) {
+				if (!answer.contains(Integer.parseInt(tmp[i]))) {
+					answer.add(Integer.parseInt(tmp[i]));
 				}
 			}
 		}
