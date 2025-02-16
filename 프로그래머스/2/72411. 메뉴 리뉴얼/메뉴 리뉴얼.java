759e964cc62edfb64dee140ebed42f345c0ae6bc
euics <pjh575157@gmail.com>
2025-02-16 19:35:26 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 30.10 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,42 +1,44 @@
 import java.util.*;
 
 class Solution {
-	static ArrayList<String> answer = new ArrayList<>();
-	static char[] combi;
-	static Map<String, Integer> map = new HashMap<>();
+	static List<String> answer = new ArrayList<>();
+	static Map<String, Integer> candidates = new HashMap<>();
 
 	public String[] solution(String[] orders, int[] course) {
-		int[] max = new int[course.length];
-		Arrays.fill(max, Integer.MIN_VALUE);
-
-		for (int i = 0; i < orders.length; i++) {
-			char[] ch = orders[i].toCharArray();
-			Arrays.sort(ch);
-
-			orders[i] = new String(ch);
-		}
-
 		for (String order : orders) {
-			for (int length = 2; length <= order.length(); length++) {
-				combi = new char[length];
-				combination(0, 0, length, order);
+			for (int i = 2; i <= order.length(); i++) {
+				char[] ch = order.toCharArray();
+				Arrays.sort(ch);
+
+				combination(0, i, 0, ch, new char[i]);
 			}
 		}
+		candidates.keySet().removeIf(key -> candidates.get(key) < 2);
 
-		for (int i = 0; i < course.length; i++) {
-			int length = course[i];
+		LinkedList<String> keySet = new LinkedList<>(candidates.keySet());
+		Collections.sort(keySet, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if (o1.length() == o2.length()) {
+					return o1.compareTo(o2);
+				} else {
+					return o1.length() - o2.length();
+				}
+			}
+		});
 
-			for (String keySet : map.keySet()) {
-				if (keySet.length() == length && map.get(keySet) > 1) {
-					max[i] = Math.max(max[i], map.get(keySet));
+		for (int length : course) {
+			int max = Integer.MIN_VALUE;
+
+			for (String key : keySet) {
+				if (length == key.length()) {
+					max = Math.max(max, candidates.get(key));
 				}
 			}
 
-			if (max[i] != Integer.MIN_VALUE) {
-				for (String keySet : map.keySet()) {
-					if (keySet.length() == length && map.get(keySet) == max[i]) {
-						answer.add(keySet);
-					}
+			for (String key : keySet) {
+				if (length == key.length() && candidates.get(key) == max) {
+					answer.add(key);
 				}
 			}
 		}
@@ -46,21 +48,21 @@ class Solution {
 		return answer.stream().toArray(String[]::new);
 	}
 
-	public void combination(int L, int s, int length, String order) {
+	public void combination(int L, int length, int s, char[] order, char[] permutation) {
 		if (L == length) {
 			StringBuilder sb = new StringBuilder();
-			for (char ch : combi) {
-				sb.append(ch);
+			for (int i = 0; i < permutation.length; i++) {
+				sb.append(permutation[i]);
 			}
 
-			map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+			candidates.put(sb.toString(), candidates.getOrDefault(sb.toString(), 0) + 1);
 
 			return;
 		}
 
-		for (int i = s; i < order.length(); i++) {
-			combi[L] = order.charAt(i);
-			combination(L + 1, i + 1, length, order);
+		for (int i = s; i < order.length; i++) {
+			permutation[L] = order[i];
+			combination(L + 1, length, i + 1, order, permutation);
 		}
 	}
 }
