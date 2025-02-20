f64f9cc07363ebfd096e14d1362a704d270ce845
euics <pjh575157@gmail.com>
2025-02-20 17:51:20 +0900

    [level 2] Title: 할인 행사, Time: 90.04 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,46 +2,28 @@ import java.util.*;
 
 class Solution {
 	static int answer = 0;
-
+	
 	public int solution(String[] want, int[] number, String[] discount) {
-		Map<String, Integer> map = new HashMap<>();
-		for (int i = 0; i < 10; i++) {
-			map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
-		}
+		for (int i = 0; i < discount.length - 9; i++) {
+			Map<String, Integer> map = new HashMap<>();
 
-		if (buyMembership(map, want, number)) {
-			answer++;
-		}
+			for (int j = i; j < i + 10; j++) {
+				map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
+			}
 
-		if (discount.length > 10) {
-			for (int i = 10; i < discount.length; i++) {
-				map.put(discount[i - 10], map.get(discount[i - 10]) - 1);
-				if (map.get(discount[i - 10]) == 0) {
-					map.remove(discount[i - 10]);
+			boolean bool = true;
+			for (int j = 0; j < want.length; j++) {
+				if (!map.containsKey(want[j]) || map.get(want[j]) < number[j]) {
+					bool = false;
+					break;
 				}
+			}
 
-				map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
-
-				if (buyMembership(map, want, number)) {
-					answer++;
-				}
+			if(bool) {
+				answer++;
 			}
 		}
 
 		return answer;
-	}
-
-	public boolean buyMembership(Map<String, Integer> map, String[] want, int[] number) {
-		for (int i = 0; i < want.length; i++) {
-			if (!map.containsKey(want[i])) {
-				return false;
-			} else {
-				if (map.get(want[i]) < number[i]) {
-					return false;
-				}
-			}
-		}
-
-		return true;
 	}
 }
