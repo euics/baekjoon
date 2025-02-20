255cb6c28e8203c623f564643d092a9ba71b5ca0
euics <pjh575157@gmail.com>
2025-02-20 17:41:30 +0900

    [level 1] Title: 신고 결과 받기, Time: 1300.35 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,24 +3,34 @@ import java.util.stream.Collectors;
 
 class Solution {
 	static int[] answer;
-	static Map<String, Set<String>> reportInfo = new HashMap<>();
 
 	public int[] solution(String[] id_list, String[] report, int k) {
-		answer = new int[id_list.length];
-		ArrayList<String> list = new ArrayList<>();
-		list.addAll(Arrays.asList(id_list));
+		Map<String, ArrayList<String>> reportMap = new HashMap<>();
+		Map<String, Integer> reportedMap = new HashMap<>();
 
 		for (String rep : report) {
-			String[] tmp = rep.split(" ");
-			reportInfo.putIfAbsent(tmp[1], new HashSet<String>());
-			reportInfo.get(tmp[1]).add(tmp[0]);
+			String[] split = rep.split(" ");
+			reportMap.putIfAbsent(split[0], new ArrayList<String>());
+			reportedMap.putIfAbsent(split[1], 0);
+
+			if (reportMap.get(split[0]).contains(split[1])) {
+				continue;
+			}
+
+			reportMap.get(split[0]).add(split[1]);
+			reportedMap.put(split[1], reportedMap.getOrDefault(split[1], 0) + 1);
 		}
 
-		for (String keySet : reportInfo.keySet()) {
-			if (reportInfo.get(keySet).size() >= k) {
-				for (String rep : reportInfo.get(keySet)) {
-					answer[list.indexOf(rep)]++;
+		ArrayList<String> ids = new ArrayList<>(Arrays.asList(id_list));
+		answer = new int[id_list.length];
+		for (String reportedKey : reportedMap.keySet()) {
+			if (reportedMap.get(reportedKey) >= k) {
+				for (String reportKey : reportMap.keySet()) {
+					if (reportMap.get(reportKey).contains(reportedKey)) {
+						answer[ids.indexOf(reportKey)]++;
+					}
 				}
+				
 			}
 		}
 
