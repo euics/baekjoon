f739d17ee0b001c1735da65a8bd981a7c5d41d2f
euics <pjh575157@gmail.com>
2025-02-20 17:32:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 111.70 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,19 @@
 import java.util.*;
 
 class Solution {
-	static Map<String, Integer> map = new HashMap<>();
-
 	public String solution(String[] participant, String[] completion) {
+		Map<String, Integer> map = new HashMap<>();
 		for (String par : participant) {
 			map.put(par, map.getOrDefault(par, 0) + 1);
 		}
 
 		for (String com : completion) {
 			map.put(com, map.get(com) - 1);
-
 			if (map.get(com) == 0) {
 				map.remove(com);
 			}
 		}
 
-		StringBuilder sb = new StringBuilder();
-		for (String key : map.keySet()) {
-			sb.append(key);
-		}
-
-		return sb.toString();
+		return map.keySet().stream().toArray(String[]::new)[0];
 	}
 }
