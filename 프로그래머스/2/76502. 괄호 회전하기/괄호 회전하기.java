215083adf95a96f7fe461c8fc7846db9442686c0
euics <pjh575157@gmail.com>
2025-02-24 16:14:09 +0900

    [level 2] Title: 괄호 회전하기, Time: 99.17 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -9,39 +9,36 @@ class Solution {
 			dq.addLast(ch);
 		}
 
-		int x = s.length();
-		for (int i = 0; i < x; i++) {
-			StringBuilder sb = new StringBuilder();
-
-			for (char ch : dq) {
-				sb.append(ch);
-			}
-
-			if (correctPattern(sb.toString())) {
+		for (int rotate = 0; rotate < s.length(); rotate++) {
+			if (validate(s)) {
 				answer++;
 			}
 
-			dq.addLast(dq.pollFirst());
+			s = rotate(dq);
 		}
 
 		return answer;
 	}
 
-	public boolean correctPattern(String s) {
+	public boolean validate(String s) {
 		Stack<Character> stack = new Stack<>();
 
 		for (char ch : s.toCharArray()) {
-			if (ch == '[' || ch == '(' || ch == '{') {
+			if (ch == '[' || ch == '{' || ch == '(') {
 				stack.push(ch);
 			} else {
 				if (stack.isEmpty()) {
 					return false;
+				}
+
+				if (ch == ']' && stack.peek() == '[') {
+					stack.pop();
+				} else if (ch == '}' && stack.peek() == '{') {
+					stack.pop();
+				} else if (ch == ')' && stack.peek() == '(') {
+					stack.pop();
 				} else {
-					if (matchPattern(stack.peek(), ch)) {
-						stack.pop();
-					} else {
-						return false;
-					}
+					return false;
 				}
 			}
 		}
@@ -49,9 +46,17 @@ class Solution {
 		return stack.isEmpty();
 	}
 
-	public boolean matchPattern(char open, char close) {
-		return (open == '(' && close == ')') ||
-			(open == '[' && close == ']') ||
-			(open == '{' && close == '}');
+	public String rotate(Deque<Character> dq) {
+		dq.addLast(dq.pollFirst());
+
+		StringBuilder sb = new StringBuilder();
+		int size = dq.size();
+		for (int i = 0; i < size; i++) {
+			char cur = dq.pollFirst();
+			sb.append(cur);
+			dq.addLast(cur);
+		}
+
+		return sb.toString();
 	}
 }
