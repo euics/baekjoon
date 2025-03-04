779fa5885aefc57adfc4e60a284294b53f0fe03c
euics <pjh575157@gmail.com>
2025-03-04 18:20:30 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.36 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,38 +1,18 @@
 import java.util.*;
 
 class Solution {
-	static Set<String> set = new HashSet<>();
-
-	public static void main(String[] args) {
-		Solution T = new Solution();
-		System.out.println(
-			Arrays.toString(T.solution(2, new String[] {"hello", "one", "even", "never", "now", "world", "draw"}))
-		);
-	}
-
 	public int[] solution(int n, String[] words) {
-		String prev = "", cur = "";
+		Set<String> set = new HashSet<>();
+		Stack<String> stack = new Stack<>();
 
-		for (int i = 0; i < words.length / n; i++) {
-			for (int j = 0; j < n; j++) {
-				cur = words[n * i + j];
-
-				if (i != 0 || j != 0) {
-					if (prev.charAt(prev.length() - 1) != cur.charAt(0)) {
-						return new int[] {j + 1, i + 1};
-					}
-				}
-
-				if (set.contains(cur)) {
-					return new int[] {j + 1, i + 1};
-				} else {
-					set.add(cur);
-				}
-
-				System.out.printf("%s %s\n", prev, cur);
-
-				prev = cur;
+		for (int i = 0; i < words.length; i++) {
+			if ((!stack.isEmpty() && stack.peek().charAt(stack.peek().length() - 1) != words[i].charAt(0))
+				|| set.contains(words[i])) {
+				return new int[] {i % n + 1, i / n + 1};
 			}
+
+			stack.push(words[i]);
+			set.add(words[i]);
 		}
 
 		return new int[] {0, 0};
