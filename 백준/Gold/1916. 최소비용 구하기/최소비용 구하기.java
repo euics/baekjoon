f29b2f2484bc0b0b3e4b12043157edd1bd1ac766
euics <pjh575157@gmail.com>
2025-03-17 18:02:50 +0900

    [Gold V] Title: 최소비용 구하기, Time: 464 ms, Memory: 50644 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,52 +1,48 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	static ArrayList<ArrayList<Edge>> graph;
+public class Main {
 	static int[] dis;
+	static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-
-		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++)
-			graph.add(new ArrayList<Edge>());
 		dis = new int[N + 1];
 		Arrays.fill(dis, Integer.MAX_VALUE);
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<Edge>());
+		}
 
-		for(int i = 0; i < M; i++) {
+		int M = Integer.parseInt(br.readLine());
+		for (int i = 0; i < M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-
-			graph.get(u).add(new Edge(v, w));
+			graph.get(Integer.parseInt(st.nextToken()))
+				.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-
-		dijkstra(start);
-
-		System.out.println(dis[end]);
+		int src = Integer.parseInt(st.nextToken());
+		int des = Integer.parseInt(st.nextToken());
+		dijkstra(src);
+		System.out.println(dis[des]);
 	}
-	
-	public static void dijkstra(int start) {
+
+	public static void dijkstra(int src) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		pq.add(new Edge(start , 0));
-		dis[start] = 0;
-		
+		pq.add(new Edge(src, 0));
+		dis[src] = 0;
+
 		while (!pq.isEmpty()) {
 			Edge cur = pq.poll();
-			
-			if(dis[cur.v] < cur.c) continue;
 
-			for(Edge o : graph.get(cur.v)) {
-				if(dis[o.v] > cur.c + o.c) {
+			if (cur.c > dis[cur.v]) {
+				continue;
+			}
+
+			for (Edge o : graph.get(cur.v)) {
+				if (dis[o.v] > cur.c + o.c) {
 					dis[o.v] = cur.c + o.c;
 					pq.add(new Edge(o.v, cur.c + o.c));
 				}
