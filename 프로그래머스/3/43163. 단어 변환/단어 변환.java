704a27bbdf7d26ca5faa82946ef4d628d42523d0
euics <pjh575157@gmail.com>
2025-04-02 18:19:47 +0900

    [level 3] Title: 단어 변환, Time: 0.50 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,43 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
-	static int answer = Integer.MAX_VALUE;
-	static List<String> list;
+	static int min = Integer.MAX_VALUE;
 
 	public int solution(String begin, String target, String[] words) {
-		list = Arrays.stream(words).collect(Collectors.toList());
 		DFS(0, begin, target, words, new boolean[words.length]);
 
-		return answer == Integer.MAX_VALUE ? 0 : answer;
+		return min == Integer.MAX_VALUE ? 0 : min;
 	}
 
 	public void DFS(int L, String begin, String target, String[] words, boolean[] bool) {
-		if (begin.equals(target)) {
-			answer = Math.min(answer, L);
-
+		if (min < L) {
 			return;
 		}
 
+		if (begin.equals(target)) {
+			min = L;
+		}
+
 		for (int i = 0; i < words.length; i++) {
-			if (countDiff(begin, words[i])) {
-				if (!bool[list.indexOf(words[i])]) {
-					bool[list.indexOf(words[i])] = true;
+			if (diff(begin, words[i])) {
+				if (!bool[i]) {
+					bool[i] = true;
 					DFS(L + 1, words[i], target, words, bool);
-					bool[list.indexOf(words[i])] = false;
+					bool[i] = false;
 				}
 			}
 		}
 	}
 
-	public boolean countDiff(String o1, String o2) {
+	public boolean diff(String begin, String word) {
 		int cnt = 0;
 
-		if (o1.length() != o2.length()) {
+		if (begin.length() != word.length()) {
 			return false;
 		}
 
-		for (int i = 0; i < o1.length(); i++) {
-			if (o1.charAt(i) != o2.charAt(i)) {
+		for (int i = 0; i < begin.length(); i++) {
+			if (begin.charAt(i) != word.charAt(i)) {
 				cnt++;
 			}
 		}
