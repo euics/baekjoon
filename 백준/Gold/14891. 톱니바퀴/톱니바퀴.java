5d0ab00cbd9772ec1f733b678e54fe0cddfc0532
euics <pjh575157@gmail.com>
2025-04-07 16:32:05 +0900

    [Gold V] Title: 톱니바퀴, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -2,71 +2,87 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[][] gears = new int[4][8];
+	static int answer = 0;
+	static int[][] arr = new int[4][8];
+	static int K;
+	static int[][] info;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		init();
 
-        for(int i = 0; i < 4; i++){
-            String input = br.readLine();
-            for(int j = 0; j < 8; j++)
-                gears[i][j] = input.charAt(j) - '0';
-        }
+		for (int i = 0; i < K; i++) {
+			int[] dir = new int[4];
+			dir[info[i][0]] = info[i][1];
 
-        int k = Integer.parseInt(br.readLine());
-        for(int i = 0; i < k; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int gearNum = Integer.parseInt(st.nextToken()) - 1;
-            int dir = Integer.parseInt(st.nextToken());
+			for (int sequence = info[i][0] - 1; sequence >= 0; sequence--) {
+				if (arr[sequence][2] != arr[sequence + 1][6]) {
+					dir[sequence] = -dir[sequence + 1];
+				} else {
+					break;
+				}
+			}
 
-            findDir(gearNum, dir);
-        }
+			for (int sequence = info[i][0] + 1; sequence < 4; sequence++) {
+				if (arr[sequence][6] != arr[sequence - 1][2]) {
+					dir[sequence] = -dir[sequence - 1];
+				} else {
+					break;
+				}
+			}
 
-        int score = 0;
-        for(int i = 0; i < 4; i++){
-            if(gears[i][0] == 1)
-                score += Math.pow(2, i);
-        }
+			for (int sequence = 0; sequence < 4; sequence++) {
+				if (dir[sequence] == 1) {
+					rotateRight(sequence);
+				} else if (dir[sequence] == -1) {
+					rotateLeft(sequence);
+				}
+			}
+		}
 
-        System.out.println(score);
-    }
+		for (int i = 0; i < 4; i++) {
+			if (arr[i][0] == 1) {
+				answer += (int)Math.pow(2, i);
+			}
+		}
 
-    private static void findDir(int gearNum, int dir){
-        int[] directions = new int[4];
-        directions[gearNum] = dir;
+		System.out.println(answer);
+	}
 
-        for(int i = gearNum; i > 0; i--){
-            if(gears[i][6] != gears[i - 1][2])
-                directions[i - 1] = -directions[i];
-            else
-                break;
-        }
+	public static void rotateRight(int sequence) {
+		int tmp = arr[sequence][7];
+		for (int i = 7; i >= 1; i--) {
+			arr[sequence][i] = arr[sequence][i - 1];
+		}
+		arr[sequence][0] = tmp;
+	}
 
-        for(int i = gearNum; i < 3; i++){
-            if(gears[i][2] != gears[i + 1][6])
-                directions[i + 1] = -directions[i];
-            else
-                break;
-        }
+	public static void rotateLeft(int sequence) {
+		int tmp = arr[sequence][0];
+		for (int i = 0; i <= 6; i++) {
+			arr[sequence][i] = arr[sequence][i + 1];
+		}
+		arr[sequence][7] = tmp;
+	}
 
-        rotate(directions);
-    }
+	public static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < 8; j++) {
+				arr[i][j] = Integer.parseInt(String.valueOf(input.charAt(j)));
+			}
+		}
 
-    private static void rotate(int[] directions){
-        for(int i = 0; i < 4; i++){
-            if(directions[i] == 1){
-                int temp = gears[i][7];
-                for(int j = 7; j > 0; j--)
-                    gears[i][j] = gears[i][j - 1];
-                gears[i][0] = temp;
-            } else if(directions[i] == -1){
-                int temp = gears[i][0];
-                for(int j = 0; j < 7; j++)
-                    gears[i][j] = gears[i][j + 1];
-                gears[i][7] = temp;
-            }
-        }
-    }
+		K = Integer.parseInt(br.readLine());
+		info = new int[K][2];
+		for (int i = 0; i < K; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int sequence = Integer.parseInt(st.nextToken()) - 1;
+			int dir = Integer.parseInt(st.nextToken());
+
+			info[i][0] = sequence;
+			info[i][1] = dir;
+		}
+	}
 }
-
