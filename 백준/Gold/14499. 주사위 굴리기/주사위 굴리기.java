d0834c3f97bc2f46fc35faebddd79b8aa9de1150
euics <pjh575157@gmail.com>
2025-04-08 18:22:11 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 140 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,72 +1,140 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n, m, x, y, k;
-    static int[][] arr;
-    static int[] dix = {0, 1, -1, 0, 0};
-    static int[] diy = {0, 0, 0, -1, 1};
+	static int N, M;
+	static int x, y;
+	static int K;
+	static int[][] arr;
+	static int[] order;
+	static int[] dice = new int[] {0, 0, 0, 0, 0, 0};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        y = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
+	public static void main(String[] args) throws IOException {
+		init();
 
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++)
-                arr[i][j] = Integer.parseInt(st.nextToken());
-        }
+		for (int i = 0; i < K; i++) {
+			int dir = order[i];
 
-        Dice curDice = new Dice(0, 0, 0, 0, 0, 0);
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < k; i++){
-            int a = Integer.parseInt(st.nextToken());
+			if (dir == 1) {
+				int nx = x + 1;
+				int ny = y;
 
-            int nx = x + dix[a];
-            int ny = y + diy[a];
-            if(nx < 0 || ny < 0 || nx >= m || ny >= n)
-                continue;
+				if (!isValid(nx, ny)) {
+					continue;
+				}
+				right();
 
-            Dice newDice = null;
-            if(a == 1)
-                newDice = new Dice(curDice.left, curDice.up, curDice.down, curDice.bottom, curDice.top, curDice.right);
-            else if(a == 2)
-                newDice = new Dice(curDice.right, curDice.up, curDice.down, curDice.top, curDice.bottom, curDice.left);
-            else if(a == 3)
-                newDice = new Dice(curDice.up, curDice.bottom, curDice.top, curDice.left, curDice.right, curDice.down);
-            else if(a == 4)
-                newDice = new Dice(curDice.down, curDice.top, curDice.bottom, curDice.left, curDice.right, curDice.up);
+				x = nx;
+				y = ny;
+				copy(x, y);
+			} else if (dir == 2) {
+				int nx = x - 1;
+				int ny = y;
 
-            curDice = newDice;
-            x = nx;
-            y = ny;
+				if (!isValid(nx, ny)) {
+					continue;
+				}
+				left();
 
-            if(arr[y][x] == 0){
-                arr[y][x] = curDice.bottom;
-                System.out.println(curDice.top);
-            } else{
-                curDice.bottom = arr[y][x];
-                arr[y][x] = 0;
-                System.out.println(curDice.top);
-            }
-        }
-    }
-}
+				x = nx;
+				y = ny;
+				copy(x, y);
+			} else if (dir == 3) {
+				int nx = x;
+				int ny = y - 1;
 
-class Dice {
-    int top, up, down, left, right, bottom;
-    public Dice(int top, int up, int down, int left, int right, int bottom){
-        this.top = top;
-        this.up = up;
-        this.down = down;
-        this.left = left;
-        this.right = right;
-        this.bottom = bottom;
-    }
+				if (!isValid(nx, ny)) {
+					continue;
+				}
+				up();
+
+				x = nx;
+				y = ny;
+				copy(x, y);
+			} else {
+				int nx = x;
+				int ny = y + 1;
+
+				if (!isValid(nx, ny)) {
+					continue;
+				}
+				down();
+
+				x = nx;
+				y = ny;
+				copy(x, y);
+			}
+
+			System.out.println(dice[1]);
+		}
+	}
+
+	public static void right() {
+		int tmp = dice[5];
+		dice[5] = dice[1];
+		dice[1] = dice[4];
+		dice[4] = dice[3];
+		dice[3] = tmp;
+	}
+
+	public static void left() {
+		int tmp = dice[4];
+		dice[4] = dice[1];
+		dice[1] = dice[5];
+		dice[5] = dice[3];
+		dice[3] = tmp;
+	}
+
+	public static void up() {
+		int tmp = dice[0];
+		dice[0] = dice[1];
+		dice[1] = dice[2];
+		dice[2] = dice[3];
+		dice[3] = tmp;
+	}
+
+	public static void down() {
+		int tmp = dice[3];
+		dice[3] = dice[2];
+		dice[2] = dice[1];
+		dice[1] = dice[0];
+		dice[0] = tmp;
+	}
+
+	public static void copy(int x, int y) {
+		if (arr[y][x] != 0) {
+			dice[3] = arr[y][x];
+			arr[y][x] = 0;
+		} else {
+			arr[y][x] = dice[3];
+		}
+	}
+
+	public static boolean isValid(int x, int y) {
+		return x >= 0 && y >= 0 && x < M && y < N;
+	}
+
+	public static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		order = new int[K];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < K; i++) {
+			order[i] = Integer.parseInt(st.nextToken());
+		}
+	}
 }
