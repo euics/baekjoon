ec6ef2b83bf5b018bd2251f41965981de5d68f0c
euics <pjh575157@gmail.com>
2025-04-08 21:01:29 +0900

    [Gold IV] Title: 연구소, Time: 356 ms, Memory: 150300 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,86 +1,108 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static final int[] dx = {0, 0, 1, -1};
-    static final int[] dy = {1, -1, 0, 0};
-    static int[][] originalMap;
-    static int n, m;
-    static int maxSafeZone = 0;
+	static int N, M;
+	static int[][] arr;
+	static List<int[]> coordinate = new ArrayList<>();
+	static int[] combination = new int[3];
+	static int[] dix = {0, 0, -1, 1};
+	static int[] diy = {-1, 1, 0, 0};
+	static int max = Integer.MIN_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		init();
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        originalMap = new int[n][m];
+		DFS(0, 0);
 
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
-                originalMap[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		System.out.println(max);
+	}
 
-        dfs(0, 0, 0);
-        System.out.println(maxSafeZone);
-    }
+	public static void DFS(int L, int s) {
+		if (L == 3) {
+			max = Math.max(max, BFS());
 
-    static void dfs(int x, int y, int wallCount) {
-        if(wallCount == 3) {
-            bfs();
-            return;
-        }
+			return;
+		}
 
-        for(int i = x; i < n; i++, y=0) {
-            for(int j = y; j < m; j++) {
-                if(originalMap[i][j] == 0) {
-                    originalMap[i][j] = 1;
-                    dfs(i, j, wallCount+1);
-                    originalMap[i][j] = 0;
-                }
-            }
-        }
-    }
+		for (int i = s; i < coordinate.size(); i++) {
+			combination[L] = i;
+			DFS(L + 1, i + 1);
+		}
+	}
 
-    static void bfs() {
-        int[][] copyMap = new int[n][m];
-        Queue<int[]> queue = new LinkedList<>();
-        
-        for(int i = 0; i < n; i++) {
-            copyMap[i] = originalMap[i].clone();
-        }
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(copyMap[i][j] == 2) {
-                    queue.offer(new int[] {i, j});
-                }
-            }
-        }
+	public static int BFS() {
+		int[][] clone = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			clone[i] = arr[i].clone();
+		}
 
-        while(!queue.isEmpty()) {
-            int[] current = queue.poll();
-            
-            for(int k = 0; k < 4; k++) {
-                int nx = current[0] + dx[k];
-                int ny = current[1] + dy[k];
-                
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m && copyMap[nx][ny] == 0) {
-                    copyMap[nx][ny] = 2;
-                    queue.offer(new int[] {nx, ny});
-                }
-            }
-        }
-        
-        int safeZone = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(copyMap[i][j] == 0) safeZone++;
-            }
-        }
-        
-        maxSafeZone = Math.max(maxSafeZone, safeZone);
-    }
+		for (int idx : combination) {
+			clone[coordinate.get(idx)[1]][coordinate.get(idx)[0]] = 1;
+		}
+
+		Queue<int[]> q = virus();
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+
+			for (int d = 0; d < 4; d++) {
+				int nx = cur[0] + dix[d];
+				int ny = cur[1] + diy[d];
+
+				if (nx >= 0 && ny >= 0 && nx < M && ny < N && clone[ny][nx] == 0) {
+					clone[ny][nx] = 2;
+					q.add(new int[] {nx, ny});
+				}
+			}
+		}
+
+		return count(clone);
+	}
+
+	public static Queue<int[]> virus() {
+		Queue<int[]> q = new LinkedList<>();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j] == 2) {
+					q.add(new int[] {j, i});
+				}
+			}
+		}
+
+		return q;
+	}
+
+	public static int count(int[][] clone) {
+		int cnt = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (clone[i][j] == 0) {
+					cnt++;
+				}
+			}
+		}
+
+		return cnt;
+	}
+
+	public static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+
+				if (arr[i][j] == 0) {
+					coordinate.add(new int[] {j, i});
+				}
+			}
+		}
+	}
 }
