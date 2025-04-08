c33552976146e8f8ef52cd4b375516a43182a54f
euics <pjh575157@gmail.com>
2025-04-08 20:42:36 +0900

    [Gold IV] Title: 테트로미노, Time: 620 ms, Memory: 31544 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,63 +1,69 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n, m, max = Integer.MIN_VALUE;
-    static int[][] arr;
-    static boolean[][] bool;
+	static int N, M;
+	static int[][] arr;
+	static boolean[][] bool;
+	static int max = Integer.MIN_VALUE;
+	static int[] dix = {0, 0, -1, 1};
+	static int[] diy = {-1, 1, 0, 0};
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
-        bool = new boolean[n][m];
+	public static void main(String[] args) throws IOException {
+		init();
 
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++)
-                arr[i][j] = Integer.parseInt(st.nextToken());
-        }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				bool[i][j] = true;
+				DFS(1, j, i, arr[i][j]);
+				bool[i][j] = false;
+			}
+		}
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                bool[i][j] = true;
-                solution(1, j, i, arr[i][j]);
-                bool[i][j] = false;
-            }
-        }
+		System.out.println(max);
+	}
 
-        System.out.println(max);
-    }
+	public static void DFS(int L, int x, int y, int sum) {
+		if (L == 4) {
+			max = Math.max(max, sum);
 
-    static int[] dix = {0, 0, -1, 1};
-    static int[] diy = {-1, 1, 0, 0};
+			return;
+		}
 
-    private static void solution(int L, int x, int y, int sum){
-        if(L == 4) {
-            max = Math.max(max, sum);
-            return;
-        }
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dix[d];
+			int ny = y + diy[d];
 
-        for(int i = 0; i < 4; i++){
-            int nx = x + dix[i];
-            int ny = y + diy[i];
+			if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
+				if (!bool[ny][nx]) {
+					if (L == 2) {
+						bool[ny][nx] = true;
+						DFS(L + 1, x, y, sum + arr[ny][nx]);
+						bool[ny][nx] = false;
+					}
 
-            if(nx >= 0 && ny >= 0 && nx < m && ny < n){
-                if(!bool[ny][nx]){
+					bool[ny][nx] = true;
+					DFS(L + 1, nx, ny, sum + arr[ny][nx]);
+					bool[ny][nx] = false;
+				}
+			}
+		}
+	}
 
-                    if(L == 2){
-                        bool[ny][nx] = true;
-                        solution(L + 1, x, y, sum + arr[ny][nx]);
-                        bool[ny][nx] = false;
-                    }
+	public static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-                    bool[ny][nx] = true;
-                    solution(L + 1, nx, ny, sum + arr[ny][nx]);
-                    bool[ny][nx] = false;
-                }
-            }
-        }
-    }
+		arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		bool = new boolean[N][M];
+	}
 }
