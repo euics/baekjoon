3faea849c6ae7637b16de1854865bfc588ce74fe
euics <pjh575157@gmail.com>
2025-08-22 22:55:57 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -3,31 +3,31 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long n = Long.parseLong(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(br.readLine());
 
-		System.out.println(phi(n));
-	}
+        System.out.println(phi(n));
+    }
 
-	public static long phi(long n) {
-		long result = n;
+    public static long phi(long n) {
+        long result = n;
 
-		for (long p = 2; p <= Math.sqrt(n); p++) {
-			if (n % p == 0) {
-				result = result - result / p;
+        for (long p = 2; p <= Math.sqrt(n); p++) {
+            if (n % p == 0) {
+                result = result - result / p;
 
-				while (n % p == 0) {
-					n /= p;
-				}
-			}
-		}
+                while (n % p == 0) {
+                    n /= p;
+                }
+            }
+        }
 
-		if (n > 1) {
-			result = result - result / n;
-		}
+        if (n > 1) {
+            result = result - result / n;
+        }
 
-		return result;
-	}
+        return result;
+    }
 }
