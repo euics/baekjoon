cc2ac07d261c63f9e32cc27a2191ee4dc8d1cdad
euics <pjh575157@gmail.com>
2025-08-22 23:00:35 +0900

    [level 2] Title: 소수 찾기, Time: 29.81 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,38 +1,28 @@
 import java.util.*;
 
 class Solution {
-	public int solution(String numbers) {
-		Set<Integer> set = new HashSet<>();
-		permutation("", numbers, set);
+    public int solution(String numbers) {
+        Set<Integer> permutation = new HashSet<>();
+        permutation("", numbers, permutation);
 
-		return (int)set.stream().filter(this::isPrime).count();
-	}
+        return (int) permutation.stream().filter(this::isPrime).count();
+    }
 
-	public void permutation(String prefix, String str, Set<Integer> set) {
-		if (!prefix.isEmpty()) {
-			set.add(Integer.parseInt(prefix));
-		}
+    public void permutation(String prefix, String str, Set<Integer> permutation) {
+        if (!prefix.isEmpty()) permutation.add(Integer.parseInt(prefix));
 
-		int n = str.length();
-		for (int i = 0; i < n; i++) {
-			permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), set);
-		}
-	}
+        int n = str.length();
+        for (int i = 0; i < n; i++)
+            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), permutation);
+    }
 
-	public boolean isPrime(int n) {
-		if (n <= 1)
-			return false;
-		if (n == 2)
-			return true;
-		if (n % 2 == 0)
-			return false;
+    public boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
 
-		for (int i = 3; i * i <= n; i += 2) {
-			if (n % i == 0) {
-				return false;
-			}
-		}
+        for (int i = 3; i * i <= n; i += 2) if (n % i == 0) return false;
 
-		return true;
-	}
+        return true;
+    }
 }
