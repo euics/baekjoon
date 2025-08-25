4a0c486c9a6f34b46eecf96cfe6565052df1c881
euics <pjh575157@gmail.com>
2025-08-25 15:08:40 +0900

    [level 2] Title: 짝지어 제거하기, Time: 81.60 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,18 +1,16 @@
 import java.util.*;
 
 class Solution {
-	public int solution(String s) {
-		Stack<Character> stack = new Stack<>();
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
 
-		for (char ch : s.toCharArray()) {
-			if (!stack.isEmpty() && stack.peek() == ch) {
-				stack.pop();
-				continue;
-			}
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty()) {
+                if (stack.peek() != ch) stack.push(ch);
+                else while (!stack.isEmpty() && stack.peek() == ch) stack.pop();
+            } else stack.push(ch);
+        }
 
-			stack.push(ch);
-		}
-
-		return stack.isEmpty() ? 1 : 0;
-	}
+        return stack.isEmpty() ? 1 : 0;
+    }
 }
