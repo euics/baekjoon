6d518bb192553adf3887de8836aa713a49d21115
euics <pjh575157@gmail.com>
2025-08-28 13:03:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,19 +1,25 @@
 import java.util.*;
 
 class Solution {
-	public int[] solution(int n, String[] words) {
-		Set<String> set = new HashSet<>();
-		Stack<String> stack = new Stack<>();
+    public int[] solution(int n, String[] words) {
+        Set<String> dic = new HashSet<>();
 
-		for (int i = 0; i < words.length; i++) {
-			if ((!stack.isEmpty() && stack.peek().charAt(stack.peek().length() - 1) != words[i].charAt(0)) || set.contains(words[i])) {
-				return new int[]{i % n + 1, i / n + 1};
-			}
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0 && fail(dic, words[i - 1], words[i])) {
+                return new int[]{i % n + 1, i / n + 1};
+            }
 
-			set.add(words[i]);
-			stack.push(words[i]);
-		}
+            dic.add(words[i]);
+        }
 
-		return new int[] {0, 0};
-	}
+        return new int[]{0, 0};
+    }
+
+    public boolean fail(Set<String> dic, String prev, String next) {
+        if (dic.contains(next)) return true;
+        if (prev.charAt(prev.length() - 1) != next.charAt(0)) return true;
+        if (next.length() == 1) return true;
+
+        return false;
+    }
 }
