ef5dc5cb29f1040df04e1c8c059777714d1a4300
euics <pjh575157@gmail.com>
2025-08-28 13:58:39 +0900

    [level 2] Title: 괄호 회전하기, Time: 58.25 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,62 +1,32 @@
 import java.util.*;
 
 class Solution {
-	static int answer = 0;
+    public int solution(String s) {
+        int ans = 0;
 
-	public int solution(String s) {
-		Deque<Character> dq = new LinkedList<>();
-		for (char ch : s.toCharArray()) {
-			dq.addLast(ch);
-		}
+        for (int i = 0; i < s.length(); i++) {
+            if (accept(s)) ans++;
+            s = s.substring(1) + s.charAt(0);
+        }
 
-		for (int rotate = 0; rotate < s.length(); rotate++) {
-			if (validate(s)) {
-				answer++;
-			}
+        return ans;
+    }
 
-			s = rotate(dq);
-		}
+    public boolean accept(String s) {
+        Stack<Character> stack = new Stack<>();
 
-		return answer;
-	}
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') stack.push(ch);
+            else {
+                if (stack.isEmpty()) return false;
+                if (ch == ')') if (stack.peek() != '(') return false;
+                if (ch == ']') if (stack.peek() != '[') return false;
+                if (ch == '}') if (stack.peek() != '{') return false;
 
-	public boolean validate(String s) {
-		Stack<Character> stack = new Stack<>();
+                stack.pop();
+            }
+        }
 
-		for (char ch : s.toCharArray()) {
-			if (ch == '[' || ch == '{' || ch == '(') {
-				stack.push(ch);
-			} else {
-				if (stack.isEmpty()) {
-					return false;
-				}
-
-				if (ch == ']' && stack.peek() == '[') {
-					stack.pop();
-				} else if (ch == '}' && stack.peek() == '{') {
-					stack.pop();
-				} else if (ch == ')' && stack.peek() == '(') {
-					stack.pop();
-				} else {
-					return false;
-				}
-			}
-		}
-
-		return stack.isEmpty();
-	}
-
-	public String rotate(Deque<Character> dq) {
-		dq.addLast(dq.pollFirst());
-
-		StringBuilder sb = new StringBuilder();
-		int size = dq.size();
-		for (int i = 0; i < size; i++) {
-			char cur = dq.pollFirst();
-			sb.append(cur);
-			dq.addLast(cur);
-		}
-
-		return sb.toString();
-	}
+        return stack.isEmpty();
+    }
 }
