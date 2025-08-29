05ad1134165ad2cb0c3863c0164916eea590385f
euics <pjh575157@gmail.com>
2025-08-29 16:12:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.90 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,42 @@
 import java.util.*;
 
 class Solution {
-	static int[] dix = {0, 0, -1, 1};
-	static int[] diy = {-1, 1, 0, 0};
 
-	public int solution(int[][] maps) {
-		int n = maps.length;
-		int m = maps[0].length;
+    public int solution(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        int[] dix = {0, 0, -1, 1};
+        int[] diy = {-1, 1, 0, 0};
 
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {0, 0});
-		boolean[][] bool = new boolean[n][m];
-		bool[0][0] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        visited[0][0] = true;
+        int L = 1;
 
-		int L = 1;
-		while (!q.isEmpty()) {
-			int length = q.size();
+        while (!q.isEmpty()) {
+            int length = q.size();
 
-			for (int i = 0; i < length; i++) {
-				int[] cur = q.poll();
+            for (int i = 0; i < length; i++) {
+                int[] cur = q.poll();
+                if (cur[0] == m - 1 && cur[1] == n - 1) return L;
 
-				if (cur[0] == m - 1 && cur[1] == n - 1) {
-					return L;
-				}
+                for (int d = 0; d < 4; d++) {
+                    int nx = cur[0] + dix[d];
+                    int ny = cur[1] + diy[d];
 
-				for (int d = 0; d < 4; d++) {
-					int nx = cur[0] + dix[d];
-					int ny = cur[1] + diy[d];
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && maps[ny][nx] == 1) {
+                        if (!visited[ny][nx]) {
+                            visited[ny][nx] = true;
+                            q.add(new int[]{nx, ny});
+                        }
+                    }
+                }
+            }
 
-					if (nx >= 0 && ny >= 0 && nx < m && ny < n && maps[ny][nx] == 1) {
-						if (!bool[ny][nx]) {
-							bool[ny][nx] = true;
-							q.add(new int[] {nx, ny});
-						}
-					}
-				}
-			}
+            L++;
+        }
 
-			L++;
-		}
-
-		return -1;
-	}
+        return -1;
+    }
 }
