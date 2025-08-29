ceab038d32b8a185ed3b8e7d9daf34f3c8324dae
euics <pjh575157@gmail.com>
2025-08-29 15:26:57 +0900

    [level 2] Title: 전화번호 목록, Time: 323.28 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,9 @@
 import java.util.*;
 
 class Solution {
-	public boolean solution(String[] phone_book) {
-		Arrays.sort(phone_book);
-
-		for (int i = 0; i < phone_book.length - 1; i++) {
-			if (phone_book[i + 1].startsWith(phone_book[i])) {
-				return false;
-			}
-		}
-
-		return true;
-	}
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for (int i = 1; i < phone_book.length; i++) if (phone_book[i].startsWith(phone_book[i - 1])) return false;
+        return true;
+    }
 }
