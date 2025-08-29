43ca5d3267b67357b0685337a29ec74e65fcc1a1
euics <pjh575157@gmail.com>
2025-08-29 15:22:59 +0900

    [level 2] Title: 튜플, Time: 55.13 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,27 +1,30 @@
 import java.util.*;
 
 class Solution {
-	static List<Integer> answer = new ArrayList<>();
+    static List<Integer> ans = new ArrayList<>();
 
-	public int[] solution(String s) {
-		String[] str = s.substring(2, s.length() - 2).split("\\},\\{");
-		Arrays.sort(str, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o1.length() - o2.length();
-			}
-		});
+    public int[] solution(String s) {
+        String[] words = s.substring(2, s.length() - 2).split("\\},\\{");
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
 
-		for (String string : str) {
-			String[] tmp = string.split(",");
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            String[] word = words[i].split(",");
 
-			for (int i = 0; i < tmp.length; i++) {
-				if (!answer.contains(Integer.parseInt(tmp[i]))) {
-					answer.add(Integer.parseInt(tmp[i]));
-				}
-			}
-		}
+            for (int j = 0; j < word.length; j++) {
+                int number = Integer.parseInt(word[j]);
+                if (!set.contains(number)) {
+                    ans.add(number);
+                    set.add(number);
+                }
+            }
+        }
 
-		return answer.stream().mapToInt(i -> i).toArray();
-	}
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
 }
