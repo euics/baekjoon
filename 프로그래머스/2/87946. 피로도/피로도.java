6c3b60eb367c33fc7b2426a7b808078af7533b1c
euics <pjh575157@gmail.com>
2025-08-29 15:11:03 +0900

    [level 2] Title: 피로도, Time: 8.17 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,46 +1,49 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-	static int answer = 0;
-	static int[] dis;
+    static int n, ans = Integer.MIN_VALUE;
 
-	public int solution(int k, int[][] dungeons) {
-		int n = dungeons.length;
-		dis = new int[n];
+    public int solution(int k, int[][] dungeons) {
+        n = dungeons.length;
+        permutation(k, dungeons, 0, new int[n], new boolean[n]);
 
-		permutation(0, n, new boolean[n], k, dungeons);
+        return ans;
+    }
 
-		return answer;
-	}
+    public void permutation(int k, int[][] dungeons, int L, int[] permutation, boolean[] bool) {
+        if (L == n) {
+//            for (int x : permutation) System.out.printf("%d ", x);
+//            System.out.println();
+            ans = Math.max(ans, move(k, dungeons, permutation));
 
-	public void permutation(int L, int n, boolean[] bool, int k, int[][] dungeons) {
-		if (L == n) {
-			int cnt = 0;
+            return;
+        }
 
-			for (int i = 0; i < n; i++) {
-				if (dungeons[dis[i]][0] <= k) {
-					k -= dungeons[dis[i]][1];
-					cnt++;
-				}
+        for (int i = 0; i < n; i++) {
+            if (!bool[i]) {
+                bool[i] = true;
+                permutation[L] = i;
+                permutation(k, dungeons, L + 1, permutation, bool);
+                bool[i] = false;
+            }
+        }
+    }
 
-				if (k <= 0) {
-					break;
-				}
-			}
+    public int move(int k, int[][] dungeons, int[] permutation) {
+        int cnt = 0, remain = k;
 
-			answer = Math.max(answer, cnt);
+        for (int i = 0; i < n; i++) {
+            int min = dungeons[permutation[i]][0];
+            int remove = dungeons[permutation[i]][1];
 
-			return;
-		}
+            if (remain >= min) {
+                remain -= remove;
+                cnt++;
+            } else {
+                break;
+            }
+        }
 
-		for (int i = 0; i < n; i++) {
-			if (!bool[i]) {
-				bool[i] = true;
-				dis[L] = i;
-				permutation(L + 1, n, bool, k, dungeons);
-				bool[i] = false;
-			}
-		}
-	}
+        return cnt;
+    }
 }
