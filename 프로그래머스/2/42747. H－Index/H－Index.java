9d0802d81b4d23deabf3349971bb0e4cdd921634
euics <pjh575157@gmail.com>
2025-08-29 14:50:35 +0900

    [level 2] Title: H-Index, Time: 34.64 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,28 +1,23 @@
 import java.util.*;
 
 class Solution {
-	public int solution(int[] citations) {
-		Integer[] citationArray = Arrays.stream(citations).boxed().toArray(Integer[]::new);
-		Arrays.sort(citationArray, Collections.reverseOrder());
+    public int solution(int[] citations) {
+        Integer[] arr = Arrays.stream(citations).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, Collections.reverseOrder());
 
-		int h = citationArray[0], maxH = Integer.MIN_VALUE;
-		while (h >= 0) {
-			int cnt = 0;
+        int h = arr[0], maxH = Integer.MIN_VALUE;
+        while (h >= 0) {
+            int cnt = 0;
+            for (int citation : citations) if (citation >= h) cnt++;
 
-			for (int citation : citations) {
-				if (citation >= h) {
-					cnt++;
-				}
-			}
+            if (cnt >= h) {
+                maxH = h;
+                break;
+            }
 
-			if(cnt >= h) {
-				maxH = h;
-				break;
-			}
+            h--;
+        }
 
-			h--;
-		}
-
-		return maxH;
-	}
+        return maxH;
+    }
 }
