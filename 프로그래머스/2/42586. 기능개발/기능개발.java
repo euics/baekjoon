dcd861d25a9cd5d4af9b8409e60ccce99b2800f2
euics <pjh575157@gmail.com>
2025-08-29 15:00:46 +0900

    [level 2] Title: 기능개발, Time: 11.97 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,27 +1,29 @@
 import java.util.*;
 
 class Solution {
-	static ArrayList<Integer> answer = new ArrayList<>();
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] days = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            if ((100 - progresses[i]) % speeds[i] == 0) days[i] = (100 - progresses[i]) / speeds[i];
+            else days[i] = (100 - progresses[i]) / speeds[i] + 1;
+        }
 
-	public int[] solution(int[] progresses, int[] speeds) {
-		int[] remains = new int[progresses.length];
-		for (int i = 0; i < progresses.length; i++) {
-			remains[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
-		}
+        for (int i = 1; i < days.length; i++) if (days[i - 1] >= days[i]) days[i] = days[i - 1];
+        for (int day : days) System.out.printf("%d ", day);
 
-		int start = 0, end = 0, cnt = 0;
-		while (end < progresses.length) {
-			if (remains[start] >= remains[end]) {
-				cnt++;
-				end++;
-			} else {
-				answer.add(cnt);
-				start = end;
-				cnt = 0;
-			}
-		}
+        List<Integer> ans = new ArrayList<>();
+        int start = 0, end = 0;
+        while (end < days.length) {
+            int cnt = 0;
+            while (end < days.length && days[start] == days[end]) {
+                end++;
+                cnt++;
+            }
 
-		answer.add(cnt);
-		return answer.stream().mapToInt(i -> i).toArray();
-	}
+            ans.add(cnt);
+            start = end;
+        }
+
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
 }
