d5c6ee60bf7b0c0bde008fd828e25bcc5cb14bb8
euics <pjh575157@gmail.com>
2025-09-01 15:39:44 +0900

    [level 2] Title: 주차 요금 계산, Time: 22.53 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,50 +1,58 @@
 import java.util.*;
 
 class Solution {
-	static ArrayList<Integer> answer = new ArrayList<>();
-	static HashMap<Integer, Integer> inMap = new HashMap<>();
-	static TreeMap<Integer, Integer> recordMap = new TreeMap<>();
+    static List<Integer> ans = new ArrayList<>();
 
-	public int[] solution(int[] fees, String[] records) {
-		for (String record : records) {
-			String[] tmp = record.split(" ");
-			int carNum = Integer.parseInt(tmp[1]);
-			int time = convert(tmp[0]);
+    public int[] solution(int[] fees, String[] records) {
+        Map<String, Integer> parkingLot = new HashMap<>();
+        Map<String, Integer> times = new HashMap<>();
+        Map<String, Integer> fee = new HashMap<>();
 
-			switch (tmp[2]) {
-				case "IN":
-					inMap.put(carNum, time);
-					break;
-				case "OUT":
-					recordMap.put(carNum, recordMap.getOrDefault(carNum, 0) + time - inMap.get(carNum));
-					inMap.remove(carNum);
-					break;
-			}
-		}
+        for (String record : records) {
+            String[] split = record.split(" ");
 
-		for (int carNum : inMap.keySet()) {
-			int time = convert("23:59");
-			recordMap.put(carNum, recordMap.getOrDefault(carNum, 0) + time - inMap.get(carNum));
-		}
+            if (split[2].equals("IN")) {
+                parkingLot.put(split[1], time(split[0]));
+            } else {
+                int in = parkingLot.get(split[1]);
+                int out = time(split[0]);
+                int time = out - in;
 
-		for (int carNum : recordMap.keySet()) {
-			answer.add(calculate(fees, recordMap.get(carNum)));
-		}
+                times.put(split[1], times.getOrDefault(split[1], 0) + time);
+                parkingLot.remove(split[1]);
+            }
+        }
 
-		return answer.stream().mapToInt(i -> i).toArray();
-	}
+        for (String car : parkingLot.keySet()) {
+            int in = parkingLot.get(car);
+            int out = time("23:59");
+            int time = out - in;
 
-	public int convert(String time) {
-		return Integer.parseInt(time.split(":")[0]) * 60 + Integer.parseInt(time.split(":")[1]);
-	}
+            times.put(car, times.getOrDefault(car, 0) + time);
+        }
 
-	public int calculate(int[] fees, int time) {
-		if (time <= fees[0]) {
-			return fees[1];
-		} else {
-			double fee = (Math.ceil((double)(time - fees[0]) / fees[2])) * fees[3];
+        for (String car : times.keySet()) {
+            int calculate = calculate(fees[0], fees[1], fees[2], fees[3], times.get(car));
+            fee.put(car, calculate);
+        }
 
-			return (int)(fees[1] + fee);
-		}
-	}
+        List<String> cars = new LinkedList<>(fee.keySet());
+        Collections.sort(cars);
+        for (String car : cars) ans.add(fee.get(car));
+
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
+
+    public int time(String time) {
+        int hour = Integer.parseInt(time.split(":")[0]) * 60;
+        int min = Integer.parseInt(time.split(":")[1]);
+
+        return hour + min;
+    }
+
+    public int calculate(int basic, int fee, int per, int overFee, int time) {
+        if (time <= basic) return fee;
+
+        return (time - basic) % per == 0 ? fee + ((time - basic) / per) * overFee : fee + ((time - basic) / per + 1) * overFee;
+    }
 }
