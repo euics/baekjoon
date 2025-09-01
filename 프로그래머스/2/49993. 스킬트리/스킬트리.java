6919399bdb4fe12386c33a57574a6ff31ff0f1a3
euics <pjh575157@gmail.com>
2025-09-01 15:08:28 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,10 +1,28 @@
 import java.util.*;
 
 class Solution {
-	public int solution(String skill, String[] skill_trees) {
-		return (int)Arrays.stream(skill_trees)
-			.map(s -> s.replaceAll("[^" + skill + "]", ""))
-			.filter(skill::startsWith)
-			.count();
-	}
+    public int solution(String skill, String[] skill_trees) {
+        int ans = 0;
+
+        for (int i = 0; i < skill_trees.length; i++) {
+            int p = 0;
+            boolean bool = true;
+
+            for (int j = 0; j < skill_trees[i].length(); j++) {
+                int idx = skill.indexOf(skill_trees[i].charAt(j));
+                if (idx == -1) continue;
+
+                if (p != idx) {
+                    bool = false;
+                    break;
+                } else {
+                    p++;
+                }
+            }
+
+            if(bool) ans++;
+        }
+
+        return ans;
+    }
 }
