f2f8e9fc8ebb98dc41b23b3fef25751b1184cbe1
euics <pjh575157@gmail.com>
2025-09-01 13:45:45 +0900

    [level 2] Title: 더 맵게, Time: 1485.04 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = initPQ(scoville);
-        
-        int answer = 0;
-        while(!pq.isEmpty() && pq.size() >= 2 && pq.peek() < K) {
-            pq.add(pq.poll() + (pq.poll() * 2));
-            answer++;
-        }
-        
-        return pq.peek() < K ? -1 : answer;
-    }
-    
-    public PriorityQueue<Integer> initPQ(int[] scoville) {
+        int ans = 0;
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i = 0; i < scoville.length; i++) pq.add(scoville[i]);
-        
-        return pq;
+        for (int x : scoville) pq.add(x);
+
+        while (!pq.isEmpty()) {
+            int cur = pq.poll();
+            if (cur >= K) return ans;
+            if (pq.isEmpty()) return -1;
+
+            int next = pq.poll();
+            pq.add(cur + (next * 2));
+
+            ans++;
+        }
+
+        return ans;
     }
 }
