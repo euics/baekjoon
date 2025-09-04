1ed02e245cd40184d401f38642e85d13cdedc66f
euics <pjh575157@gmail.com>
2025-09-04 14:39:22 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 81.89 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,34 +1,32 @@
 import java.util.*;
 
 class Solution {
-	static int[] answer;
+    static int[] ans = new int[2];
 
-	public int[] solution(String[] gems) {
-		answer = new int[2];
-		Set<String> set = new HashSet<>(Arrays.asList(gems));
+    public int[] solution(String[] gems) {
+        Set<String> set = new HashSet<>(Arrays.asList(gems));
+        Map<String, Integer> map = new HashMap<>();
 
-		Map<String, Integer> map = new HashMap<>();
-		int start = 0, end = 0, min = Integer.MAX_VALUE;
-		while (end < gems.length) {
-			map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
+        int start = 0, end = 0, minLength = Integer.MAX_VALUE;
+        while (start < gems.length && end < gems.length) {
+            map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
 
-			while (start <= end && set.size() == map.keySet().size()) {
-				if (end - start + 1 < min) {
-					min = end - start + 1;
-					answer = new int[] {start + 1, end + 1};
-				}
+            while (start <= end && map.size() == set.size()) {
+                int curLength = end - start + 1;
+                if (curLength < minLength) {
+                    minLength = curLength;
+                    ans[0] = start + 1;
+                    ans[1] = end + 1;
+                }
 
-				map.put(gems[start], map.get(gems[start]) - 1);
-				if (map.get(gems[start]) == 0) {
-					map.remove(gems[start]);
-				}
+                map.put(gems[start], map.getOrDefault(gems[start], 0) - 1);
+                if (map.get(gems[start]) == 0) map.remove(gems[start]);
+                start++;
+            }
 
-				start++;
-			}
+            end++;
+        }
 
-			end++;
-		}
-
-		return answer;
-	}
+        return ans;
+    }
 }
