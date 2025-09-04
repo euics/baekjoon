38ab04f446c85a97acb1246ea9be8c7352d14b79
euics <pjh575157@gmail.com>
2025-09-04 16:48:08 +0900

    [level 3] Title: 불량 사용자, Time: 381.18 ms, Memory: 399 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,28 +1,28 @@
 import java.util.*;
 
 class Solution {
-	static Set<Set<String>> result = new HashSet<>();
+    static Set<Set<String>> result = new HashSet<>();
 
-	public int solution(String[] user_id, String[] banned_id) {
-		matchPattern(0, user_id, banned_id, new HashSet<String>());
+    public int solution(String[] user_id, String[] banned_id) {
+        matches(0, user_id, banned_id, new HashSet<>());
 
-		return result.size();
-	}
+        return result.size();
+    }
 
-	public void matchPattern(int L, String[] user_id, String[] banned_id, Set<String> set) {
-		if (L == banned_id.length) {
-			result.add(new HashSet<>(set));
+    public void matches(int L, String[] user_id, String[] banned_id, Set<String> set) {
+        if (L == banned_id.length) {
+            result.add(new HashSet<>(set));
 
-			return;
-		}
+            return;
+        }
 
-		String pattern = banned_id[L].replace("*", ".");
-		for (int i = 0; i < user_id.length; i++) {
-			if (user_id[i].matches(pattern) && !set.contains(user_id[i])) {
-				set.add(user_id[i]);
-				matchPattern(L + 1, user_id, banned_id, set);
-				set.remove(user_id[i]);
-			}
-		}
-	}
+        String pattern = banned_id[L].replace("*", ".");
+        for (int i = 0; i < user_id.length; i++) {
+            if (user_id[i].matches(pattern) && !set.contains(user_id[i])) {
+                set.add(user_id[i]);
+                matches(L + 1, user_id, banned_id, set);
+                set.remove(user_id[i]);
+            }
+        }
+    }
 }
