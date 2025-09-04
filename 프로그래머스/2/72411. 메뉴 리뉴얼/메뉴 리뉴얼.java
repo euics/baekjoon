16663740ff9c5b4a3f6ff6fcb341843efde31a38
euics <pjh575157@gmail.com>
2025-09-04 15:40:47 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5642.23 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,68 +1,91 @@
 import java.util.*;
 
 class Solution {
-	static List<String> answer = new ArrayList<>();
-	static Map<String, Integer> candidates = new HashMap<>();
+    static List<String> ans = new ArrayList<>();
+    static Map<String, Integer> candidate = new HashMap<>();
 
-	public String[] solution(String[] orders, int[] course) {
-		for (String order : orders) {
-			for (int i = 2; i <= order.length(); i++) {
-				char[] ch = order.toCharArray();
-				Arrays.sort(ch);
+    public String[] solution(String[] orders, int[] course) {
+        Set<Character> set = new HashSet<>();
+        for (String order : orders) for (int i = 0; i < order.length(); i++) set.add(order.charAt(i));
 
-				combination(0, i, 0, ch, new char[i]);
-			}
-		}
-		candidates.keySet().removeIf(key -> candidates.get(key) < 2);
+        Map<Integer, List<Character>> map = new HashMap<>();
+        for (int i = 0; i < orders.length; i++) {
+            map.putIfAbsent(i, new ArrayList<Character>());
+            for (int j = 0; j < orders[i].length(); j++) {
+                map.get(i).add(orders[i].charAt(j));
+            }
+        }
 
-		LinkedList<String> keySet = new LinkedList<>(candidates.keySet());
-		Collections.sort(keySet, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				if (o1.length() == o2.length()) {
-					return o1.compareTo(o2);
-				} else {
-					return o1.length() - o2.length();
-				}
-			}
-		});
+        char[] menu = new char[set.size()];
+        int idx = 0;
+        for (char ch : set) menu[idx++] = ch;
+        Arrays.sort(menu);
 
-		for (int length : course) {
-			int max = Integer.MIN_VALUE;
+        for (int i = 0; i < course.length; i++) {
+            int length = course[i];
+            char[] combination = new char[length];
+            combination(0, length, 0, menu, map, combination);
+        }
 
-			for (String key : keySet) {
-				if (length == key.length()) {
-					max = Math.max(max, candidates.get(key));
-				}
-			}
+        List<String> key = new LinkedList<>(candidate.keySet());
+        Collections.sort(key, (a, b) -> {
+            if (a.length() != b.length()) return a.length() - b.length();
 
-			for (String key : keySet) {
-				if (length == key.length() && candidates.get(key) == max) {
-					answer.add(key);
-				}
-			}
-		}
+            return candidate.get(b) - candidate.get(a);
+        });
 
-		Collections.sort(answer);
+        for (int i = 0; i < course.length; i++) {
+            int length = course[i];
+            int max = Integer.MIN_VALUE;
 
-		return answer.stream().toArray(String[]::new);
-	}
+            for (String k : key) {
+                if (k.length() == length) {
+                    max = Math.max(max, candidate.get(k));
+                }
+            }
 
-	public void combination(int L, int length, int s, char[] order, char[] permutation) {
-		if (L == length) {
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < permutation.length; i++) {
-				sb.append(permutation[i]);
-			}
+            for (String k : key) {
+                if (k.length() == length && max == candidate.get(k)) {
+                    ans.add(k);
+                }
+            }
+        }
 
-			candidates.put(sb.toString(), candidates.getOrDefault(sb.toString(), 0) + 1);
+        Collections.sort(ans);
 
-			return;
-		}
+        return ans.stream().toArray(String[]::new);
+    }
 
-		for (int i = s; i < order.length; i++) {
-			permutation[L] = order[i];
-			combination(L + 1, length, i + 1, order, permutation);
-		}
-	}
+    public void combination(int L, int length, int s, char[] menu, Map<Integer, List<Character>> map, char[] combination) {
+        if (L == length) {
+            int count = count(map, combination);
+            if (count >= 2) candidate.put(String.valueOf(combination), count);
+
+            return;
+        }
+
+        for (int i = s; i < menu.length; i++) {
+            combination[L] = menu[i];
+            combination(L + 1, length, i + 1, menu, map, combination);
+        }
+    }
+
+    public int count(Map<Integer, List<Character>> map, char[] combination) {
+        int cnt = 0;
+
+        for (int key : map.keySet()) {
+            boolean bool = true;
+
+            for (int i = 0; i < combination.length; i++) {
+                if (!map.get(key).contains(combination[i])) {
+                    bool = false;
+                    break;
+                }
+            }
+
+            if (bool) cnt++;
+        }
+
+        return cnt;
+    }
 }
