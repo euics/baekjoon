e8b0fb81617820da7b01449aa5c9173d1675b2d0
euics <pjh575157@gmail.com>
2025-09-05 14:03:13 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.23 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,37 +1,32 @@
 import java.util.*;
 
 class Solution {
-	static int answer = 0;
+    static int ans = 0;
 
-	public int solution(int[][] board, int[] moves) {
-		Stack<Integer> stack = new Stack<>();
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer> box = new Stack<>();
+        for (int i = 0; i < moves.length; i++) move(board, box, moves[i] - 1);
 
-		for (int x : moves) {
-			x -= 1;
-			int y = isEmpty(board, x);
+        return ans;
+    }
 
-			if (y != -1) {
-				if (!stack.isEmpty() && stack.peek() == board[y][x]) {
-					stack.pop();
-					answer += 2;
-				} else {
-					stack.push(board[y][x]);
-				}
+    public void move(int[][] board, Stack<Integer> box, int col) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][col] != 0) {
+                if (!box.isEmpty()) {
+                    if (board[row][col] == box.peek()) {
+                        box.pop();
+                        ans += 2;
+                    } else {
+                        box.push(board[row][col]);
+                    }
+                } else {
+                    box.push(board[row][col]);
+                }
 
-				board[y][x] = 0;
-			}
-		}
-
-		return answer;
-	}
-
-	public int isEmpty(int[][] board, int x) {
-		for (int y = 0; y < board.length; y++) {
-			if (board[y][x] != 0) {
-				return y;
-			}
-		}
-
-		return -1;
-	}
+                board[row][col] = 0;
+                break;
+            }
+        }
+    }
 }
