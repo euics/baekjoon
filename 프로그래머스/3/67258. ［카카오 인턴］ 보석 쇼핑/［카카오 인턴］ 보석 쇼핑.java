a06b07328dca565a3abeba4123c305b85200e179
euics <pjh575157@gmail.com>
2025-09-10 14:34:39 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 70.11 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -5,21 +5,20 @@ class Solution {
 
     public int[] solution(String[] gems) {
         Set<String> set = new HashSet<>(Arrays.asList(gems));
-        Map<String, Integer> map = new HashMap<>();
 
-        int start = 0, end = 0, minLength = Integer.MAX_VALUE;
-        while (start < gems.length && end < gems.length) {
+        Map<String, Integer> map = new HashMap<>();
+        int start = 0, end = 0, min = Integer.MAX_VALUE;
+        while (end < gems.length) {
             map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
 
-            while (start <= end && map.size() == set.size()) {
-                int curLength = end - start + 1;
-                if (curLength < minLength) {
-                    minLength = curLength;
+            while (start <= end && set.size() == map.size()) {
+                if (min > end - start + 1) {
+                    min = end - start + 1;
                     ans[0] = start + 1;
                     ans[1] = end + 1;
                 }
 
-                map.put(gems[start], map.getOrDefault(gems[start], 0) - 1);
+                map.put(gems[start], map.get(gems[start]) - 1);
                 if (map.get(gems[start]) == 0) map.remove(gems[start]);
                 start++;
             }
