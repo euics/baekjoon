b157b96f6ea2bec008cd4eb0cd71a70441923f65
euics <pjh575157@gmail.com>
2025-09-11 16:53:53 +0900

    [level 2] Title: 거리두기 확인하기, Time: 9.08 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,74 +1,60 @@
+import java.util.*;
+
 class Solution {
+    static List<Integer> ans = new ArrayList<>();
+
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-        int idx = 0;
-        
-        for(String[] place : places) {
-            boolean bool = false;
-            
-            for(int i = 0; i < 5; i++) {
-                for(int j = 0; j < 5; j++) {
-                    if(place[i].charAt(j) == 'P') {
-                        if(isDistance(place, j, i)) {
-                            bool = true;
+        for (int i = 0; i < 5; i++) {
+            char[][] ch = new char[5][5];
+            for (int j = 0; j < 5; j++) for (int k = 0; k < 5; k++) ch[j][k] = places[i][j].charAt(k);
+
+            boolean safe = true;
+            for (int row = 0; row < 5; row++) {
+                for (int col = 0; col < 5; col++) {
+                    if (ch[row][col] == 'P') {
+                        if (isDistance(ch, row, col)) {
+                            safe = false;
                             break;
                         }
                     }
                 }
-                
-                if(bool) {
-                    break;
-                }
             }
-            
-            answer[idx++] = bool ? 0 : 1;
+
+            if (safe) ans.add(1);
+            else ans.add(0);
         }
-        
-        return answer;
+
+        return ans.stream().mapToInt(i -> i).toArray();
     }
-    
-    static int[] dix = {0, 0, -1, 1};
-    static int[] diy = {-1, 1, 0, 0};
-    
-    static int[] dix2 = {0, 0, -2, 2, 1, 1, -1, 1};
-    static int[] diy2 = {-2, 2, 0, 0, -1, 1, 1, -1};
-    
-    public boolean isDistance(String[] place, int x, int y) {
-        // 멘하튼 거리 1
-        for(int d = 0; d < 4; d++) {
-            int nx = x + dix[d];
-            int ny = y + diy[d];
-            
-            if(nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
-                if(place[ny].charAt(nx) == 'P') {
-                    return true;
-                }
-            }
+
+    public boolean isDistance(char[][] ch, int row, int col) {
+        int[] dix = new int[]{0, 0, -1, 1};
+        int[] diy = new int[]{-1, 1, 0, 0};
+        for (int d = 0; d < 4; d++) {
+            int nx = col + dix[d];
+            int ny = row + diy[d];
+            if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5) if (ch[ny][nx] == 'P') return true;
         }
-        
-        // 멘하튼 거리 2
-        for(int d = 0; d < 8; d++) {
-            int nx = x + dix2[d];
-            int ny = y + diy2[d];
-            
-            if(nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
-                if(place[ny].charAt(nx) == 'P') {
-                    if(d < 4) {
-                        int midX = (x + nx) / 2;
-                        int midY = (y + ny) / 2;
-                        
-                        if(place[midY].charAt(midX) != 'X') {
-                            return true;
-                        }
-                    } else {
-                        if(place[ny].charAt(x) != 'X' || place[y].charAt(nx) != 'X') {
-                            return true;
-                        }
+
+        dix = new int[]{0, 0, -2, 2, 1, 1, -1, -1};
+        diy = new int[]{-2, 2, 0, 0, -1, 1, -1, 1};
+        for (int d = 0; d < 8; d++) {
+            int nx = col + dix[d];
+            int ny = row + diy[d];
+
+            if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5 && ch[ny][nx] == 'P') {
+                if (d < 4) {
+                    if (ch[(row + ny) / 2][(col + nx) / 2] != 'X') {
+                        return true;
+                    }
+                } else {
+                    if (ch[ny][col] != 'X' || ch[row][nx] != 'X') {
+                        return true;
                     }
                 }
             }
         }
-        
+
         return false;
     }
 }
