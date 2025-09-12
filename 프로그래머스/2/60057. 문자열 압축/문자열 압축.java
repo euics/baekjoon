a0201cdcababce4e10e862c81deca8308224b810
euics <pjh575157@gmail.com>
2025-09-12 17:14:29 +0900

    [level 2] Title: 문자열 압축, Time: 16.48 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,38 +1,37 @@
 import java.util.*;
 
 class Solution {
+    static int min = Integer.MAX_VALUE;
+
     public int solution(String s) {
-        int min = s.length();
-        
-        for(int compress = 1; compress <= s.length(); compress++) {
-            Queue<String> q = new LinkedList<>();
-            
-            for(int i = 0; i < s.length(); i += compress) {
-                String str = s.substring(i, Math.min(i + compress, s.length()));
-                q.add(str);
-            }
-            
-            StringBuilder sb = new StringBuilder();
-            
-            while(!q.isEmpty()) {
-                String cur = q.poll();
-                int cnt = 1;
-                
-                while(!q.isEmpty() && cur.equals(q.peek())) {
-                    q.poll();
-                    cnt++;
-                }
-                
-                if(cnt == 1) {
-                    sb.append(cur);
-                } else {
-                    sb.append(cnt).append(cur);
-                }
-            }
-            
-            min = Math.min(min, sb.toString().length());
+        for (int length = 1; length <= s.length(); length++) {
+            String compress = compress(s, length);
+            if (min > compress.length()) min = compress.length();
         }
-        
+
         return min;
+    }
+
+    public String compress(String s, int length) {
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        while (idx < s.length() - length + 1) {
+            String prefix = s.substring(idx, idx + length);
+            int cnt = 1;
+
+            for (int j = idx + length; j < s.length() - length + 1; j += length) {
+                String suffix = s.substring(j, j + length);
+                if (prefix.equals(suffix)) cnt++;
+                else break;
+            }
+
+            if (cnt == 1) sb.append(prefix);
+            else sb.append(cnt).append(prefix);
+
+            idx += cnt * length;
+        }
+
+        while (idx < s.length()) sb.append(s.charAt(idx++));
+        return sb.toString();
     }
 }
