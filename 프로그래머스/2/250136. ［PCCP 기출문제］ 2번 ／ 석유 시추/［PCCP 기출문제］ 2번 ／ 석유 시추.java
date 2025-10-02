f50285fb8fc796bd40d7b4810466448413230ff8
euics <pjh575157@gmail.com>
2025-10-02 12:42:50 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 76.14 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,58 +1,51 @@
 import java.util.*;
 
 class Solution {
-	static int L = 0, idx = 1, answer = Integer.MIN_VALUE;
-	static int[] dix = {0, 0, -1, 1};
-	static int[] diy = {-1, 1, 0, 0};
-	static Map<Integer, Integer> map = new HashMap<>();
+    static int ans = Integer.MIN_VALUE, cnt, idx = 0;
+    static Map<Integer, Integer> map = new HashMap<>();
+    static int[] dix = {0, 0, -1, 1};
+    static int[] diy = {-1, 1, 0, 0};
 
-	public int solution(int[][] land) {
-		boolean[][] bool = new boolean[land.length][land[0].length];
+    public int solution(int[][] land) {
+        boolean[][] bool = new boolean[land.length][land[0].length];
+        for (int i = 0; i < land.length; i++) {
+            for (int j = 0; j < land[i].length; j++) {
+                if (land[i][j] == 1 && !bool[i][j]) {
+                    cnt = 0;
+                    idx++;
+                    DFS(land, j, i, bool);
+                    map.put(idx, cnt);
+                }
+            }
+        }
 
-		for (int i = 0; i < land.length; i++) {
-			for (int j = 0; j < land[i].length; j++) {
-				if (land[i][j] == 1 && !bool[i][j]) {
-					L = 0;
-					DFS(land, j, i, bool);
+        for (int col = 0; col < land[0].length; col++) {
+            Set<Integer> set = new HashSet<>();
+            for (int row = 0; row < land.length; row++) if (land[row][col] != 0) set.add(land[row][col]);
 
-					map.put(idx, L);
-					idx++;
-				}
-			}
-		}
+            int sum = 0;
+            for (int idx : set) sum += map.get(idx);
+            ans = Math.max(ans, sum);
+        }
 
-		for (int x = 0; x < land[0].length; x++) {
-			int sum = 0;
-			Set<Integer> set = new HashSet<>();
+        return ans;
+    }
 
-			for (int y = 0; y < land.length; y++) {
-				if (land[y][x] != 0 && !set.contains(land[y][x])) {
-					sum += map.get(land[y][x]);
-					set.add(land[y][x]);
-				}
-			}
+    public void DFS(int[][] land, int x, int y, boolean[][] bool) {
+        cnt++;
+        bool[y][x] = true;
+        land[y][x] = idx;
 
-			answer = Math.max(answer, sum);
-		}
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dix[d];
+            int ny = y + diy[d];
 
-		return answer;
-	}
-
-	public void DFS(int[][] land, int x, int y, boolean[][] bool) {
-		L++;
-		land[y][x] = idx;
-		bool[y][x] = true;
-
-		for (int d = 0; d < 4; d++) {
-			int nx = x + dix[d];
-			int ny = y + diy[d];
-
-			if (nx >= 0 && ny >= 0 && nx < land[0].length && ny < land.length && land[ny][nx] == 1) {
-				if (!bool[ny][nx]) {
-					bool[ny][nx] = true;
-					DFS(land, nx, ny, bool);
-				}
-			}
-		}
-	}
+            if (nx >= 0 && ny >= 0 && nx < land[0].length && ny < land.length && land[ny][nx] == 1) {
+                if (!bool[ny][nx]) {
+                    bool[ny][nx] = true;
+                    DFS(land, nx, ny, bool);
+                }
+            }
+        }
+    }
 }
