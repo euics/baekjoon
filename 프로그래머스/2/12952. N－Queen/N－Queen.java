5f5b2cd8d641f68a3d33cb675ef2035100fa062a
euics <pjh575157@gmail.com>
2025-10-14 20:46:17 +0900

    [level 2] Title: N-Queen, Time: 151.00 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,40 +1,35 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-	static int answer = 0;
-	static int[] permutation;
+    static int ans = 0;
+    static int[] permutation;
 
-	public int solution(int n) {
-		permutation = new int[n];
-		permutation(0, n);
+    public int solution(int n) {
+        permutation = new int[n];
+        permutation(0, n);
 
-		return answer;
-	}
+        return ans;
+    }
 
-	public void permutation(int row, int n) {
-		if (row == n) {
-			answer++;
+    public void permutation(int L, int n) {
+        if (L == n) {
+            ans++;
 
-			return;
-		}
+            return;
+        }
 
-		for (int col = 0; col < n; col++) {
-			permutation[row] = col;
+        for (int col = 0; col < n; col++) {
+            permutation[L] = col;
+            if (!conflict(L)) permutation(L + 1, n);
+        }
+    }
 
-			if (queenRoute(row)) {
-				permutation(row + 1, n);
-			}
-		}
-	}
+    public boolean conflict(int row) {
+        for (int i = 0; i < row; i++) {
+            if (permutation[i] == permutation[row]) return true;
+            if (Math.abs(permutation[i] - permutation[row]) == Math.abs(row - i)) return true;
+        }
 
-	public boolean queenRoute(int row) {
-		for (int i = 0; i < row; i++) {
-			if (permutation[i] == permutation[row] || Math.abs(permutation[i] - permutation[row]) == row - i) {
-				return false;
-			}
-		}
-
-		return true;
-	}
+        return false;
+    }
 }
