2d84520ac3658e480790da06788d2edbccfa5188
euics <pjh575157@gmail.com>
2025-10-16 13:41:23 +0900

    [level 2] Title: 요격 시스템, Time: 446.24 ms, Memory: 197 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -5,18 +5,18 @@ class Solution {
 
     public int solution(int[][] targets) {
         Arrays.sort(targets, (a, b) -> {
-            if (a[1] == b[0]) return a[0] - b[0];
-            else return a[1] - b[1];
+            if (a[1] == b[1]) return a[0] - b[0];
+            return a[1] - b[1];
         });
 
         int missile = Integer.MIN_VALUE;
-        for (int[] target : targets) {
-            int start = target[0];
-            int end = target[1];
+        for (int i = 0; i < targets.length; i++) {
+            int s = targets[i][0];
+            int e = targets[i][1];
 
-            if (start >= missile) {
+            if (s >= missile) {
                 ans++;
-                missile = end;
+                missile = e;
             }
         }
 
