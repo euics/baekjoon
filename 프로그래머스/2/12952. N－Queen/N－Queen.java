1c25a1c5b5305d96a643d81287d31c392d07d68b
euics <pjh575157@gmail.com>
2025-10-16 13:29:02 +0900

    [level 2] Title: N-Queen, Time: 175.35 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -6,30 +6,33 @@ class Solution {
 
     public int solution(int n) {
         permutation = new int[n];
-        permutation(0, n);
+        DFS(0, n);
 
         return ans;
     }
 
-    public void permutation(int L, int n) {
-        if (L == n) {
+    public void DFS(int row, int n) {
+        if (row == n) {
             ans++;
 
             return;
         }
 
         for (int col = 0; col < n; col++) {
-            permutation[L] = col;
-            if (!conflict(L)) permutation(L + 1, n);
+            permutation[row] = col;
+
+            if (canMove(row)) {
+                DFS(row + 1, n);
+            }
         }
     }
 
-    public boolean conflict(int row) {
+    public boolean canMove(int row) {
         for (int i = 0; i < row; i++) {
-            if (permutation[i] == permutation[row]) return true;
-            if (Math.abs(permutation[i] - permutation[row]) == Math.abs(row - i)) return true;
+            if (permutation[row] == permutation[i]) return false;
+            if (Math.abs(i - row) == Math.abs(permutation[i] - permutation[row])) return false;
         }
 
-        return false;
+        return true;
     }
 }
