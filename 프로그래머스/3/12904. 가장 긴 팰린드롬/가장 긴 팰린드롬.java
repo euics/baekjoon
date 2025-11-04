5f2804ab76a61b46a05e4886d5d7d78af84b03f7
euics <pjh575157@gmail.com>
2025-11-04 14:03:19 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 590.79 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,24 +1,23 @@
+import java.util.*;
+
 class Solution {
-	public int solution(String s) {
-		int answer = 0;
+    static int ans = Integer.MIN_VALUE;
 
-		for (int i = s.length() - 1; i >= 0; i--) {
-			for (int j = 0; j <= i; j++) {
-				if (isPalindrome(s, j, i)) {
-					answer = Math.max(answer, i - j + 1);
-				}
-			}
-		}
+    public int solution(String s) {
+        for (int end = s.length() - 1; end >= 0; end--) {
+            for (int start = 0; start <= end; start++) {
+                if (palindrome(s, start, end)) {
+                    ans = Math.max(ans, end - start + 1);
+                }
+            }
+        }
 
-		return answer;
-	}
+        return ans;
+    }
 
-	public boolean isPalindrome(String value, int start, int end) {
-		while (start <= end) {
-			if (value.charAt(start++) != value.charAt(end--))
-				return false;
-		}
+    public boolean palindrome(String s, int start, int end) {
+        while (start <= end) if (s.charAt(start++) != s.charAt(end--)) return false;
 
-		return true;
-	}
+        return true;
+    }
 }
