e7ac6b5c9e4dac6f25734c87dd40e2c70e915345
euics <pjh575157@gmail.com>
2025-11-04 13:42:58 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 75.33 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,38 +1,32 @@
 import java.util.*;
 
 class Solution {
-	static int[] answer;
+    static int[] ans;
+    static Map<String, String> graph = new HashMap<>();
+    static Map<String, Integer> profit = new HashMap<>();
 
-	public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amounts) {
-		Map<String, String> graph = new HashMap<>();
-		Map<String, Integer> profit = new HashMap<>();
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        for (int i = 0; i < referral.length; i++) graph.put(enroll[i], referral[i]);
+        for (int i = 0; i < amount.length; i++) amount[i] *= 100;
 
-		for (int i = 0; i < enroll.length; i++) {
-			graph.putIfAbsent(enroll[i], referral[i]);
-			profit.put(enroll[i], 0);
-		}
+        for (int i = 0; i < seller.length; i++) {
+            profit.put(seller[i], profit.getOrDefault(seller[i], 0) + amount[i] - amount[i] / 10);
+            String parent = seller[i];
+            int left = amount[i] / 10;
 
-		for (int i = 0; i < seller.length; i++) {
-			int amount = amounts[i] * 100;
-			String prev = seller[i];
+            while (!parent.equals("-")) {
+                parent = graph.get(parent);
+                if (parent.equals("-")) profit.put(parent, profit.getOrDefault(parent, 0) + left);
+                else profit.put(parent, profit.getOrDefault(parent, 0) + left - left / 10);
+                left /= 10;
 
-			while (!prev.equals("-")) {
-				int pro = amount / 10;
-				profit.put(prev, profit.getOrDefault(prev, 0) + amount - pro);
-				prev = graph.get(prev);
-				amount = pro;
-				
-				if(amount < 1) {
-					break;
-				}
-			}
-		}
+                if (left < 1) break;
+            }
+        }
 
-		answer = new int[enroll.length];
-		for (int i = 0; i < enroll.length; i++) {
-			answer[i] = profit.get(enroll[i]);
-		}
+        ans = new int[enroll.length];
+        for (int i = 0; i < enroll.length; i++) ans[i] = profit.get(enroll[i]) == null ? 0 : profit.get(enroll[i]);
 
-		return answer;
-	}
+        return ans;
+    }
 }
