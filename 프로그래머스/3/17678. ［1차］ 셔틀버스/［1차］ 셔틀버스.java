c35111f38c7372938ad17367ae28b29963cfb7d2
euics <pjh575157@gmail.com>
2025-11-04 12:31:55 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 6.57 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -3,35 +3,35 @@ import java.util.*;
 class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
         int[] times = new int[timetable.length];
-        for (int i = 0; i < timetable.length; i++) times[i] = time(timetable[i]);
+        for (int i = 0; i < timetable.length; i++) times[i] = convert(timetable[i]);
         Arrays.sort(times);
 
-        for (int i = 0, bus = time("09:00"), lastBoardTime = -1, idx = 0; i < n; i++, bus += t) {
+        for (int i = 0, shuttle = convert("09:00"), idx = 0, last = -1; i < n; i++, shuttle += t) {
             int cnt = 0;
 
-            while (idx < times.length && times[idx] <= bus && cnt < m) {
-                lastBoardTime = times[idx];
-                idx++;
+            while (idx < times.length && times[idx] <= shuttle && cnt < m) {
+                last = times[idx];
                 cnt++;
+                idx++;
             }
 
             if (i == n - 1) {
-                if (cnt < m) return time(bus);
-                else return time(lastBoardTime - 1);
+                if (cnt < m) return time(shuttle);
+                else return time(last - 1);
             }
         }
 
         return "";
     }
 
-    public int time(String s) {
-        String[] a = s.split(":");
-        return Integer.parseInt(a[0]) * 60 + Integer.parseInt(a[1]);
+    public int convert(String time) {
+        return Integer.parseInt(time.split(":")[0]) * 60 + Integer.parseInt(time.split(":")[1]);
     }
 
-    public String time(int minutes) {
-        int h = minutes / 60;
-        int m = minutes % 60;
-        return String.format("%02d:%02d", h, m);
+    public String time(int time) {
+        int hour = time / 60;
+        int minute = time % 60;
+
+        return String.format("%02d:%02d", hour, minute);
     }
 }
